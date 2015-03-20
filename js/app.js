682596d23fa1abb5a1a16182e4549953bc43f203
Richard Pinedo <richard.pinedo@yahoo.com>
2015-03-20 09:01:44 -0400

    Fixed the browser icon to open the tab, added a tab title, updated the manifest json to include it's own title, updated icons to use DIV instead of DIM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var optionsUrl = chrome.extension.getURL('window.html');
+	chrome.tabs.query({url: optionsUrl}, function(tabs) {
+	    if (tabs.length) {
+	        chrome.tabs.update(tabs[0].id, {active: true});
+	    } else {
+	        chrome.tabs.create({url: optionsUrl});
+	    }
+	});
+});
+
 var Weapon = function(stats, ids){
 	var self = this;
 	Object.keys(stats).forEach(function(key){
