2951d94156920444611288ea833c745fa106f6c4
Val Lyashov <val.lyashov@envato.com>
2015-04-14 15:14:24 +1000

    Updated *Ir Yut names in itemDefs, and damage type display

--- scripts/main.js
+++ scripts/main.js
@@ -691,7 +691,7 @@ function appendItems(owner, items) {
 			itemSort = 'Postmaster';
 		}
 
-		var dmgName = ['kinetic',,'arc','solar','void'][item.damageType];
+		var dmgName = ['na','kinetic','arc','solar','void'][item.damageType];
 
 
 		// for(var i = 0; i < item.stats.length; i++) {
