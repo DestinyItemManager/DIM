58dc929242824da36a3602eec4a754b0dea42732
SunburnedGoose <rick.casey@gmail.com>
2015-04-29 09:49:24 -0500

    Fixed search in /dist

--- dist/scripts/shell/dimSearchFilter.directive.js
+++ dist/scripts/shell/dimSearchFilter.directive.js
@@ -13,7 +13,7 @@
       bindToController: true,
       restrict: 'A',
       template: [
-        '<input type="search">'
+        '<input placeholder="filter items or is:arc" type="search" name="filter" ng-model="vm.search" ng-model-options="{ debounce: 500 }" ng-trim="true" ng-change="vm.filter()">'
       ].join('')
     };
   }
