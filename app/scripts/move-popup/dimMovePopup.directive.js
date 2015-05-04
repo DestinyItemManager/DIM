8db83a098afd01bba350b8572cdfbbbbb511dbd5
SunburnedGoose <rick.casey@gmail.com>
2015-05-04 09:26:45 -0500

    Fixed #78 by removing consumables and materials 'equip' option from move item popup.

--- app/scripts/move-popup/dimMovePopup.directive.js
+++ app/scripts/move-popup/dimMovePopup.directive.js
@@ -48,53 +48,10 @@
     var vm = this;
 
     function moveToVaultFn(store, e) {
-      // var promise = $q.when(vm.item);
-      //
-      // if (vm.item.equipped) {
-      //   var item = vm.getSimilarItem(vm.item);
-      //
-      //   if (item) {
-      //
-      //   } else {
-      //     // Can't find item to replace.
-      //   }
-      //   // get comparable item
-      //   // equip comparable item
-      // } else {
       dimItemService.moveTo(vm.item, store)
         .catch(function(a) {
           toaster.pop('error', vm.item.name, a.message);
         });
-      // .then(moveItemUI.bind(null, vm.item, store));
-      // }
-
-
-
-      // if (store.id !== 'vault') {
-      //   // move to vault
-      // }
-
-      // var current = _.find(vm.store.items, function (item) {
-      //   return ((item.type === vm.item.type) && (vm.item.sort === item.sort) && item.equipped);
-      // });
-      //
-      // var i = _.indexOf(vm.store.items, vm.item);
-      //
-      // if (i >= 0) {
-      //   vm.store.items.splice(i, 1);
-      //   store.items.push(vm.item);
-      //   vm.item.owner = store.id;
-      //   vm.item.equipped = true;
-      // }
-      //
-      // i = _.indexOf(vm.store.items, current);
-      //
-      // if (i >= 0) {
-      //   vm.store.items.splice(i, 1);
-      //   store.items.push(current);
-      //   current.owner = store.id;
-      //   current.equipped = false;
-      // }
     }
 
     function moveItemUI(item, targetStore) {
@@ -117,23 +74,6 @@
       .catch(function(a) {
         toaster.pop('error', vm.item.name, a.message);
       });
-      // if (vm.item.equipped) {
-      //   var item = vm.getSimilarItem(vm.item)
-      //     .then(function(item) {
-      //       vm.item.equipped = false;
-      //       moveItemUI(vm.item, store);
-      //       moveItemUI(item, vm.store);
-      //       item.equipped = true;
-      //     });
-      // }
-      //
-      // if ((vm.item.owner !== store.id) && (store.id !== 'vault')) {
-      //   // move to vault
-      // }
-      //
-      // if (vm.item.owner === 'vault') {
-      //   // Move to store
-      // }
     }
 
     function moveToEquipFn(store, e) {
@@ -141,95 +81,11 @@
         .catch(function(a) {
           toaster.pop('error', vm.item.name, a.message);
         });
-      // if (vm.item.equipped && (vm.item.owner !== store.id)) {
-      //   var item = vm.getSimilarItem(vm.item)
-      //     .then(function(item) {
-      //       moveItemUI(vm.item, store);
-      //       moveItemUI(item, vm.store);
-      //       item.equipped = vm.item.equipped;
-      //       vm.item.equipped = true;
-      //     });
-      //   // get comparable item
-      //   // equip comparable item
-      // }
-      //
-      // if ((vm.item.owner !== store.id) && (store.id !== 'vault')) {
-      //   // move to vault
-      // }
-      //
-      // if (vm.item.owner === 'vault') {
-      //   // Move to store
-      // }
-
-      // equip item
-    }
-
-    function getSimilarItemFn(item) {
-      return dimStoreService.getStore(item.owner)
-        .then(function(store) {
-          var result = null;
-          var stores = _.sortBy(vm.stores, function(s) {
-            if (store.id === s.id) {
-              return 0;
-            } else if (s.id === 'vault') {
-              return 1;
-            } else {
-              return 2;
-            }
-          });
-
-          _.each(stores, function(s) {
-            if (_.isNull(result)) {
-              result = getItemFn(item, s);
-            }
-          });
-
-          return result;
-        });
-    }
-
-    function getItemFn(fnItem, fnStore) {
-      var result = null;
-      var sortType = {
-        Legendary: 0,
-        Rare: 1,
-        Uncommon: 2,
-        Common: 3,
-        Basic: 4,
-        Exotic: 5
-      };
-
-      var results = _.chain(fnStore.items)
-        .where({
-          classType: fnItem.classType
-        })
-        .sortBy(function(i) {
-          return sortType[i.tier];
-        })
-        .where({
-          type: fnItem.type
-        })
-        .value();
-
-      if (_.size(results) > 0) {
-        result = results[0];
-
-        if ((result.id === fnItem.id) && (result.hash === fnItem.hash)) {
-          if (_.size(results) > 1) {
-            result = results[1];
-          } else {
-            result = null;
-          }
-        }
-      }
-
-      return result;
     }
 
     vm.moveToVault = moveToVaultFn;
     vm.moveToEquip = moveToEquipFn;
     vm.moveToGuardian = moveToGuardianFn;
-    vm.getSimilarItem = getSimilarItemFn;
 
     vm.stores = dimStoreService.getStores();
 
@@ -297,6 +153,10 @@
 
     this.canShowEquip = function canShowButton(item, itemStore, buttonStore) {
       if (buttonStore.id === 'vault') {
+        return false;
+      }
+
+      if (!item.equipment) {
         return false;
       }
 
