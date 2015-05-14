eaa75ee0e51f59dad16a6f2f8841baacdcaa35d5
phatsk <phatsk@gmail.com>
2015-05-14 14:47:39 -0400

    Issue #94, weapon type filters

--- app/scripts/services/dimStoreService.factory.js
+++ app/scripts/services/dimStoreService.factory.js
@@ -276,6 +276,13 @@
         // }
 
         var itemType = getItemType(itemDef.itemTypeName, itemDef.itemName);
+        var specificType = null;
+
+        if(itemType !== null && itemType.hasOwnProperty('general'))
+        {
+          specificType = itemType.specific;
+          itemType = itemType.general;
+        }
 
         if (!itemType) {
           return;
@@ -316,7 +323,8 @@
           classType: itemDef.classType,
           /* 0: titan, 1: hunter, 2: warlock, 3: any */
           dmg: dmgName,
-          visible: true
+          visible: true,
+          specificType: specificType
         };
 
         if (item.itemHash === 2809229973) { // Necrochasm
@@ -388,17 +396,21 @@
       if (name.indexOf("Marks") != -1) {
         return null;
       }
+      var _ret = {
+          general: null,
+          specific: type.toLowerCase().replace(/\s/g, '')
+      };
 
       if (["Pulse Rifle", "Scout Rifle", "Hand Cannon", "Auto Rifle", "Primary Weapon Engram"].indexOf(type) != -1)
-        return 'Primary';
+        _ret.general = 'Primary';
       if (["Sniper Rifle", "Shotgun", "Fusion Rifle", "Special Weapon Engram"].indexOf(type) != -1) {
         // detect special case items that are actually primary weapons.
         if (["Vex Mythoclast", "Universal Remote", "No Land Beyond"].indexOf(name) != -1)
-          return 'Primary';
-        return 'Special';
+          _ret.general = 'Primary';
+        _ret.general = 'Special';
       }
       if (["Rocket Launcher", "Machine Gun", "Heavy Weapon Engram"].indexOf(type) != -1)
-        return 'Heavy';
+        _ret.general = 'Heavy';
       if (["Titan Mark", "Hunter Cloak", "Warlock Bond", "Class Item Engram"].indexOf(type) != -1)
         return 'ClassItem';
       if (["Gauntlet Engram"].indexOf(type) != -1)
@@ -416,6 +428,9 @@
       }
       if (["Armor Shader", "Emblem", "Ghost Shell", "Ship", "Vehicle", "Consumable", "Material"].indexOf(type) != -1)
         return type.split(' ')[0];
+
+      if(_ret.general !== null)
+        return _ret;
 
       return null;
     }
