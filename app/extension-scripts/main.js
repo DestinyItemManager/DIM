7904115d9c3180e59bdb7093f9a0ec38296a9410
Kyle Shay <kyle.shay@enernoc.com>
2015-05-14 13:43:16 -0400

    #117 - use new functionality to open DIM, remove browser history req.

--- app/extension-scripts/main.js
+++ app/extension-scripts/main.js
@@ -1,18 +1,7 @@
 (function () {
-  chrome.browserAction.onClicked.addListener(function (tab) {
-    var optionsUrl = chrome.extension.getURL('index.html');
-    chrome.tabs.query({
-      url: optionsUrl
-    }, function (tabs) {
-      if (tabs.length) {
-        chrome.tabs.update(tabs[0].id, {
-          active: true
-        });
-      } else {
-        chrome.tabs.create({
-          url: optionsUrl
-        });
-      }
+  chrome.browserAction.onClicked.addListener(function () {
+    chrome.runtime.openOptionsPage(function() {
+      // do something on load
     });
   });
 
