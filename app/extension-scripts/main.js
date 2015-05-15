522451b31692d89d86c36726cf3d5005293fea55
SunburnedGoose <rick.casey@gmail.com>
2015-05-15 09:49:43 -0500

    Changed opening strategy to fix incognito mode.

--- app/extension-scripts/main.js
+++ app/extension-scripts/main.js
@@ -1,24 +1,9 @@
 (function () {
   chrome.browserAction.onClicked.addListener(function () {
-    chrome.runtime.openOptionsPage(function() {
-      // do something on load
+    chrome.tabs.create({
+        'url': chrome.extension.getURL('index.html')
+    }, function(tab) {
+
     });
   });
-
-
-  (function (b, o, i, l, e, r) {
-    b.GoogleAnalyticsObject = l;
-    b[l] || (b[l] =
-      function () {
-        (b[l].q = b[l].q || [])
-        .push(arguments)
-      });
-    b[l].l = +new Date;
-    e = o.createElement(i);
-    r = o.getElementsByTagName(i)[0];
-    e.src = '//www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e, r)
-  }(window, document, 'script', 'ga'));
-  ga('create', 'UA-60316581-1', 'auto');
-  ga('send', 'pageview');
 })();
