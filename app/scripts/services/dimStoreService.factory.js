34304ad233a4f33a3f498645652c9a0791c3affc
SunburnedGoose <rick.casey@gmail.com>
2015-05-19 17:44:06 -0500

    Added support for sidearms

--- app/scripts/services/dimStoreService.factory.js
+++ app/scripts/services/dimStoreService.factory.js
@@ -516,7 +516,7 @@
     }
 
     function sortItem(type) {
-      if (["Pulse Rifle", "Sniper Rifle", "Shotgun", "Scout Rifle", "Sidebarm", "Hand Cannon", "Fusion Rifle", "Rocket Launcher", "Auto Rifle", "Machine Gun", "Primary Weapon Engram", "Special Weapon Engram", "Heavy Weapon Engram"].indexOf(type) != -1)
+      if (["Pulse Rifle", "Sniper Rifle", "Shotgun", "Scout Rifle", "Sidearm", "Hand Cannon", "Fusion Rifle", "Rocket Launcher", "Auto Rifle", "Machine Gun", "Primary Weapon Engram", "Special Weapon Engram", "Heavy Weapon Engram"].indexOf(type) != -1)
         return 'Weapons';
       if (["Titan Mark", "Hunter Cloak", "Warlock Bond", "Helmet Engram", "Leg Armor Engram", "Body Armor Engram", "Gauntlet Engram", "Gauntlets", "Helmet", "Chest Armor", "Leg Armor", "Class Item Engram"].indexOf(type) != -1)
         return 'Armor';
