65240374ae492f0e66a2cfd2d14c1b51d15ec810
SunburnedGoose <rick.casey@gmail.com>
2015-05-25 19:23:13 -0500

    Added cache busting to items def.

--- app/scripts/services/dimItemDefinitions.factory.js
+++ app/scripts/services/dimItemDefinitions.factory.js
@@ -9,7 +9,7 @@
   function ItemDefinitions($q, $timeout, $http) {
     var deferred = $q.defer();
 
-    $http.get('scripts/api-manifest/items.json')
+    $http.get('scripts/api-manifest/items.json?v=3.0.8')
       .success(function(data) {
         deferred.resolve(data);
       })
