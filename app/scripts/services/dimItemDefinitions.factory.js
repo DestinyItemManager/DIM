f0df9b4067ce429a48da3343775d9a3cd8b62e63
SunburnedGoose <rick.casey@gmail.com>
2015-06-09 14:25:31 -0500

    Version updated.

--- app/scripts/services/dimItemDefinitions.factory.js
+++ app/scripts/services/dimItemDefinitions.factory.js
@@ -9,7 +9,7 @@
   function ItemDefinitions($q, $timeout, $http) {
     var deferred = $q.defer();
 
-    $http.get('scripts/api-manifest/items.json?v=3.0.11')
+    $http.get('scripts/api-manifest/items.json?v=3.0.12')
       .success(function(data) {
         deferred.resolve(data);
       })
