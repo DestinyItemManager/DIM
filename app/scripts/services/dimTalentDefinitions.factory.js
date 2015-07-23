b06ec3c71b58871562d4fcf38cb6b7f9fbced89d
Rick Casey <ricky.casey@sungard.com>
2015-07-22 23:57:44 -0500

    Updated bungie manifest and version number for DIM.

--- app/scripts/services/dimTalentDefinitions.factory.js
+++ app/scripts/services/dimTalentDefinitions.factory.js
@@ -9,7 +9,7 @@
   function TalentDefinitions($q, $timeout, $http) {
     var deferred = $q.defer();
 
-    $http.get('scripts/api-manifest/talent.json?v=3.0.13')
+    $http.get('scripts/api-manifest/talent.json?v=3.0.14')
       .then(function(data) {
         deferred.resolve(data);
       },
