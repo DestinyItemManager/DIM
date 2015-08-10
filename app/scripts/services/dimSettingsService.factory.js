a6fd7118b3850c6c67cf1ce8367872fe7b777694
Rick Casey <ricky.casey@sungard.com>
2015-08-10 13:53:34 -0500

    Added settings to chrome sync storage.

--- app/scripts/services/dimSettingsService.factory.js
+++ app/scripts/services/dimSettingsService.factory.js
@@ -9,45 +9,62 @@
   function SettingsService($q, $rootScope, uuid2) {
     var settingState;
 
-    loadSettings();
-
     return {
       'getSetting': getSetting,
       'saveSetting': saveSetting
     };
 
     function loadSettings() {
-      settingState = {
-        condensed: false,
-        characterOrder: 'mostRecent'
-        //characterOrder: 'fixed'
-      };
+      return $q(function(resolve, reject) {
+
+        function processStorageSettings(data) {
+          if (_.has(data, "settings-v1.0")) {
+            resolve(data["settings-v1.0"]);
+          } else {
+            resolve({
+              condensed: false,
+              characterOrder: 'mostRecent'
+            });
+          }
+        }
+
+        chrome.storage.sync.get(null, processStorageSettings);
+      });
     }
 
     function getSetting(key) {
-      return $q(function(resolve, reject) {
-        if (_.isUndefined(key)) {
-          resolve(settingState);
-        } else if (_.has(settingState, key)) {
-          resolve(_.propertyOf(settingState)(key));
-        } else {
-          reject([key, undefined]);
-        }
-      });
+      return loadSettings()
+        .then(function(settings) {
+          if (_.isUndefined(key)) {
+            return settings;
+          } else if (_.has(settings, key)) {
+            return _.propertyOf(settings)(key);
+          } else {
+            return $q.reject("The key is not defined in the settings.");
+          }
+        });
     }
 
     function saveSetting(key, value) {
-      return $q(function(resolve) {
-        settingState[key] = value;
+      return getSetting()
+        .then(function(settings) {
+          settings[key] = value;
+          var data = {};
+          var kvp = {};
 
-        var kvp = {};
+          kvp[key] = value;
 
-        kvp[key] = value;
+          data["settings-v1.0"] = settings;
 
-        $rootScope.$broadcast('dim-settings-updated', kvp);
-
-        resolve([key, value]);
-      });
+          chrome.storage.sync.set(data, function(e) {
+            if (chrome.runtime.lastError) {
+              $q.reject(chrome.runtime.lastError);
+            } else {
+              $rootScope.$broadcast('dim-settings-updated', kvp);
+              return true;
+            }
+          });
+        });
     }
   }
 })();
