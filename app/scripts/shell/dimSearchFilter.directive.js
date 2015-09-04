db2b9bce95832bfc78c1cbebdd12db84c7906250
Miguel Pasadinhas <miguel.pasadinhas@tecnico.ulisboa.pt>
2015-09-04 16:16:17 +0100

    Add is:unique filter

--- app/scripts/shell/dimSearchFilter.directive.js
+++ app/scripts/shell/dimSearchFilter.directive.js
@@ -139,6 +139,7 @@
       'upgraded':     ['upgraded'],
       'classType':    ['titan', 'hunter', 'warlock'],
       'dupe':         ['dupe', 'duplicate'],
+      'unique':       ['unique'],
       'unascended':   ['unascended', 'unassended', 'unasscended'],
       'ascended':     ['ascended', 'assended', 'asscended'],
       'locked':       ['locked'],
@@ -221,6 +222,27 @@
         }
 
         return _.some(_duplicates.dupes, function(hash) { return item.hash === hash; });
+      },
+      'unique': function() {
+          if (!_duplicates.hasOwnProperty('dupes')) {
+          var allItems = _.chain(dimStoreService.getStores())
+            .map(function(store) {
+              return store.items;
+            })
+          .flatten()
+            .sortBy('hash')
+            .value();
+
+          _duplicates.dupes = [];
+
+          for (var i = 0; i < allItems.length - 1; i++) {
+            if (allItems[i + 1].hash == allItems[i].hash) {
+              _duplicates.dupes.push(allItems[i].hash);
+            }
+          }
+        }
+
+        return _.all(_duplicates.dupes, function(hash) { return item.hash !== hash; });
       },
       'classType': function(predicate, item) {
         var value;
