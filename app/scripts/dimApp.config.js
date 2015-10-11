8cf32db754b00294119736c6f715409d10125e4b
Kyle Shay <kyle.shay@ymail.com>
2015-10-11 09:44:54 -0400

    3.1.13 version bump

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -188,7 +188,7 @@ if (typeof window.onerror == "object") {
 
       _gaq.push([
         'errorTracker._trackEvent',
-        'DIM - Chrome Extension - v3.1.12.2',
+        'DIM - Chrome Extension - v3.1.13',
         exceptionDescription,
         ' @ ' + url + ':' + lineNumber + ':' + columnNumber,
         0,
