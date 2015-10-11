1459bf687b9e381051348684ead6ddf35412309d
Kyle Shay <kyle.shay@enernoc.com>
2015-10-11 16:31:51 -0400

    version bump.

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -188,7 +188,7 @@ if (typeof window.onerror == "object") {
 
       _gaq.push([
         'errorTracker._trackEvent',
-        'DIM - Chrome Extension - v3.1.13.1',
+        'DIM - Chrome Extension - v3.1.13.2',
         exceptionDescription,
         ' @ ' + url + ':' + lineNumber + ':' + columnNumber,
         0,
--- app/scripts/services/dimStatDefinitions.factory.js
+++ app/scripts/services/dimStatDefinitions.factory.js
@@ -9,7 +9,7 @@
   function StatDefinitions($q, $timeout, $http) {
     var deferred = $q.defer();
 
-    $http.get('scripts/api-manifest/stats.json?v=3.1.13.1')
+    $http.get('scripts/api-manifest/stats.json?v=3.1.13.2')
       .success(function(data) {
         deferred.resolve(data);
       })
