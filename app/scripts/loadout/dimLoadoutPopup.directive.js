75e0e3eec18790a3533e1139d2590531a6cef0f1
Ben Hollis <ben@benhollis.net>
2015-10-11 18:55:20 -0700

    Add a dynamic layout for each character that will maximize their light level.
    This will figure out, across all items in the account, what combination of items will maximize
    light for the given character, and equip them as if it were a loadout. It will avoid changing
    already equipped equipment if it wouldn't make a difference. It also correctly handles exotics.

--- app/scripts/loadout/dimLoadoutPopup.directive.js
+++ app/scripts/loadout/dimLoadoutPopup.directive.js
@@ -26,14 +26,17 @@
         '      <span class="button-edit" ng-click="vm.editLoadout(loadout, $event)"><i class="fa fa-pencil"></i></span>',
         '    </div>',
         '  </div>',
+        '  <div class="loadout-list"><div class="loadout-set">',
+        '    <span class="button-name button-random-name" ng-click="vm.maxLightLoadout($event)"><i class="fa fa-star"></i> Maximize Light</span>',
+        '  </div></div>',
         '</div>'
       ].join('')
     };
   }
 
-  LoadoutPopupCtrl.$inject = ['$rootScope', 'ngDialog', 'dimLoadoutService', 'dimItemService', 'toaster', '$q', 'dimStoreService'];
+  LoadoutPopupCtrl.$inject = ['$rootScope', 'ngDialog', 'dimLoadoutService', 'dimItemService', 'toaster', '$q', 'dimStoreService', 'dimItemTier'];
 
-  function LoadoutPopupCtrl($rootScope, ngDialog, dimLoadoutService, dimItemService, toaster, $q, dimStoreService) {
+  function LoadoutPopupCtrl($rootScope, ngDialog, dimLoadoutService, dimItemService, toaster, $q, dimStoreService, dimItemTier) {
     var vm = this;
 
     vm.classTypeId = -1;
@@ -113,6 +116,89 @@
         .value();
 
       applyLoadoutItems(items, loadout, _items, scope);
+    };
+
+    // Apply a loadout that's dynamically calculated to maximize Light level (preferring not to change currently-equipped items)
+    vm.maxLightLoadout = function maxLightLoadout($event) {
+      // These types contribute to light level
+      var lightTypes = ['Primary', 'Special', 'Heavy', 'Helmet', 'Gauntlets', 'Chest', 'Leg', 'ClassItem', 'Artifact', 'Ghost'];
+
+      var applicableItems = _.select(dimItemService.getItems(), function(i) {
+        return i.equipment &&
+          i.primStat !== undefined &&
+          (i.classTypeName === 'unknown' || i.classTypeName === vm.classType) &&
+          _.contains(lightTypes, i.type);
+      });
+      var itemsByType = _.groupBy(applicableItems, 'type');
+
+      var bestItemFn = function(item) {
+        // Add .1 to the value if equipped, to break ties and avoid changing currently-equipped items out
+        // for other stuff with the same level.
+        return item.primStat.value + (item.equipped ? 0.1 : 0);
+      };
+      var isExotic = function(item) {
+        return item.tier === dimItemTier.exotic;
+      };
+
+      // Pick the best item by primary stat
+      var items = {};
+      _.each(lightTypes, function(type) {
+        items[type] = _.max(itemsByType[type], bestItemFn);
+      });
+
+      // Solve for the case where our optimizer decided to equip two exotics
+      var exoticGroups = [ ['Primary', 'Special', 'Heavy'], ['Helmet', 'Gauntlets', 'Chest', 'Leg'] ];
+      _.each(exoticGroups, function(group) {
+        var itemsInGroup = _.pick(items, group);
+        var numExotics = _.select(_.values(itemsInGroup), isExotic).length;
+        if (numExotics > 1) {
+          var options = [];
+
+          // Generate an option where we use each exotic
+          _.each(itemsInGroup, function(item, type) {
+            if (isExotic(item)) {
+              var option = angular.copy(itemsInGroup);
+              var optionValid = true;
+              // Switch the other exotic items to the next best non-exotic
+              _.each(_.omit(itemsInGroup, type), function(otherItem, otherType) {
+                if (isExotic(otherItem)) {
+                  var nonExotics = _.reject(itemsByType[type], isExotic);
+                  if (_.isEmpty(nonExotics)) {
+                    // this option isn't usable because we couldn't swap this exotic for any non-exotic
+                    optionValid = false;
+                  } else {
+                    option[otherType] = _.max(nonExotics, bestItemFn);
+                  }
+                }
+              });
+
+              if (optionValid) {
+                options.push(option);
+              }
+            }
+          });
+
+          // Pick the option where the primary stats add up to the biggest number, again favoring equipped stuff
+          var bestOption = _.max(options, function(opt) { return _.reduce(_.values(opt), function(sum, i) { return bestItemFn(i) + sum; }, 0) });
+          _.assign(items, bestOption);
+        }
+      });
+
+      // Copy the items and mark them "equipped" and put them in arrays, so they look like a loadout
+      var finalItems = {};
+      _.each(items, function(item, type) {
+        var itemCopy = angular.copy(item);
+        itemCopy.equipped = true;
+        finalItems[type.toLowerCase()] = [ itemCopy ];
+      });
+
+      var loadout = {
+        classType: -1,
+        name: 'Maximize Light',
+        items: finalItems
+      };
+
+      vm.applyLoadout(loadout, $event);
     };
 
     function applyLoadoutItems(items, loadout, _items, scope) {
