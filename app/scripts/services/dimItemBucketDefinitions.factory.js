c12ea79009916c2d2d542380cb50d64ac4770502
Casey, Ricky <ricky.casey@sungard.com>
2015-11-22 22:30:02 -0600

    Updated manifest file.  Removed majority of images because they were unnecessary.  Only save images that mutate when manifest file is updated.

--- app/scripts/services/dimItemBucketDefinitions.factory.js
+++ app/scripts/services/dimItemBucketDefinitions.factory.js
@@ -9,7 +9,7 @@
   function ItemBucketDefinitions($q, $timeout, $http) {
     var deferred = $q.defer();
 
-    $http.get('scripts/api-manifest/buckets.json?v=3.1.17.2')
+    $http.get('scripts/api-manifest/buckets.json?v=3.1.18')
       .success(function(data) {
         deferred.resolve(data);
       })
