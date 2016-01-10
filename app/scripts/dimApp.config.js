e9877caf753a75a8b12cd8844e8dc880ffbce22e
Rick Casey <ricky.casey@sungard.com>
2016-01-10 12:35:58 -0600

    Updated version number to v3.2.0

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -188,7 +188,7 @@ if (typeof window.onerror == "object") {
       //
       // _gaq.push([
       //   'errorTracker._trackEvent',
-      //   'DIM - Chrome Extension - v3.1.24',
+      //   'DIM - Chrome Extension - v3.2.0',
       //   exceptionDescription,
       //   ' @ ' + url + ':' + lineNumber + ':' + columnNumber,
       //   0,
