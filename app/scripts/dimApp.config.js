9be3a16bff796080b443af28c50bbe56e9c18fa1
Ben Hollis <ben@benhollis.net>
2016-02-01 01:27:48 -0800

    We do not need the verge library.

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -206,21 +206,19 @@
     });
 })();
 
-$(document)
-  .ready(function() {
-    if(verge.viewportW() !== $(window)
-      .width()) {
-      $('body')
-        .addClass('pad-margin');
-      var style = document.createElement('style');
-      style.type = 'text/css';
-      style.innerHTML = '.about.ngdialog-open.pad-margin #header, .app-settings.ngdialog-open.pad-margin #header, .support.ngdialog-open.pad-margin #header, .filters.ngdialog-open.pad-margin #header { padding-right: ' + (verge.viewportW() - $(window)
-        .width()) + 'px; }';
-      document.getElementsByTagName('head')[0].appendChild(style);
-    }
-  });
+$(document).ready(function() {
+  var viewportWidth = Math.max(document.documentElement.clientWidth,
+                               document.documentElement.innerWidth);
+  if (viewportWidth !== $(window).width()) {
+    $('body').addClass('pad-margin');
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.about.ngdialog-open.pad-margin #header, .app-settings.ngdialog-open.pad-margin #header, .support.ngdialog-open.pad-margin #header, .filters.ngdialog-open.pad-margin #header { padding-right: ' + (viewportWidth - $(window).width()) + 'px; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }
+});
 
-if(typeof window.onerror == "object") {
+if (typeof window.onerror == "object") {
   window.onerror = function(err, url, line) {};
 }
 
