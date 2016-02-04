6a4686028919eb81e2cfc9f446fb9868c4a8cf5e
Rick Casey <ricky.casey@sungard.com>
2016-02-04 15:34:48 -0600

    Added toggle for styles and js.

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -53,88 +53,121 @@
 
   angular.module('dimApp')
     .run(['$rootScope', 'loadingTracker', '$cookies', '$timeout', 'toaster',
-          function($rootScope, loadingTracker, $cookies, $timeout, toaster) {
-      $rootScope.loadingTracker = loadingTracker;
+      function($rootScope, loadingTracker, $cookies, $timeout, toaster) {
+        $rootScope.loadingTracker = loadingTracker;
 
-      //1 Hour
-      $rootScope.inactivityLength = 60 * 60 * 1000;
+        //1 Hour
+        $rootScope.inactivityLength = 60 * 60 * 1000;
 
-      $rootScope.isUserInactive = function() {
-        var currentTime = Date.now();
+        $rootScope.isUserInactive = function() {
+          var currentTime = Date.now();
 
-        //Has This User Been Inactive For More Than An Hour
-        return ((currentTime) - $rootScope.lastActivity) > $rootScope.inactivityLength;
-      };
+          //Has This User Been Inactive For More Than An Hour
+          return((currentTime) - $rootScope.lastActivity) > $rootScope.inactivityLength;
+        };
 
-      $rootScope.trackActivity = function() {
-        $rootScope.lastActivity = Date.now();
-      };
+        $rootScope.trackActivity = function() {
+          $rootScope.lastActivity = Date.now();
+        };
 
-      //Track Our Initial Activity of Starting the App
-      $rootScope.trackActivity();
+        //Track Our Initial Activity of Starting the App
+        $rootScope.trackActivity();
 
-      // chrome.storage.sync.get('2015.09.02-Blacksmith', function(data) {
-      //   if (_.isNull(data) || _.isEmpty(data)) {
-      //     $timeout(function() {
-      //       toaster.pop({
-      //         type: 'info',
-      //         title: 'Blacksmith Shader Giveaway',
-      //         body: '<p>The DIM team is giving away six Blacksmith shaders on Twitter to celebrate your #YearOneGear.</p><p>Visit us at <a href="https://twitter.com/ThisIsDIM/status/639237265944899584" target="_blank">@ThisIsDIM</a> on twitter or the <a href="https://www.reddit.com/r/DestinyItemManager/comments/3jfl0f/blacksmith_shader_giveaway/" target="_blank">/r/destinyitemmanager</a> subreddit to learn how to enter.</p><p>See you starside Guardians.</p><p><input style="margin-top: 1px; vertical-align: middle;" id="20150902Checkbox" type="checkbox"> <label for="20150902Checkbox">Hide This Popup</label></p>',
-      //         timeout: 0,
-      //         bodyOutputType: 'trustedHtml',
-      //         showCloseButton: true,
-      //         clickHandler: function(a,b,c,d,e,f,g) {
-      //           if (b) {
-      //             return true;
-      //           }
-      //
-      //           return false;
-      //         },
-      //         onHideCallback: function() {
-      //           if ($('#20150902Checkbox').is(':checked')) {
-      //             chrome.storage.sync.set({
-      //               "2015.09.02-Blacksmith": 1
-      //             }, function(e) {});
-      //           }
-      //         }
-      //       });
-      //     }, 3000);
-      //   }
-      // });
+        // chrome.storage.sync.get('2015.09.02-Blacksmith', function(data) {
+        //   if (_.isNull(data) || _.isEmpty(data)) {
+        //     $timeout(function() {
+        //       toaster.pop({
+        //         type: 'info',
+        //         title: 'Blacksmith Shader Giveaway',
+        //         body: '<p>The DIM team is giving away six Blacksmith shaders on Twitter to celebrate your #YearOneGear.</p><p>Visit us at <a href="https://twitter.com/ThisIsDIM/status/639237265944899584" target="_blank">@ThisIsDIM</a> on twitter or the <a href="https://www.reddit.com/r/DestinyItemManager/comments/3jfl0f/blacksmith_shader_giveaway/" target="_blank">/r/destinyitemmanager</a> subreddit to learn how to enter.</p><p>See you starside Guardians.</p><p><input style="margin-top: 1px; vertical-align: middle;" id="20150902Checkbox" type="checkbox"> <label for="20150902Checkbox">Hide This Popup</label></p>',
+        //         timeout: 0,
+        //         bodyOutputType: 'trustedHtml',
+        //         showCloseButton: true,
+        //         clickHandler: function(a,b,c,d,e,f,g) {
+        //           if (b) {
+        //             return true;
+        //           }
+        //
+        //           return false;
+        //         },
+        //         onHideCallback: function() {
+        //           if ($('#20150902Checkbox').is(':checked')) {
+        //             chrome.storage.sync.set({
+        //               "2015.09.02-Blacksmith": 1
+        //             }, function(e) {});
+        //           }
+        //         }
+        //       });
+        //     }, 3000);
+        //   }
+        // });
 
-      chrome.storage.sync.get('2016.01.18-Perf', function(data) {
-        if (_.isNull(data) || _.isEmpty(data)) {
-          $timeout(function() {
-            toaster.pop({
-              type: 'info',
-              title: 'v3.2 Released',
-              body: [
-'<p>We\'ve added a \'Maximum Light\' loadout, greatly improved the performance while moving items, and added <span style="font-style: italic">is:year1</span> and <span style="font-style: italic">is:year2</span> filters.',
-'<p><a href="http://bit.ly/1SssIq9" target="_blank">Learn more about the the updates in v3.2.</a>',
-'<p>Follow us on: <a style="margin: 0 5px;" href="http://destinyitemmanager.reddit.com" target="_blank"><img title="/r/DIM on Reddit" style="vertical-align: text-bottom;" src="images/reddit.png"></a> <a style="margin: 0 5px;" href="http://twitter.com/ThisIsDIM" target="_blank"><img style="vertical-align: text-bottom;" title="@ThisIsDIM on Twitter" src="images/twitter.png"></a>',
-'<p><input style="margin-top: 1px; vertical-align: middle;" id="20151010Checkbox" type="checkbox"> <label for="20151010Checkbox">Hide This Popup</label></p>'].join(''),
-              timeout: 0,
-              bodyOutputType: 'trustedHtml',
-              showCloseButton: true,
-              clickHandler: function(a,b,c,d,e,f,g) {
-                if (b) {
-                  return true;
+        chrome.storage.sync.get('20160204CrimsonTheme', function(data) {
+          if(_.isNull(data) || _.isEmpty(data) || data['20160204CrimsonTheme'] === 1) {
+            $('body')
+              .addClass('crimson');
+            $(document)
+              .ready(function() {
+                $(document)
+                  .octoberLeaves({
+                    leafStyles: 3, // Number of leaf styles in the sprite (leaves.png)
+                    speedC: 2, // Speed of leaves
+                    rotation: 1, // Define rotation of leaves
+                    rotationTrue: 1, // Whether leaves rotate (1) or not (0)
+                    numberOfLeaves: 15, // Number of leaves
+                    size: 20, // General size of leaves, final size is calculated randomly (with this number as general parameter)
+                    cycleSpeed: 30 // <a href="http://www.jqueryscript.net/animation/">Animation</a> speed (Inverse frames per second) (10-100)
+                  })
+              });
+          }
+        });
+
+        chrome.storage.sync.get('2016.02.04-Crimson', function(data) {
+          if(_.isNull(data) || _.isEmpty(data)) {
+            $timeout(function() {
+              toaster.pop({
+                type: 'info',
+                title: 'Crimson Days, February 9th - 16th',
+                body: [
+                  '<p>We\'ve been looking forward to the Crimson Days. Our only bit of advice, <a href="http://i.imgur.com/5HrbN28.gif" target="_blank">never leave your wingman.</a>',
+                  '<p>If you want to use the old theme, there is a toggle next to the \'Support DIM\' donation link in the header.',
+                  '<p>Follow us on: <a style="margin: 0 5px;" href="http://destinyitemmanager.reddit.com" target="_blank"><img title="/r/DIM on Reddit" style="vertical-align: text-bottom;" src="images/reddit.png"></a> <a style="margin: 0 5px;" href="http://twitter.com/ThisIsDIM" target="_blank"><img style="vertical-align: text-bottom;" title="@ThisIsDIM on Twitter" src="images/twitter.png"></a>',
+                  '<p><input style="margin-top: 1px; vertical-align: middle;" id="20160204Crimson" type="checkbox"> <label for="20160204Crimson">Hide This Popup</label></p>'
+                ].join(''),
+                timeout: 0,
+                bodyOutputType: 'trustedHtml',
+                showCloseButton: true,
+                clickHandler: function(a, b, c, d, e, f, g) {
+                  if(b) {
+                    return true;
+                  }
+
+                  return false;
+                },
+                onHideCallback: function() {
+                  if($('#20160204Crimson')
+                    .is(':checked')) {
+                    chrome.storage.sync.set({
+                      "2016.02.04-Crimson": 1
+                    }, function(e) {});
+                  }
+                  if($('#20160204CrimsonTheme')
+                    .is(':checked')) {
+                    $('body')
+                      .removeClass('crimson');
+                    $(document)
+                      .octoberLeaves('stop');
+                    chrome.storage.sync.set({
+                      "20160204CrimsonTheme": 1
+                    }, function(e) {});
+                  }
                 }
-
-                return false;
-              },
-              onHideCallback: function() {
-                if ($('#20151010Checkbox').is(':checked')) {
-                  chrome.storage.sync.set({
-                    "2016.01.18-Perf": 1
-                  }, function(e) {});
-                }
-              }
-            });
-          }, 3000);
-        }
-      });
-    }]);
+              });
+            }, 3000);
+          }
+        });
+      }
+    ]);
 
   angular.module('dimApp')
     .config([
@@ -147,7 +180,9 @@
       '$compileProvider',
       function($compileProvider) {
         var currentImgSrcSanitizationWhitelist = $compileProvider.imgSrcSanitizationWhitelist();
-        var newImgSrcSanitizationWhiteList = currentImgSrcSanitizationWhitelist.toString().slice(0, -1) + '|chrome-extension:' + currentImgSrcSanitizationWhitelist.toString().slice(-1);
+        var newImgSrcSanitizationWhiteList = currentImgSrcSanitizationWhitelist.toString()
+          .slice(0, -1) + '|chrome-extension:' + currentImgSrcSanitizationWhitelist.toString()
+          .slice(-1);
 
         //console.log("Changing imgSrcSanitizationWhiteList from " + currentImgSrcSanitizationWhitelist + " to " + newImgSrcSanitizationWhiteList);
         $compileProvider.imgSrcSanitizationWhitelist(newImgSrcSanitizationWhiteList);
@@ -171,17 +206,21 @@
     });
 })();
 
-$(document).ready(function() {
-  if (verge.viewportW() !== $(window).width()) {
-    $('body').addClass('pad-margin');
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = '.about.ngdialog-open.pad-margin #header, .app-settings.ngdialog-open.pad-margin #header, .support.ngdialog-open.pad-margin #header, .filters.ngdialog-open.pad-margin #header { padding-right: ' + (verge.viewportW() - $(window).width()) + 'px; }';
-    document.getElementsByTagName('head')[0].appendChild(style);
-  }
-});
+$(document)
+  .ready(function() {
+    if(verge.viewportW() !== $(window)
+      .width()) {
+      $('body')
+        .addClass('pad-margin');
+      var style = document.createElement('style');
+      style.type = 'text/css';
+      style.innerHTML = '.about.ngdialog-open.pad-margin #header, .app-settings.ngdialog-open.pad-margin #header, .support.ngdialog-open.pad-margin #header, .filters.ngdialog-open.pad-margin #header { padding-right: ' + (verge.viewportW() - $(window)
+        .width()) + 'px; }';
+      document.getElementsByTagName('head')[0].appendChild(style);
+    }
+  });
 
-if (typeof window.onerror == "object") {
+if(typeof window.onerror == "object") {
   window.onerror = function(err, url, line) {};
 }
 
@@ -190,22 +229,22 @@ if (typeof window.onerror == "object") {
   var originalWindowErrorCallback = window.onerror;
 
   window.onerror = function customErrorHandler(errorMessage, url, lineNumber, columnNumber, errorObject) {
-      var exceptionDescription = errorMessage;
-      if (typeof errorObject !== 'undefined' && typeof errorObject.message !== 'undefined') {
-        exceptionDescription = errorObject.message;
-      }
-      //
-      // _gaq.push([
-      //   'errorTracker._trackEvent',
-      //   'DIM - Chrome Extension - v3.2.0',
-      //   exceptionDescription,
-      //   ' @ ' + url + ':' + lineNumber + ':' + columnNumber,
-      //   0,
-      //   true
-      // ]);
+    var exceptionDescription = errorMessage;
+    if(typeof errorObject !== 'undefined' && typeof errorObject.message !== 'undefined') {
+      exceptionDescription = errorObject.message;
+    }
+    //
+    // _gaq.push([
+    //   'errorTracker._trackEvent',
+    //   'DIM - Chrome Extension - v3.2.0',
+    //   exceptionDescription,
+    //   ' @ ' + url + ':' + lineNumber + ':' + columnNumber,
+    //   0,
+    //   true
+    // ]);
 
     // If the previous "window.onerror" callback can be called, pass it the data:
-    if (typeof originalWindowErrorCallback === 'function') {
+    if(typeof originalWindowErrorCallback === 'function') {
       return originalWindowErrorCallback(errorMessage, url, lineNumber, columnNumber, errorObject);
     }
     // Otherwise, Let the default handler run:
--- app/scripts/shell/dimAppCtrl.controller.js
+++ app/scripts/shell/dimAppCtrl.controller.js
@@ -1,7 +1,8 @@
 (function() {
   'use strict';
 
-  angular.module('dimApp').controller('dimAppCtrl', DimApp);
+  angular.module('dimApp')
+    .controller('dimAppCtrl', DimApp);
 
   DimApp.$inject = ['ngDialog', '$rootScope', 'loadingTracker', 'dimPlatformService', 'dimStoreService', '$interval', 'hotkeys', '$timeout', 'dimStoreService'];
 
@@ -10,7 +11,7 @@
     var aboutResult = null;
     var settingResult = null;
     var supportResult = null;
-    var filterResult  = null;
+    var filterResult = null;
 
     hotkeys.add({
       combo: ['f'],
@@ -47,7 +48,7 @@
     vm.showSetting = function(e) {
       e.stopPropagation();
 
-      if (!_.isNull(settingResult)) {
+      if(!_.isNull(settingResult)) {
         settingResult.close();
       } else {
         ngDialog.closeAll();
@@ -56,21 +57,58 @@
           template: 'views/setting.html',
           overlay: false,
           className: 'app-settings',
-          scope: $('body > div').scope()
+          scope: $('body > div')
+            .scope()
         });
-        $('body').addClass('app-settings');
+        $('body')
+          .addClass('app-settings');
 
         settingResult.closePromise.then(function() {
           settingResult = null;
-          $('body').removeClass('app-settings');
+          $('body')
+            .removeClass('app-settings');
         });
       }
     };
 
+    vm.toggleCrimsonDays = function() {
+      if(!$('body')
+        .hasClass('crimson')) {
+        $('body')
+          .addClass('crimson');
+        $(document)
+          .octoberLeaves({
+            leafStyles: 3, // Number of leaf styles in the sprite (leaves.png)
+            speedC: 2, // Speed of leaves
+            rotation: 1, // Define rotation of leaves
+            rotationTrue: 1, // Whether leaves rotate (1) or not (0)
+            numberOfLeaves: 15, // Number of leaves
+            size: 20, // General size of leaves, final size is calculated randomly (with this number as general parameter)
+            cycleSpeed: 30 // <a href="http://www.jqueryscript.net/animation/">Animation</a> speed (Inverse frames per second) (10-100)
+          });
+        $(document)
+          .octoberLeaves('start');
+        chrome.storage.sync.set({
+          "20160204CrimsonTheme": 1
+        }, function(e) {});
+      } else {
+        $('body')
+          .removeClass('crimson');
+        $(document)
+          .ready(function() {
+            $(document)
+              .octoberLeaves('stop');
+          });
+        chrome.storage.sync.set({
+          "20160204CrimsonTheme": 0
+        }, function(e) {});
+      }
+    }
+
     vm.showAbout = function(e) {
       e.stopPropagation();
 
-      if (!_.isNull(aboutResult)) {
+      if(!_.isNull(aboutResult)) {
         aboutResult.close();
       } else {
         ngDialog.closeAll();
@@ -79,21 +117,26 @@
           template: 'views/about.html',
           overlay: false,
           className: 'about',
-          scope: $('body > div').scope()
+          scope: $('body > div')
+            .scope()
         });
-        $('body').addClass('about');
+        $('body')
+          .addClass('about');
 
         aboutResult.closePromise.then(function() {
           aboutResult = null;
-          $('body').removeClass('about');
+          $('body')
+            .removeClass('about');
         });
       }
     };
 
     vm.refresh = function refresh() {
       (function(activePlatform) {
-        if (!_.isNull(activePlatform)) {
-          $rootScope.$broadcast('dim-active-platform-updated', { platform: activePlatform });
+        if(!_.isNull(activePlatform)) {
+          $rootScope.$broadcast('dim-active-platform-updated', {
+            platform: activePlatform
+          });
         }
       })(dimPlatformService.getActive());
     };
@@ -101,7 +144,7 @@
     vm.showSupport = function(e) {
       e.stopPropagation();
 
-      if (!_.isNull(supportResult)) {
+      if(!_.isNull(supportResult)) {
         supportResult.close();
       } else {
         ngDialog.closeAll();
@@ -110,13 +153,16 @@
           template: 'views/support.html',
           overlay: false,
           className: 'support',
-          scope: $('body > div').scope()
+          scope: $('body > div')
+            .scope()
         });
-        $('body').addClass('support');
+        $('body')
+          .addClass('support');
 
         supportResult.closePromise.then(function() {
           supportResult = null;
-          $('body').removeClass('support');
+          $('body')
+            .removeClass('support');
         });
       }
     };
@@ -124,7 +170,7 @@
     vm.showFilters = function(e) {
       e.stopPropagation();
 
-      if (!_.isNull(filterResult)) {
+      if(!_.isNull(filterResult)) {
         filterResult.close();
       } else {
         ngDialog.closeAll();
@@ -133,31 +179,35 @@
           template: 'views/filters.html',
           overlay: false,
           className: 'filters',
-          scope: $('body > div').scope()
+          scope: $('body > div')
+            .scope()
         });
-        $('body').addClass('filters');
+        $('body')
+          .addClass('filters');
 
         setTimeout(function() {
-          var spans = $('#filter-view span').each(function() {
-            var item = $(this);
-            var text = item.text();
+          var spans = $('#filter-view span')
+            .each(function() {
+              var item = $(this);
+              var text = item.text();
 
-            item.click(function() {
-              addFilter(text);
+              item.click(function() {
+                addFilter(text);
+              });
             });
-          });
           //<span onclick="addFilter('is:arc')">
         }, 250);
 
         filterResult.closePromise.then(function() {
           filterResult = null;
-          $('body').removeClass('filters');
+          $('body')
+            .removeClass('filters');
         });
       }
     };
 
     vm.closeLoadoutPopup = function closeLoadoutPopup() {
-      if (!_.isNull(aboutResult) || !_.isNull(settingResult) || !_.isNull(supportResult) || !_.isNull(filterResult)) {
+      if(!_.isNull(aboutResult) || !_.isNull(settingResult) || !_.isNull(supportResult) || !_.isNull(filterResult)) {
         ngDialog.closeAll();
       }
     };
@@ -165,29 +215,29 @@
     // Don't refresh more than once a minute
     var refresh = _.throttle(vm.refresh, 60 * 1000);
 
-    vm.startAutoRefreshTimer = function () {
+    vm.startAutoRefreshTimer = function() {
       var secondsToWait = 360;
 
-      $rootScope.autoRefreshTimer = $interval(function () {
-       //Only Refresh If We're Not Already Doing Something
-       //And We're Not Inactive
-       if (!loadingTracker.active() && !$rootScope.isUserInactive() && document.visibilityState == 'visible') {
-         refresh();
-       }
+      $rootScope.autoRefreshTimer = $interval(function() {
+        //Only Refresh If We're Not Already Doing Something
+        //And We're Not Inactive
+        if(!loadingTracker.active() && !$rootScope.isUserInactive() && document.visibilityState == 'visible') {
+          refresh();
+        }
       }, secondsToWait * 1000);
     };
 
     vm.startAutoRefreshTimer();
 
     $rootScope.$on('dim-settings-updated', function(event, arg) {
-      if (_.has(arg, 'characterOrder')) {
+      if(_.has(arg, 'characterOrder')) {
         refresh();
       }
     });
 
     // Refresh when the user comes back to the page
     document.addEventListener("visibilitychange", function() {
-      if (!loadingTracker.active() && !$rootScope.isUserInactive() && document.visibilityState == 'visible') {
+      if(!loadingTracker.active() && !$rootScope.isUserInactive() && document.visibilityState == 'visible') {
         refresh();
       }
     }, false);
@@ -199,13 +249,13 @@ function addFilter(filter) {
   var input = $('input[name=filter]');
   var itemNameFilter = false;
 
-  if (filter === 'item name') {
+  if(filter === 'item name') {
     itemNameFilter = true;
     filter = prompt("Enter an item name:");
     filter = filter.trim();
   }
 
-  if (filter.indexOf('light:') == 0) {
+  if(filter.indexOf('light:') == 0) {
     var lightFilterType = filter.substring(6);
     var light = prompt("Enter a light value:");
     if(light) {
@@ -214,7 +264,7 @@ function addFilter(filter) {
       return;
     }
     filter = 'light:';
-    switch (lightFilterType) {
+    switch(lightFilterType) {
       case 'value':
         filter += light;
         break;
@@ -239,10 +289,11 @@ function addFilter(filter) {
   var text = input.val();
 
 
-  if (itemNameFilter) {
+  if(itemNameFilter) {
     input.val(filter + ((text.length > 0) ? ' ' + text : ''));
-  } else if ((text + ' ').indexOf(filter + ' ') < 0) {
-    if (text.length > 0) {
+  } else if((text + ' ')
+    .indexOf(filter + ' ') < 0) {
+    if(text.length > 0) {
       input.val(text + ' ' + filter);
     } else {
       input.val(filter);
