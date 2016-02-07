27b3427be461023c47a74c45eeb0f127d59765e8
Ben Hollis <ben@benhollis.net>
2016-02-06 16:39:09 -0800

    Move functionality for applying loadouts to dimLoadoutService.

--- app/scripts/services/dimLoadoutService.factory.js
+++ app/scripts/services/dimLoadoutService.factory.js
@@ -4,18 +4,20 @@
   angular.module('dimApp')
     .factory('dimLoadoutService', LoadoutService);
 
-  LoadoutService.$inject = ['$q', '$rootScope', 'uuid2', 'dimItemService'];
+  LoadoutService.$inject = ['$q', '$rootScope', 'uuid2', 'dimItemService', 'dimStoreService', 'toaster', 'loadingTracker'];
 
-  function LoadoutService($q, $rootScope, uuid2, dimItemService) {
+  function LoadoutService($q, $rootScope, uuid2, dimItemService, dimStoreService, toaster, loadingTracker) {
     var _loadouts = [];
 
     return {
-      'dialogOpen': false,
-      'getLoadouts': getLoadouts,
-      'deleteLoadout': deleteLoadout,
-      'saveLoadouts': saveLoadouts,
-      'saveLoadout': saveLoadout,
-      'addItemToLoadout': addItemToLoadout
+      dialogOpen: false,
+      getLoadouts: getLoadouts,
+      deleteLoadout: deleteLoadout,
+      saveLoadouts: saveLoadouts,
+      saveLoadout: saveLoadout,
+      addItemToLoadout: addItemToLoadout,
+      applyLoadout: applyLoadout,
+      previousLoadouts: {} // by character ID
     };
 
     function addItemToLoadout(item) {
@@ -176,10 +178,154 @@
         'v2.0': hydratev2d0,
         'v3.0': hydratev3d0,
         'default': hydratev3d0
-      }
+      };
 
       // v1.0 did not have a 'version' property so if it fails, we'll assume.
       return (hydration[(loadout.version)] || hydration['v1.0'])(loadout);
+    }
+
+    function applyLoadout(store, loadout) {
+      var scope = {
+        failed: false
+      };
+
+      var items = _.chain(loadout.items)
+        .values()
+        .flatten()
+        .value();
+
+      var _types = _.chain(items)
+        .pluck('type')
+        .uniq()
+        .value();
+
+      var _items = _.chain(store.items)
+        .filter(function(item) {
+          return _.contains(_types, item.type);
+        })
+        .filter(function(item) {
+          return (!_.some(items, function(i) {
+            return ((i.id === item.id) && (i.hash === item.hash));
+          }));
+        })
+        .groupBy(function(item) {
+          return item.type;
+        })
+        .value();
+
+      applyLoadoutItems(store, items, loadout, _items, scope);
+    }
+
+    function applyLoadoutItems(store, items, loadout, _items, scope) {
+      if (items.length > 0) {
+        var pseudoItem = items.splice(0, 1)[0];
+        var item = dimItemService.getItem(pseudoItem);
+
+        if (item.type === 'Class') {
+          item = _.findWhere(store.items, {
+            hash: pseudoItem.hash
+          });
+        }
+
+        if (item) {
+          var size = _.chain(store.items)
+            .filter(function(i) {
+              return item.type === i.type;
+            })
+            .size()
+            .value();
+
+          var p = $q.when(item);
+
+          var target;
+
+          if (size === 10) {
+            if (item.owner !== store.id) {
+              var moveItem = _items[item.type].splice(0, 1);
+              p = $q.when(dimStoreService.getStores())
+                .then(function(stores) {
+                  return _.chain(stores)
+                    .sortBy(function(s) {
+                      if (s.id === store.id) {
+                        return 0;
+                      } else if (s.id === 'vault') {
+                        return 2;
+                      } else {
+                        return 1;
+                      }
+                    })
+                    .value();
+                })
+                .then(function(sortedStores) {
+                  return _.find(sortedStores, function(s) {
+                    return _.chain(s.items)
+                      .filter(function(i) {
+                        return item.type === i.type;
+                      })
+                      .size()
+                      .value() < ((s.id === 'vault') ? 72 : 10);
+                  });
+                })
+                .then(function(t) {
+                  if (_.isUndefined(t)) {
+                    throw new Error('Collector eh?  All your characters ' + moveItem[0].type.toLowerCase() + ' slots are full and I can\'t move items of characters, yet... Clear a slot on a character and I can complete the loadout.');
+                  }
+                  target = t;
+
+                  return dimItemService.moveTo(moveItem[0], target);
+                });
+            }
+          }
+
+          var promise = p
+            .then(function() {
+              return dimItemService.moveTo(item, store, pseudoItem.equipped);
+            })
+            .catch(function(a) {
+              scope.failed = true;
+              toaster.pop('error', item.name, a.message);
+            })
+            .finally(function() {
+              applyLoadoutItems(store, items, loadout, _items, scope);
+            });
+
+          loadingTracker.addPromise(promise);
+        }
+      } else {
+        var dimStores;
+
+        $q.when(dimStoreService.getStores())
+          .then(function(stores) {
+            dimStores = stores;
+            return dimStoreService.updateStores();
+          })
+          .then(function(bungieStores) {
+            _.each(dimStores, function(dStore) {
+              if (dStore.id !== 'vault') {
+                var bStore = _.find(bungieStores, function(bStore) {
+                  return dStore.id === bStore.id;
+                });
+
+                dStore.level = bStore.base.characterLevel;
+                dStore.percentToNextLevel = bStore.base.percentToNextLevel;
+                dStore.powerLevel = bStore.base.characterBase.powerLevel;
+                dStore.background = bStore.base.backgroundPath;
+                dStore.icon = bStore.base.emblemPath;
+              }
+            });
+          })
+          .then(function() {
+            var value = 'success';
+            var message = 'Your loadout has been transfered.';
+
+            if (scope.failed) {
+              value = 'warning';
+              message = 'Your loadout has been transfered, with errors.';
+            }
+
+            toaster.pop(value, loadout.name, message);
+          });
+      }
     }
 
     function hydratev3d0(loadoutPrimitive) {
