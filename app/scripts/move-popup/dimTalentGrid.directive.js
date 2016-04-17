eb9fd74a68379b0735b6433337e68d2691229ee5
Ben Hollis <ben@benhollis.net>
2016-04-17 00:28:28 -0700

    Bring back infusion calculator.
    This reverts commit 52169dfbbfa4e7b6e24fc2e68a16874691751ccf.

--- app/scripts/move-popup/dimTalentGrid.directive.js
+++ app/scripts/move-popup/dimTalentGrid.directive.js
@@ -37,14 +37,15 @@
       controller: TalentGridCtrl,
       controllerAs: 'vm',
       scope: {
-        talentGrid: '=dimTalentGrid'
+        talentGrid: '=dimTalentGrid',
+        infuse: '&dimInfuse'
       },
       restrict: 'E',
       replace: true,
       template: [
         '<svg preserveAspectRatio="xMaxYMin meet" svg-bind-viewbox="0 0 {{(vm.numColumns * vm.totalNodeSize - vm.nodePadding) * vm.scaleFactor}} {{(vm.numRows * vm.totalNodeSize - vm.nodePadding) * vm.scaleFactor + 1}}" class="talent-grid" ng-attr-height="{{(vm.numRows * vm.totalNodeSize - vm.nodePadding) * vm.scaleFactor}}" ng-attr-width="{{(vm.numColumns * vm.totalNodeSize - vm.nodePadding) * vm.scaleFactor}}">',
         ' <g ng-attr-transform="scale({{vm.scaleFactor}})">',
-        '  <g class="talent-node" ng-attr-transform="translate({{node.column * (vm.totalNodeSize)}},{{node.row * (vm.totalNodeSize)}})" ng-repeat="node in vm.talentGrid.nodes | talentGridNodes track by $index" ng-class="{ \'talent-node-activated\': node.activated, \'talent-node-showxp\': (!node.activated && node.xpRequired), \'talent-node-default\': (node.activated && !node.xpRequired && !node.exclusiveInColumn) }">',
+        '  <g class="talent-node" ng-attr-transform="translate({{node.column * (vm.totalNodeSize)}},{{node.row * (vm.totalNodeSize)}})" ng-repeat="node in vm.talentGrid.nodes | talentGridNodes track by $index" ng-class="{ \'talent-node-activated\': node.activated, \'talent-node-showxp\': (!node.activated && node.xpRequired), \'talent-node-default\': (node.activated && !node.xpRequired && !node.exclusiveInColumn) }" ng-click="node.name == \'Infuse\' ? vm.infuse({\'$event\': $event}) : true">',
         '    <circle r="16" cx="-17" cy="17" transform="rotate(-90)" class="talent-node-xp" stroke-width="2" ng-attr-stroke-dasharray="{{100.0 * node.xp / node.xpRequired}} 100" />',
         '    <image class="talent-node-img" xlink:href="" ng-attr-xlink:href="{{ node.icon | bungieIcon }}" x="20" y="20" height="96" width="96" transform="scale(0.25)"/>',
         '    <title>{{node.name}}\n{{node.description}}</title>',
@@ -61,7 +62,7 @@
     var vm = this;
     vm.nodeSize = 34;
     vm.nodePadding = 4;
-    vm.scaleFactor = 1.1;
+    vm.scaleFactor = 1.3;
     vm.totalNodeSize = vm.nodeSize + vm.nodePadding;
     if (vm.talentGrid) {
       vm.numColumns = _.max(vm.talentGrid.nodes, 'column').column + 1;
