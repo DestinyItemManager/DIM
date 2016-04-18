5e2c1b2c8ed0416c0024011a66cef2e537d1c66e
Ben Hollis <ben@benhollis.net>
2016-04-17 23:09:04 -0700

    Allow vault to be up to 12 spaces wide.

--- app/scripts/shell/dimSettingsCtrl.controller.js
+++ app/scripts/shell/dimSettingsCtrl.controller.js
@@ -20,7 +20,10 @@
       { id: 6, name: '6' },
       { id: 7, name: '7' },
       { id: 8, name: '8' },
-      { id: 9, name: '9' }
+      { id: 9, name: '9' },
+      { id: 10, name: '10' },
+      { id: 11, name: '11' },
+      { id: 12, name: '12' }
     ];
 
     settings.getSetting()
@@ -28,7 +31,7 @@
         vm.settings = s;
       });
 
-    vm.save = function(key) {      
+    vm.save = function(key) {
       settings.saveSetting(key, vm.settings[key]);
     };
   }
