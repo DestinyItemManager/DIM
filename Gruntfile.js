e306ad5edf0891ad72364840f58f6af9b8ea0b97
Kyle Shay <kyle.shay@ymail.com>
2016-05-11 10:09:33 -0400

    remove grunt stuff, move item bar inside of div that controls search opacity

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,6 @@ module.exports = function(grunt) {
   var pkg = grunt.file.readJSON('package.json');
   var betaVersion = pkg.version.toString() + "." + (Math.floor(Date.now() / 60000) - 24365210);
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
   grunt.initConfig({
     pkg: pkg,
 
@@ -49,76 +46,6 @@ module.exports = function(grunt) {
           from: pkg.version.toString(),
           to: betaVersion
         }]
-      }
-    },
-    uglify: {
-      it: {
-        options: {
-          mangle: false
-        },
-        files: {
-          'app/scripts/vendor.min.js': [
-            'app/vendor/jquery/dist/jquery.min.js',
-            'app/vendor/angular/angular.min.js',
-            'app/vendor/angular-ui-router/release/angular-ui-router.min.js',
-            'app/vendor/angular-animate/angular-animate.min.js',
-            'app/vendor/angular-aria/angular-aria.min.js',
-            'app/vendor/angular-native-dragdrop/draganddrop.js',
-            'app/vendor/ngDialog/js/ngDialog.min.js',
-            'app/vendor/Angular.uuid2/dist/angular-uuid2.min.js',
-            'app/vendor/underscore/underscore-min.js',
-            'app/vendor/lz-string/libs/lz-string.min.js',
-            'app/vendor/angular-chrome-storage/angular-chrome-storage.js',
-            'app/vendor/angular-messages/angular-messages.min.js',
-            'app/vendor/angular-promise-tracker/promise-tracker.js',
-            'app/vendor/jquery-textcomplete/dist/jquery.textcomplete.js',
-            'app/vendor/angularjs-slider/dist/rzslider.js',
-            'app/vendor/angular-chrome-storage/angular-chrome-storage.js',
-            'app/vendor/angular-hotkeys/build/hotkeys.js'
-          ],
-          'app/scripts/dim.min.js': [
-            'app/scripts/toaster.js',
-            'app/scripts/jquery-ui.position.js',
-            'app/scripts/ScrollToFixed.js',
-            'app/scripts/util.js',
-//            'app/scripts/dimApp.module.js',
-//            'app/scripts/dimApp.config.js',
-            'app/scripts/services/dimRateLimit.factory.js',
-            'app/scripts/services/dimActionQueue.factory.js',
-            'app/scripts/services/dimBungieService.factory.js',
-            'app/scripts/services/dimDefinitions.factory.js',
-            'app/scripts/services/dimInfoService.factory.js',
-            'app/scripts/services/dimPlatformService.factory.js',
-            'app/scripts/services/dimLoadoutService.factory.js',
-            'app/scripts/services/dimSettingsService.factory.js',
-            'app/scripts/services/dimStoreService.factory.js',
-            'app/scripts/services/dimXurService.factory.js',
-            'app/scripts/services/dimItemService.factory.js',
-            'app/scripts/services/dimEngramFarmingService.factory.js',
-            'app/scripts/loadout/dimLoadout.directive.js',
-            'app/scripts/loadout/dimLoadoutPopup.directive.js',
-//            'app/scripts/shell/dimAppCtrl.controller.js',
-            'app/scripts/shell/dimSettingsCtrl.controller.js',
-//            'app/scripts/shell/dimPlatformChoice.directive.js',
-//            'app/scripts/shell/dimSearchFilter.directive.js',
-            'app/scripts/shell/dimClickAnywhereButHere.directive.js',
-            'app/scripts/store/dimBungieImageFallback.directive.js',
-//            'app/scripts/store/dimStores.directive.js',
-//            'app/scripts/store/dimStoreItems.directive.js',
-            'app/scripts/store/dimStoreItem.directive.js',
-//            'app/scripts/store/dimStoreHeading.directive.js',
-            'app/scripts/store/dimSimpleItem.directive.js',
-            'app/scripts/store/dimStats.directive.js',
-//            'app/scripts/store/dimEngramFarming.directive.js',
-            'app/scripts/move-popup/dimMoveAmount.directive.js',
-            'app/scripts/move-popup/dimMovePopup.directive.js',
-            'app/scripts/move-popup/dimTalentGrid.directive.js',
-            'app/scripts/move-popup/dimMoveItemProperties.directive.js',
-            'app/scripts/infuse/dimInfuse.controller.js',
-            'app/scripts/xur/dimXur.controller.js',
-            'app/scripts/minmax/dimMinMax.controller.js'
-          ]
-        }
       }
     },
     sass: {
