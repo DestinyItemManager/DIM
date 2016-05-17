4ea838a6b166e546e82ca2b1c71bd37f0697aa3f
Ben Hollis <ben@benhollis.net>
2016-05-17 00:44:10 -0700

    Get rid of unused "condensed" setting.

--- app/scripts/store/dimStores.directive.js
+++ app/scripts/store/dimStores.directive.js
@@ -16,7 +16,6 @@
         '<div ng-repeat="store in vm.stores track by store.id"',
         '  class="storage dim-col-{{(store.id === \'vault\') ? vm.vaultCol : vm.charCol}}"',
         '  ng-class="{',
-        '    condensed: vm.condensed,',
         "    guardian: !store.isVault,",
         "    vault: store.isVault,",
         "    'hide-filtered': vm.hideFilteredItems,",
@@ -35,14 +34,12 @@
     var vm = this;
 
     vm.stores = null;
-    vm.condensed = false;
     vm.charCol = 3;
     vm.vaultCol = 4;
 
     settings.getSettings()
       .then(function(settings) {
         vm.hideFilteredItems = settings.hideFilteredItems;
-        vm.condensed = settings.condensed;
         vm.charCol = Math.max(3, Math.min(settings.charCol, 5));
         vm.vaultCol = Math.max(4, Math.min(settings.vaultCol, 12));
         vm.itemQuality = settings.itemQuality;
@@ -50,9 +47,7 @@
       });
 
     $scope.$on('dim-settings-updated', function(event, arg) {
-      if (_.has(arg, 'condensed')) {
-        vm.condensed = arg.condensed;
-      } else if (_.has(arg, 'charCol')) {
+      if (_.has(arg, 'charCol')) {
         vm.charCol = arg.charCol;
       } else if (_.has(arg, 'vaultCol')) {
         vm.vaultCol = arg.vaultCol;
