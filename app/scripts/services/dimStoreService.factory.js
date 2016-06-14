80769d93b21ded5530682f5483719e0d01ccabc4
Ben Hollis <ben@benhollis.net>
2016-06-13 22:05:23 -0700

    Precalculate stat-range and fix width.
    Fixes #603.

--- app/scripts/services/dimStoreService.factory.js
+++ app/scripts/services/dimStoreService.factory.js
@@ -773,7 +773,7 @@
       return {
         min: Math.floor((base)*(fitValue(max)/fitValue(light))),
         max: Math.floor((base+1)*(fitValue(max)/fitValue(light)))
-      }
+      };
     }
 
     // thanks to bungie armory for the max-base stats
@@ -781,6 +781,16 @@
     // https://www.reddit.com/r/DestinyTheGame/comments/4geixn/a_shift_in_how_we_view_stat_infusion_12tier/
     // TODO set a property on a bucket saying whether it can have quality rating, etc
     function getQualityRating(stats, light, type) {
+      // For a quality property, return a range string (min-max percentage)
+      function getQualityRange(light, quality) {
+        if (!quality) {
+          return '';
+        }
+        return ((quality.min === quality.max || light === 335) ?
+                quality.min :
+                (quality.min + "%-" +  quality.max)) + '%';
+      }
+
       var maxLight = 335;
 
       if (!stats || light.value < 280) {
@@ -835,7 +845,7 @@
         stat.qualityPercentage = {
           min: Math.round(100 * stat.scaled.min / stat.split),
           max: Math.round(100 * stat.scaled.max / stat.split)
-        }
+        };
         ret.total.min += scaled.min || 0;
         ret.total.max += scaled.max || 0;
       });
@@ -849,10 +859,9 @@
           stat.qualityPercentage = {
             min: Math.round(100 * stat.scaled.min / stat.split),
             max: Math.round(100 * stat.scaled.max / stat.split)
-          }
+          };
         });
       }
-
 
       var quality = {
         min: Math.round(ret.total.min / ret.max * 100),
@@ -871,6 +880,11 @@
           max: Math.min(100, quality.max)
         };
       }
+
+      stats.forEach(function(stat) {
+        stat.qualityPercentage.range = getQualityRange(light, stat.qualityPercentage);
+      });
+      quality.range = getQualityRange(light, quality);
 
       return quality;
     }
