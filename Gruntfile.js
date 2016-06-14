4078c16e492a9e8b0b779ecea72cbcaf0965c98a
Ben Hollis <ben@benhollis.net>
2016-06-14 00:28:59 -0700

    Use TRAVIS_BUILD_NUMBER for beta patch versions instead of hardcoded timestamp.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   var pkg = grunt.file.readJSON('package.json');
-  var betaVersion = pkg.version.toString() + "." + (Math.floor(Date.now() / 60000) - 24365210);
+  var betaVersion = pkg.version.toString() + "." + process.env.TRAVIS_BUILD_NUMBER;
 
   grunt.initConfig({
     pkg: pkg,
