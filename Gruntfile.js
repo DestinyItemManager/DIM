3b6dc34ff593a23d4aec58e289f576e3233f1495
Kyle Shay <kyle.shay@ymail.com>
2016-06-20 16:29:46 -0400

    grunt-sync is only a dev dep.

--- Gruntfile.js
+++ Gruntfile.js
@@ -149,7 +149,7 @@ module.exports = function(grunt) {
 
     // See https://github.com/c301/grunt-webstore-upload
     webstore_upload: {
-      "accounts": {
+      accounts: {
         // This is set up by environment variables from Travis. To
         // push locally, create a file with these variables defined
         // and source it before running "grunt publish:beta". E.g.:
@@ -160,25 +160,27 @@ module.exports = function(grunt) {
         // export CHROME_SECRET="bar"
         // export CHROME_REFRESH_TOKEN="baz"
         //
-        // Then run "source ./beta_credentials; grunt publish-beta"
+        // Then run "source ./beta_credentials; grunt publish_chrome_beta"
         //
         // To set up these variables to be available in Travis:
         // travis encrypt CHROME_CLIENT_ID=super_secret --add env.matrix
         // travis encrypt CHROME_SECRET=super_secret --add env.matrix
         // travis encrypt CHROME_REFRESH_TOKEN=super_secret --add env.matrix
-        "default": { //account under this section will be used by default
+        default: { //account under this section will be used by default
           publish: true, //publish item right after uploading. default false
           client_id: process.env.CHROME_CLIENT_ID,
           client_secret: process.env.CHROME_SECRET,
           refresh_token: process.env.CHROME_REFRESH_TOKEN
         }
       },
-      "extensions": {
-        "beta": {
-          //required
+      extensions: {
+        DIM: {
+          appID: "apghicjnekejhfancbkahkhdckhdagna",
+          publish: false,
+          zip: "dist/chrome.zip"
+        },
+        beta: {
           appID: "mkiipknpfaacbjdagdeppdacpgpdjklc",
-          publish: true,
-          //required, we can use dir name and upload most recent zip file
           zip: "dist/chrome.zip"
         }
       }
@@ -236,11 +238,12 @@ module.exports = function(grunt) {
     'compress:firefox'
   ]);
 
-  // Builds a release-able extension in dist/chrome.zip
+  // Builds release-able extensions in dist/
   grunt.registerTask('build_extension', [
     'build',
     'replace:main_version',
-    'compress:chrome'
+    'compress:chrome',
+    'compress:firefox',
   ]);
 
   grunt.registerTask('log_beta_version', function() {
