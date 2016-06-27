fedfc899c0a2e4dba82f1d03f15fce803e35e288
Dave Caslin <dcaslin@gmail.com>
2016-06-27 11:53:47 -0400

    Fix eslint tasks

--- app/scripts/services/dimCsvService.factory.js
+++ app/scripts/services/dimCsvService.factory.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   angular.module('dimApp')
@@ -7,14 +7,13 @@
   CsvService.$inject = ['dimStoreService'];
 
   function CsvService() {
-
-    //step node names we'll hide, we'll leave "* Chroma" for now though, since we don't otherwise indicate Chroma
+    // step node names we'll hide, we'll leave "* Chroma" for now though, since we don't otherwise indicate Chroma
     var FILTER_NODE_NAMES = ["Upgrade Defense", "Ascend", "Infuse", "Increase Intellect", "Increase Discipline",
       "Increase Strength", "Twist Fate", "The Life Exotic", "Reforge Artifact", "Reforge Shell",
       "Deactivate Chroma", "Kinetic Damage", "Solar Damage", "Arc Damage", "Void Damage"];
 
     function capitalizeFirstLetter(string) {
-      if (!string || string.length == 0) {
+      if (!string || string.length === 0) {
         return "";
       }
       return string.charAt(0).toUpperCase() + string.slice(1);
@@ -30,10 +29,14 @@
 
     function buildNodeString(nodes) {
       var data = "";
-      nodes.forEach(function (node) {
-        if (_.contains(FILTER_NODE_NAMES, node.name)) return;
+      nodes.forEach(function(node) {
+        if (_.contains(FILTER_NODE_NAMES, node.name)){
+          return;
+        }
         data += node.name;
-        if (node.activated) data += "*";
+        if (node.activated){
+          data += "*";
+        }
         data += ", ";
       });
 
@@ -44,12 +47,12 @@
       var header = "Name, Tier, Type, Equippable, Light, Owner, % Leveled, Locked, Equipped, " +
         "% Quality, % IntQ, % DiscQ, % StrQ, Int, Disc, Str, Perks\n";
       var data = "";
-      items.forEach(function (item) {
+      items.forEach(function(item) {
         data += item.name + ", ";
         data += item.tier + ", ";
         data += item.typeName + ", ";
         var equippable = item.classTypeName;
-        if (!equippable || "unknown" == equippable) {
+        if (!equippable || equippable === "unknown") {
           equippable = "Any";
         }
         else {
@@ -63,7 +66,7 @@
         data += item.equipped + ", ";
         data += item.quality ? item.quality.max + ", " : "0, ";
         var stats = {};
-        item.stats.forEach(function (stat) {
+        item.stats.forEach(function(stat) {
           var pct = 0;
           if (stat.scaled && stat.scaled.min) {
             pct = Math.round(100 * stat.scaled.min / stat.split);
@@ -71,15 +74,15 @@
           stats[stat.name] = {
             value: stat.value,
             pct: pct
-          }
+          };
         });
-        data += stats["Intellect"] ? stats["Intellect"].pct + ", " : "0, ";
-        data += stats["Discipline"] ? stats["Discipline"].pct + ", " : "0, ";
-        data += stats["Strength"] ? stats["Strength"].pct + ", " : "0, ";
-        data += stats["Intellect"] ? stats["Intellect"].value + ", " : "0, ";
-        data += stats["Discipline"] ? stats["Discipline"].value + ", " : "0, ";
-        data += stats["Strength"] ? stats["Strength"].value + ", " : "0, ";
-        //if DB is out of date this can be null, can't hurt to be careful
+        data += stats.Intellect ? stats.Intellect.pct + ", " : "0, ";
+        data += stats.Discipline ? stats.Discipline.pct + ", " : "0, ";
+        data += stats.Strength ? stats.Strength.pct + ", " : "0, ";
+        data += stats.Intellect ? stats.Intellect.value + ", " : "0, ";
+        data += stats.Discipline ? stats.Discipline.value + ", " : "0, ";
+        data += stats.Strength ? stats.Strength.value + ", " : "0, ";
+        // if DB is out of date this can be null, can't hurt to be careful
         if (item.talentGrid) {
           data += buildNodeString(item.talentGrid.nodes);
         }
@@ -93,7 +96,7 @@
         "AA, Impact, Range, Stability, ROF, Reload, Mag, Equip, " +
         "Nodes\n";
       var data = "";
-      guns.forEach(function (gun) {
+      guns.forEach(function(gun) {
         data += gun.name + ", ";
         data += gun.tier + ", ";
         data += gun.typeName + ", ";
@@ -118,32 +121,32 @@
           magazine: 0,
           equipSpeed: 0
         };
-        gun.stats.forEach(function (stat) {
+        gun.stats.forEach(function(stat) {
           switch (stat.name) {
-            case 'Aim Assist':
-              stats.aa = stat.value;
-              break;
-            case 'Impact':
-              stats.impact = stat.value;
-              break;
-            case 'Range':
-              stats.range = stat.value;
-              break;
-            case 'Stability':
-              stats.stability = stat.value;
-              break;
-            case 'Rate of Fire':
-              stats.rof = stat.value;
-              break;
-            case 'Reload':
-              stats.reload = stat.value;
-              break;
-            case 'Magazine':
-              stats.magazine = stat.value;
-              break;
-            case 'Equip Speed':
-              stats.equipSpeed = stat.value;
-              break;
+          case 'Aim Assist':
+            stats.aa = stat.value;
+            break;
+          case 'Impact':
+            stats.impact = stat.value;
+            break;
+          case 'Range':
+            stats.range = stat.value;
+            break;
+          case 'Stability':
+            stats.stability = stat.value;
+            break;
+          case 'Rate of Fire':
+            stats.rof = stat.value;
+            break;
+          case 'Reload':
+            stats.reload = stat.value;
+            break;
+          case 'Magazine':
+            stats.magazine = stat.value;
+            break;
+          case 'Equip Speed':
+            stats.equipSpeed = stat.value;
+            break;
           }
         });
         data += stats.aa + ", ";
@@ -154,7 +157,7 @@
         data += stats.reload + ", ";
         data += stats.magazine + ", ";
         data += stats.equipSpeed + ", ";
-        //haven't seen this null yet, but can't hurt to check since we saw it on armor above
+        // haven't seen this null yet, but can't hurt to check since we saw it on armor above
         if (gun.talentGrid) {
           data += buildNodeString(gun.talentGrid.nodes);
         }
@@ -164,13 +167,15 @@
     }
 
     function downloadCsvFiles(stores, type) {
-      //perhaps we're loading
-      if (stores.length == 0) return;
+      // perhaps we're loading
+      if (stores.length === 0){
+        return;
+      }
       var nameMap = {};
       var allItems = [];
-      stores.forEach(function (store) {
+      stores.forEach(function(store) {
         allItems = allItems.concat(store.items);
-        if (store.id == "vault") {
+        if (store.id === "vault") {
           nameMap[store.id] = "Vault";
         }
         else {
@@ -178,22 +183,22 @@
         }
       });
       var items = [];
-      allItems.forEach(function (item) {
-        if (item.primStat == null) {
+      allItems.forEach(function(item) {
+        if (!item.primStat) {
           return;
         }
-        if (type == "Weapons") {
-          if (item.primStat.statHash == 368428387) {
+        if (type === "Weapons") {
+          if (item.primStat.statHash === 368428387) {
             items.push(item);
           }
         }
-        else if (type == "Armor") {
-          if (item.primStat.statHash == 3897883278) {
+        else if (type === "Armor") {
+          if (item.primStat.statHash === 3897883278) {
             items.push(item);
           }
         }
       });
-      if (type == "Weapons") {
+      if (type === "Weapons") {
         downloadWeapons(items, nameMap);
       }
       else {
