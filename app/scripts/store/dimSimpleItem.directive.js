c313bfc3d5e0f711695e80b335e6dcde67e8601c
Ben Hollis <ben@benhollis.net>
2016-07-08 22:34:07 -0700

    Expand item quality to simple items.
    This shows in loadouts, Xur, and infusion calculator.

--- app/scripts/store/dimSimpleItem.directive.js
+++ app/scripts/store/dimSimpleItem.directive.js
@@ -15,7 +15,8 @@
         '<div title="{{ vm.item.primStat.value }} {{:: vm.item.name }}" alt="{{ vm.item.primStat.value }} {{:: vm.item.name }}" class="item">',
         '  <div class="item-elem" ng-class="{ complete: vm.item.complete }">',
         '    <div class="img" dim-bungie-image-fallback="::vm.item.icon">',
-        '      <div class="item-stat item-equipment stat-damage-{{::vm.item.dmg}}" ng-if="vm.item.primStat.value || vm.item.maxStackSize > 1">{{ vm.item.primStat.value || vm.item.amount }}</div>',
+        '    <div ng-if="vm.item.quality" class="item-stat item-quality" ng-style="vm.item.quality.min | qualityColor">{{ vm.item.quality.min }}%</div>',
+        '    <div class="item-stat item-equipment stat-damage-{{::vm.item.dmg}}" ng-class="{\'item-stat-no-bg\': (vm.item.quality && vm.item.quality.min > 0) }" ng-if="vm.item.primStat.value || vm.item.maxStackSize > 1">{{ vm.item.primStat.value || vm.item.amount }}</div>',
         '    </div>',
         '  </div>',
         '</div>'
