e35de6c3344a5a072a16c533c893b50d2efd16ed
SunburnedGoose <rick.casey@gmail.com>
2016-07-19 19:53:04 -0500

    Updating versions.

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -59,9 +59,9 @@
         }
 
         console.log('DIM v$DIM_VERSION - Please report any errors to https://www.reddit.com/r/destinyitemmanager');
-        dimInfoService.show('changelogv382', {
-          title: 'DIM v3.8.2 Released',
-          view: 'views/changelog-toaster.html?v=v3.8.2'
+        dimInfoService.show('changelogv$DIM_VERSION', {
+          title: 'DIM v$DIM_VERSION Released',
+          view: 'views/changelog-toaster.html?v=v$DIM_VERSION'
         });
 
 //        if (chrome && chrome.identity) {
