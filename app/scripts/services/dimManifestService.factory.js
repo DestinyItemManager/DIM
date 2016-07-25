cb0568cc8704a1d5742e4cb007b4582de8992ee3
Ben Hollis <ben@benhollis.net>
2016-07-24 21:41:52 -0700

    Remove TODOs/console

--- app/scripts/services/dimManifestService.factory.js
+++ app/scripts/services/dimManifestService.factory.js
@@ -6,17 +6,13 @@
 
   ManifestService.$inject = ['$rootScope', '$q', 'dimBungieService', '$http'];
 
-  // TODO: profile memory of SQLite vs reading it all out at startup!
-  // TODO: so far this uses a lot more memory
   function ManifestService($rootScope, $q, dimBungieService, $http) {
     // Testing flags
     const alwaysLoadRemote = false;
 
     let manifestPromise = null;
 
-    // TODO: expose state properties, loading progress
     return {
-      // TODO Do this entirely with webworkers?
       getManifest: function() {
         if (manifestPromise) {
           return manifestPromise;
@@ -24,24 +20,17 @@
 
         // TODO: var language = window.navigator.language;
         var language = 'en';
-        console.time('manifest');
 
         manifestPromise = dimBungieService.getManifest()
           .then(function(data) {
-            console.log(data);
             var version = data.version;
 
             return loadManifestFromCache(version)
               .catch(function(e) {
-                console.log(e);
-                // TODO: progress events
-
-                console.log("loading from remote");
                 var path = data.mobileWorldContentPaths[language] || data.mobileWorldContentPaths.en;
                 return loadManifestRemote(version, path);
               })
               .then(function(typedArray) {
-                console.log('got buffer', typedArray.byteLength);
                 return new SQL.Database(typedArray);
               });
           });
@@ -49,25 +38,14 @@
         return manifestPromise;
       },
 
-      // TODO: wrap an object around the DB!
-
-      // TODO: memoize provides dumb caching but probably not necessary!
-      // TODO: have item services do it instead?
       getRecord: _.memoize(function(db, table, id) {
-        // console.time('getRecord ' + table + ' ' + id);
-        // TODO: prepared statements?
-        // TODO: web worker? 3.5ms per invocation right now. Time against normal DIM?
-        try {
-          // The ID in sqlite is a signed 32-bit int, while the id we use is unsigned, so we must convert
-          const sqlId = new Int32Array([id])[0];
-          const result = db.exec(`SELECT json FROM ${table} where id = ${sqlId}`);
-          if (result.length && result[0].values && result[0].values.length) {
-            return JSON.parse(result[0].values[0]);
-          }
-          return null;
-        } finally {
-          // console.timeEnd('getRecord ' + table + ' ' + id);
+        // The ID in sqlite is a signed 32-bit int, while the id we use is unsigned, so we must convert
+        const sqlId = new Int32Array([id])[0];
+        const result = db.exec(`SELECT json FROM ${table} where id = ${sqlId}`);
+        if (result.length && result[0].values && result[0].values.length) {
+          return JSON.parse(result[0].values[0]);
         }
+        return null;
       }, (db, table, id) => table + '-' + id),
 
       getAllRecords: function(db, table) {
@@ -91,12 +69,7 @@
         })
         .then(function(arraybuffer) {
           var typedArray = new Uint8Array(arraybuffer);
-          console.time('base64');
           var encoded = base64js.fromByteArray(typedArray);
-          console.timeEnd('base64');
-          console.log(encoded.length);
-          console.time('store');
-          // TODO: I guess we could store async and return it without waiting!
           return $q(function(resolve, reject) {
             // Note: this requires the 'unlimitedStorage' permission, as the
             // uncompressed manifest is ~40MB.
@@ -107,7 +80,6 @@
               if (chrome.runtime.lastError) {
                 reject(chrome.runtime.lastError);
               } else {
-                console.timeEnd('store');
                 resolve(typedArray);
               }
             });
@@ -120,7 +92,6 @@
      * version as a Uint8Array, or rejects.
      */
     function loadManifestFromCache(version) {
-      // TODO: none of this works in Firefox?
       return $q(function(resolve, reject) {
         if (alwaysLoadRemote) {
           reject(new Error("Testing - always load remote"));
@@ -136,7 +107,6 @@
               if (chrome.runtime.lastError) {
                 reject(chrome.runtime.lastError);
               } else if (currentManifest) {
-                console.log("loading from localstorage");
                 resolve(base64js.toByteArray(currentManifest));
               } else {
                 reject(new Error("no stored manifest despite version"));
@@ -153,7 +123,6 @@
      * Unzip a file from a ZIP Blob into an ArrayBuffer. Returns a promise.
      */
     function unzipManifest(blob) {
-      console.time('unzip');
       return $q(function(resolve, reject) {
         zip.workerScriptsPath = "vendor/zip.js/WebContent/";
         zip.createReader(new zip.BlobReader(blob), function(zipReader) {
@@ -164,7 +133,6 @@
                 var blobReader = new FileReader();
                 blobReader.addEventListener("loadend", function() {
                   zipReader.close(function() {
-                    console.timeEnd('unzip');
                     resolve(blobReader.result);
                   });
                 });
