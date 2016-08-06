c1f3b89c89fb77eb6eb3cdfac02cb22f065db343
EJ Nellie <enellie01@gmail.com>
2016-08-05 19:14:35 -0500

    Fix issue with popup after introduction of directive

--- app/scripts/vendors/dimVendorItems.directive.js
+++ app/scripts/vendors/dimVendorItems.directive.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var VendorItems = {
+    controller: VendorItemsCtrl,
     controllerAs: 'vm',
     bindings: {
       stores: '<storesData',
@@ -50,4 +51,91 @@
 
   angular.module('dimApp')
     .component('dimVendorItems', VendorItems);
+    
+  VendorItemsCtrl.$inject = ['$scope', 'ngDialog', 'dimStoreService'];
+    
+  function VendorItemsCtrl($scope, ngDialog, dimStoreService) {
+    var vm = this;
+    var dialogResult = null;
+    var detailItem = null;
+    var detailItemElement = null;
+    
+    $scope.$on('ngDialog.opened', function(event, $dialog) {
+      if (dialogResult && $dialog[0].id === dialogResult.id) {
+        $dialog.position({
+          my: 'left top',
+          at: 'left bottom+2',
+          of: detailItemElement,
+          collision: 'flip flip'
+        });
+      }
+    });
+    
+    angular.extend(vm, {
+      itemClicked: function(item, e) {
+        e.stopPropagation();
+        if (dialogResult) {
+          dialogResult.close();
+        }
+
+        if (detailItem === item) {
+          detailItem = null;
+          dialogResult = null;
+          detailItemElement = null;
+        } else {
+          detailItem = item;
+          detailItemElement = angular.element(e.currentTarget);
+
+          var compareItems = _.flatten(dimStoreService.getStores().map(function(store) {
+            return _.filter(store.items, { hash: item.hash });
+          }));
+
+          var compareItemCount = sum(compareItems, 'amount');
+
+          dialogResult = ngDialog.open({
+            template: [
+              '<div class="move-popup" dim-click-anywhere-but-here="closeThisDialog()">',
+              '  <div dim-move-item-properties="vm.item" dim-compare-item="vm.compareItem"></div>',
+              '  <div class="item-details more-item-details" ng-if="vm.item.equipment && vm.compareItems.length">',
+              '    <div>Compare with what you already have:</div>',
+              '    <div class="compare-items">',
+              '      <dim-simple-item ng-repeat="ownedItem in vm.compareItems track by ownedItem.index" item-data="ownedItem" ng-click="vm.setCompareItem(ownedItem)" ng-class="{ selected: (ownedItem.index === vm.compareItem.index) }"></dim-simple-item>',
+              '    </div>',
+              '  </div>',
+              '  <div class="item-description" ng-if="!vm.item.equipment">You have {{vm.compareItemCount}} of these.</div>',
+              '</div>'].join(''),
+            plain: true,
+            overlay: false,
+            className: 'move-popup vendor-move-popup',
+            showClose: false,
+            scope: angular.extend($scope.$new(true), {
+            }),
+            controllerAs: 'vm',
+            controller: [function() {
+              var vm = this;
+              angular.extend(vm, {
+                item: item,
+                compareItems: compareItems,
+                compareItem: _.first(compareItems),
+                compareItemCount: compareItemCount,
+                setCompareItem: function(item) {
+                  this.compareItem = item;
+                }
+              });
+            }],
+            // Setting these focus options prevents the page from
+            // jumping as dialogs are shown/hidden
+            trapFocus: false,
+            preserveFocus: false
+          });
+        }
+      },
+      close: function() {
+        if (dialogResult) {
+          dialogResult.close();
+        }
+        $scope.closeThisDialog();
+      }
+    });
+  }
 })();
