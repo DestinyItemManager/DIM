610e2e0ffeec358fda097910ee2f06abd9a06076
SunburnedGoose <rick.casey@gmail.com>
2016-09-12 16:00:19 -0500

    Added Google Analytics back to DIM.

--- app/scripts/google.js
+++ app/scripts/google.js
@@ -11,3 +11,9 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-60316581-1']);
 _gaq.push(['_setCustomVar', 1, 'DIMVersion', '$DIM_VERSION', 3]);
 _gaq.push(['_trackPageview']);
+
+(function () {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://www.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
