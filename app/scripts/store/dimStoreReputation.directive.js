bfaa7c8d22c76b5e589287f6f4739c3d51b8dff9
Ben Hollis <ben@benhollis.net>
2016-09-22 00:25:42 -0700

    Fix reputation progress display to line up with the game.
    Fixes #947.

--- app/scripts/store/dimStoreReputation.directive.js
+++ app/scripts/store/dimStoreReputation.directive.js
@@ -12,7 +12,7 @@
       '    <div class="item" ng-if="rep.order >= 0" ng-repeat="rep in vm.store.progression.progressions | orderBy:\'order\' track by $index" title="{{rep.faction.factionName}}\n{{rep.progressToNextLevel}}/{{rep.nextLevelAt}}\n{{\'Level\' | translate}}: {{rep.level}}">',
       '      <svg width="48" height="48">',
       '        <image xlink:href="" ng-attr-xlink:href="{{rep.faction.factionIcon | bungieIcon}}" width="48" height="48" />',
-      '        <polygon stroke-dasharray="130" ng-if="rep.progressToNextLevel > 0" style="stroke-dashoffset:{{130-(130*rep.progressToNextLevel/rep.nextLevelAt)}}" fill-opacity="0" stroke="#FFF" stroke-width="3" points="24,2.5 45.5,24 24,45.5 2.5,24" stroke-linecap="butt"/>',
+      '        <polygon stroke-dasharray="121.622368" ng-if="rep.progressToNextLevel > 0" style="stroke-dashoffset:{{121.622368-(121.622368*rep.progressToNextLevel/rep.nextLevelAt)}}" fill-opacity="0" stroke="#FFF" stroke-width="3" points="24,2.5 45.5,24 24,45.5 2.5,24" stroke-linecap="butt"/>',
       '      </svg>',
       '      <div class="item-stat item-faction" ng-bind="rep.level"></div>',
       '    </div>',
