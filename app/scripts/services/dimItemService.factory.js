b4e5162666beadc5f426278d9a42bb4ca4c55312
delphiactual <delphi.actual@gmail.com>
2016-09-29 16:47:30 -0600

    derp

--- app/scripts/services/dimItemService.factory.js
+++ app/scripts/services/dimItemService.factory.js
@@ -470,7 +470,7 @@
           resolve(true);
         } else {
 		  var classified = ["Classified", "Klassifiziert", "Confidentiel", "Clasificado", "Classificato", "機密", "Acesso restrito"];
-          if classified.includes(item.itemName) {
+          if (classified.includes(item.name)) {
 			reject(new Error("This item is classified and can not be transferred at this time."));
 		  } else {
             reject(new Error("This can only be equipped on " + item.classTypeName + "s at or above level " + item.equipRequiredLevel + "."));
