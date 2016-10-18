fcf206bdf8999a762190ed1e935a5ad36c48fd52
Scott O'Malley <scottyomalley@gmail.com>
2016-10-18 13:06:32 +0100

    Updates to the mats exchange look/feel and functionality

--- app/scripts/shell/dimMaterialsExchangeCtrl.controller.js
+++ app/scripts/shell/dimMaterialsExchangeCtrl.controller.js
@@ -7,13 +7,71 @@
 
   function MaterialsController($scope, dimItemService, dimStoreService, $state) {
     var vm = this;
+    vm.repPool = {};
+    vm.newRank = 0;
+    vm.newExperiance = 0;
 
-    var stores = dimStoreService.getStores();
+    vm.factions = [
+     // 174528503, eris breaks things atm
+      3871980777,
+      1424722124,
+      2778795080
+    ];
 
-    if (stores.length === 0) {
+    vm.characters = dimStoreService.getStores()
+      .filter(function(character) {
+        return !character.isVault;
+      })
+      .map(function(item) {
+        var background = item.background;
+        var icon = item.icon;
+        var className = item.className;
+        var genderRace = item.genderRace;
+        var factions = item.progression.progressions.filter((faction) => {
+          return vm.factions.includes(faction.hash);
+        }).reduce(function(map, obj) {
+          map[obj.hash] = obj;
+          return map;
+        }, {});
+        return { background, icon, className, genderRace, factions };
+      });
+
+    if (vm.characters.length === 0) {
       $state.go('inventory');
       return;
     }
+
+    vm.activeCharacter = vm.characters[0];
+    vm.activeFaction = vm.activeCharacter.factions[1424722124];
+    vm.setActiveFaction = function(hash) {
+      vm.activeFaction = vm.activeCharacter.factions[hash];
+      updateRanks();
+    };
+
+    vm.setActiveCharacter = function(character) {
+      vm.activeCharacter = character;
+      vm.setActiveFaction(vm.activeFaction.hash);
+    };
+
+    vm.toggleItem = function(item){
+      if (vm.repPool[item.hash]){
+        delete vm.repPool[item.hash];
+      } else {
+        vm.repPool[item.hash] = vm.calculateRep(item);
+      }
+      updateRanks();
+    };
+
+    function updateRanks() {
+    // rank 1 = 1500
+    // rank 2 = 2000
+    // rank 3+ = 2500
+      var total = Array.from(vm.repPool).reduce((totalRep, current) => {
+        return totalRep + current;
+      });
+      console.log(total);
+    }
+
 
     var materialsHashes = [
       211861343,  // heavy ammo synth
@@ -61,13 +119,13 @@
     vm.gunSmithMats = mapGunsmithItems(gunSmithMatsHashes);
 
     function mapItems(hashes) {
-      return hashes.map(function (hash) {
+      return hashes.map(function(hash) {
         var ret = angular.copy(dimItemService.getItem({
           hash: hash
         }));
         if (ret) {
           ret.amount = 0;
-          dimStoreService.getStores().forEach(function (s) {
+          dimStoreService.getStores().forEach(function(s) {
             ret.amount += s.amountOfItem(ret);
           });
         }
@@ -99,25 +157,31 @@
       return mappedItems;
     }
 
-    vm.calculateRep = function (item) {
+    vm.calculateRep = function(item) {
+      var rep = 0;
       switch (item.hash) {
-        case 211861343:
-          return Math.floor(item.amount * 25);       // heavy ammo synth
-        case 937555249:
-          return Math.floor(item.amount / 5) * 100;  // motes of light
-        case 928169143:
-          return Math.floor(item.amount / 4) * 25;   // special ammo synth
-        case 1542293174: // armor materials
-        case 1898539128: // weapon parts
-        case 1797491610: // Helium Filaments
-        case 2882093969: // Spin Metal
-        case 3242866270: // Relic Iron
-        case 2254123540: // Spirit Bloom
-        case 3164836592: // Wormspore
-          return Math.floor(item.amount / 25) * 50;
-        default:
-          return '?';
+      case 211861343:
+        rep = Math.floor(item.amount * 25);
+        break;       // heavy ammo synth
+      case 937555249:
+        rep = Math.floor(item.amount / 5) * 100;  // motes of light
+        break;
+      case 928169143:
+        rep = Math.floor(item.amount / 4) * 25;   // special ammo synth
+        break;
+      case 1542293174: // armor materials
+      case 1898539128: // weapon parts
+      case 1797491610: // Helium Filaments
+      case 2882093969: // Spin Metal
+      case 3242866270: // Relic Iron
+      case 2254123540: // Spirit Bloom
+      case 3164836592: // Wormspore
+        rep = Math.floor(item.amount / 25) * 50;
+        break;
+      default:
+        return '?';
       }
+      return rep;
     };
   }
 })();
