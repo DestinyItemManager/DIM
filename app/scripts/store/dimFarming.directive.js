9c844e6ba51e2aec6a19b871463fea4a85f74768
delphiactual <delphi.actual@gmail.com>
2016-10-24 14:34:48 -0600

    merge farming mode fixes and i18n

--- app/scripts/store/dimFarming.directive.js
+++ app/scripts/store/dimFarming.directive.js
@@ -18,13 +18,8 @@
           <span>
             <p translate="farming_desc" translate-values="{ store: vm.service.store.name }"></p>
             <div class="item-details"><span>
-<<<<<<< HEAD
               <p translate>configuration</p>
-              <p><input id="farm-greens" type='checkbox' ng-model='vm.settings.farmGreens' /><label for="farm-greens" translate translate-attr="{ title: 'farming_config1_tooltip'}">farming_config1</label></p>
-=======
-              <p>Configuration</p>
-              <p><input id="farm-greens" type='checkbox' ng-change="vm.settings.save()" ng-model='vm.settings.farming.farmGreens' /><label for="farm-greens" title="If checked, DIM will also transfer all uncommon (green) items to the vault. If it's not checked, then green items will stay on your active character.">Move Uncommon/Green Items to Vault</label></p>
->>>>>>> refs/remotes/DestinyItemManager/dev
+              <p><input id="farm-greens" type='checkbox' ng-change="vm.settings.save()" ng-model='vm.settings.farming.farmGreens' /><label for="farm-greens" translate-attr="{ title: 'farming_config1_tooltip'}" translate>farming_config1</p>
             </span><span>
               <p translate>farming_quickmove</p>
               <p><dim-simple-item ng-repeat="item in vm.service.consolidate track by $index" item-data="item" ng-click="vm.consolidate(item, vm.service.store)"></dim-simple-item></p>
