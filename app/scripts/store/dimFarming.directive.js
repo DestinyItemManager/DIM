8a792afb176cbddce4e53cb24ca825f1f6c9df75
delphiactual <delphi.actual@gmail.com>
2016-10-23 20:45:28 -0600

    Farming Mode i18n

--- app/scripts/store/dimFarming.directive.js
+++ app/scripts/store/dimFarming.directive.js
@@ -16,13 +16,13 @@
             <img class="engram" ng-class="{ active: (vm.service.movingItems || vm.service.makingRoom) }" src="/images/engram.svg" height="60" width="60"/>
           </span>
           <span>
-            <p>DIM is moving Engram and Glimmer items from {{vm.service.store.name}} to the vault and leaving one space open per item type to prevent anything from going to the Postmaster.</p>
+            <p><span translate="farming_desc"></span></p>
             <div class="item-details"><span>
-              <p>Quick Move</p>
+              <p><span translate="farming_quickmove"></span></p>
               <p><dim-simple-item ng-repeat="item in vm.service.consolidate track by $index" item-data="item" ng-click="vm.consolidate(item, vm.service.store)"></dim-simple-item></p>
             </span></div>
           </span>
-          <span><button ng-click="vm.stop($event)">Stop</button></span>
+          <span><button ng-click="vm.stop($event)"><span translate="stop"></span></button></span>
         </div>`
     };
   }
