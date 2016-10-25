8137d89ff6d3584b10eda4a982de5ecc808b6d5e
delphiactual <delphi.actual@gmail.com>
2016-10-25 07:47:41 -0600

    Maintain current operation for non-incognito mode

--- app/extension-scripts/main.js
+++ app/extension-scripts/main.js
@@ -1,6 +1,10 @@
 (function() {
   chrome.browserAction.onClicked.addListener(function() {
-    var appUrl = chrome.extension.getURL('index.html');
-    chrome.tabs.create({ url: appUrl });
+    if (chrome.extension.inIncognitoContext) {
+      var appUrl = chrome.extension.getURL('index.html');
+      chrome.tabs.create({ url: appUrl });
+    } else {
+      chrome.runtime.openOptionsPage();
+    }
   });
 })();
