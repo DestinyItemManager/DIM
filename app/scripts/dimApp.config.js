09fe50c040cbf7bc9e1fa63ed8ee371a234f4448
Kyle Shay <kyle.shay@ymail.com>
2016-12-13 01:53:14 -0500

    clean up

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -120,9 +120,8 @@
     ])
     .config([
       '$compileProvider',
-      '$qProvider',
       function($compileProvider) {
-        // TODO: remove https://github.com/angular/angular.js/blob/master/CHANGELOG.md#breaking-changes-1
+        // TODO: remove this depenency by fixing component bindings https://github.com/angular/angular.js/blob/master/CHANGELOG.md#breaking-changes-1
         $compileProvider.preAssignBindingsEnabled(true);
         // Allow chrome-extension: URLs in ng-src
         $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|chrome-extension):|data:image\/)/);
