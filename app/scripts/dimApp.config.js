97d65545f609223b1309a63a03be22d9c9391673
Kyle Shay <kyle.shay@ymail.com>
2016-12-13 01:49:20 -0500

    bump angular to 1.6

--- app/scripts/dimApp.config.js
+++ app/scripts/dimApp.config.js
@@ -120,7 +120,10 @@
     ])
     .config([
       '$compileProvider',
+      '$qProvider',
       function($compileProvider) {
+        // TODO: remove https://github.com/angular/angular.js/blob/master/CHANGELOG.md#breaking-changes-1
+        $compileProvider.preAssignBindingsEnabled(true);
         // Allow chrome-extension: URLs in ng-src
         $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|chrome-extension):|data:image\/)/);
       }
