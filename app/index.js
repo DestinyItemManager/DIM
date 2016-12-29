2edb685b17a545b346070d47d3118c4025e89396
SunburnedGoose <rick.casey@gmail.com>
2016-12-28 23:53:50 -0600

    Removed most of the dependencies from bower to npm.

--- app/index.js
+++ app/index.js
@@ -1,26 +1,33 @@
-// import angular from 'angular';
-// import angularUiRouter from 'angular-ui-router';
-// import 'babel-polyfill';
-
-// angular.module('dimApp', [
-//   angularUiRouter
-// ]);
-
-// window._ = require('lodash');
 window._ = require('underscore');
-window.$ = window.jQuery = require('jquery');
-window.angular = require('exports?window.angular!angular');
-window.moment = require('moment');
 
+window.$ = window.jQuery = require('jquery');
+require('jquery-textcomplete');
 require('jquery-ui/ui/position');
+
+window.humanizeDuration = require('humanize-duration');
+require('imports?define=>false,module=>false,self=>window!idb-keyval');
+// window.JSZip = require('jszip');
+window.LZString = require('lz-string');
+window.MessageFormat = require('messageformat');
+window.moment = require('moment');
+window.SQL = require('sql.js');
+require('imports?this=>window!zip-js/WebContent/zip.js');
+
+window.angular = require('exports?window.angular!angular');
 
 require('angular-aria');
 require('angular-chrome-storage/angular-chrome-storage');
+require('angular-hotkeys');
+require('angular-messages');
 require('angular-moment');
+require('angular-native-dragdrop');
+require('angular-promise-tracker');
 require('angular-timer');
+require('angular-translate');
+require('angular-translate-interpolation-messageformat');
 require('angular-ui-router');
-
-// require(
-//   'imports?angular=>window.angular!' +
-//   '../vendor/angular-cookies'
-// );
+require('angular-uuid2/dist/angular-uuid2.js');
+require('angularjs-slider');
+require('angularjs-toaster');
+require('ng-dialog');
+require('ng-http-rate-limiter');
--- config/webpack.js
+++ config/webpack.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './app/index.js',
   output: {
@@ -9,6 +11,24 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
     }]
-  }
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.js']
+  },
+  node: {
+    console: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      compress: false
+    })
+  ]
 }
