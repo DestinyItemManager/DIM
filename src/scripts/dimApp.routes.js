88c9af70821e30e616681ca664a529bd48486320
SunburnedGoose <rick.casey@gmail.com>
2017-01-29 01:05:58 -0600

    Added the shell to the routes.

--- src/scripts/dimApp.routes.js
+++ src/scripts/dimApp.routes.js
@@ -31,36 +31,42 @@ function getStates() {
   }, {
     state: 'best',
     config: {
+      parent: 'root',
       templateUrl: best,
       url: '/best'
     }
   }, {
     state: 'vendors',
     config: {
+      parent: 'root',
       templateUrl: vendors,
       url: '/vendors'
     }
   }, {
     state: 'materials-exchange',
     config: {
+      parent: 'root',
       url: '/materials-exchange',
       templateUrl: materialExchange,
     }
   }, {
     state: 'debugItem',
     config: {
+      parent: 'root',
       url: '/debugItem/:itemId',
       templateUrl: debugItem,
     }
   }, {
     state: 'developer',
     config: {
+      parent: 'root',
       url: '/developer',
       templateUrl: developer,
     }
   }, {
     state: 'login',
     config: {
+      parent: 'root',
       url: '/login',
       templateUrl: login,
     }
