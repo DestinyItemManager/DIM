837fa8775b7930270d3f5b23f54b53031f3da4ee
SunburnedGoose <rick.casey@gmail.com>
2017-02-09 16:11:58 -0600

    Fixed missed merge.

--- src/scripts/shell/dimAppCtrl.controller.js
+++ src/scripts/shell/dimAppCtrl.controller.js
@@ -151,19 +151,17 @@ function DimApp(dimActivityTrackerService, dimState, ngDialog, $rootScope, loadi
    * Show a popup dialog containing the given template. Its class
    * will be based on the name.
    */
-  function showPopupFunction(name, translate) {
+  function showPopupFunction(name) {
     var result;
     return function(e) {
       e.stopPropagation();
-
-      var language = translate ? vm.settings.language + '/' : ''; // set language
 
       if (result) {
         result.close();
       } else {
         ngDialog.closeAll();
         result = ngDialog.open({
-          template: require('app/views/' + language + name + '.template.html'),
+          template: require('app/views/' + name + '.template.html'),
           className: name,
           appendClassName: 'modal-dialog'
         });
@@ -176,8 +174,8 @@ function DimApp(dimActivityTrackerService, dimState, ngDialog, $rootScope, loadi
   }
 
   vm.showSetting = showPopupFunction('settings');
-  vm.showAbout = showPopupFunction('about', true);
-  vm.showSupport = showPopupFunction('support', true);
+  vm.showAbout = showPopupFunction('about');
+  vm.showSupport = showPopupFunction('support');
   vm.showFilters = showPopupFunction('filters');
   vm.showXur = showPopupFunction('xur');
   vm.showMatsExchange = showPopupFunction('mats-exchange');
