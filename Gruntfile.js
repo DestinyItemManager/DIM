023a0e6a7885d6a3b0076ee7880233d2017f6a13
Ben Hollis <ben@benhollis.net>
2017-02-20 19:08:35 -0800

    Add an htaccess file

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function(grunt) {
           cwd: 'dist',
           src: [
             '**',
-            '!chrome.zip'
+            '!chrome.zip',
+            '!.htaccess'
           ],
           dest: '/',
           filter: 'isFile'
