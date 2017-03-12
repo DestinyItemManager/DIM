a161ac68d4480ae21c87a0c8d9a1186cbb57c07d
Dave Solomon <dave.c.solomon@gmail.com>
2017-03-12 11:35:19 -0400

    Added submit button for weapon rating saves.

--- src/scripts/move-popup/dimMoveItemProperties.directive.js
+++ src/scripts/move-popup/dimMoveItemProperties.directive.js
@@ -54,7 +54,7 @@ function MoveItemProperties() {
       '  </div>',
       '  <form ng-if="vm.item.lockable && vm.featureFlags.tagsEnabled" name="notes"><textarea name="data" translate-attr="{ placeholder: \'Notes.Help\' }" class="item-notes" ng-maxlength="120" ng-model="vm.item.dimInfo.notes" ng-model-options="{ debounce: 250 }" ng-change="vm.updateNote()"></textarea></form>',
       '  <span class="item-notes-error" ng-show="notes.data.$error.maxlength" translate="Notes.Error"></span>',
-      '  <form ng-if="vm.item.lockable" name="dtrReview"><div class="item-rating-container"><select class="item-dtr-rating-input" name="dtrRating" ng-model="vm.item.dimInfo.ranking"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select><textarea name="data" translate-attr="{ placeholder: \'DtrReview.Help\' }" class="item-dtr-review" ng-maxlength="120" ng-model="vm.item.dimInfo.userReview" ng-model-options="{ debounce: 250 }" ng-change="vm.submitReview()"></textarea></div></form>',
+      '  <form ng-if="vm.item.lockable" name="dtrReview" ng-submit="vm.submitReview()"><div class="item-review-container"><select class="item-dtr-review-input" name="ranking" ng-model="vm.item.dimInfo.ranking"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select><textarea name="data" translate-attr="{ placeholder: \'DtrReview.Help\' }" class="item-dtr-review" ng-maxlength="120" ng-model="vm.item.dimInfo.userReview" ng-model-options="{ debounce: 250 }"></textarea><input class="item-dtr-review-submit" type="submit" translate-attr="{ value: \'DtrReview.Submit\' }" /></div></form>',
       '  <span class="item-dtr-review-error" ng-show="dtrReview.data.$error.maxlength" translate="DtrReview.Error"></span>',
       '  <div class="item-description" ng-if="vm.itemDetails && vm.showDescription" ng-bind="::vm.item.description"></div>',
       '  <div class="item-details" ng-if="vm.item.classified" translate="ItemService.Classified2"></div>',
@@ -105,7 +105,7 @@ function MoveItemProperties() {
 }
 
 
-function MoveItemPropertiesCtrl($sce, $q, dimStoreService, dimItemService, dimSettingsService, ngDialog, $scope, $rootScope, dimFeatureFlags, dimDefinitions) {
+function MoveItemPropertiesCtrl($sce, $q, dimStoreService, dimItemService, dimSettingsService, ngDialog, $scope, $rootScope, dimFeatureFlags, dimDefinitions, dimDestinyTrackerService) {
   var vm = this;
 
   vm.featureFlags = dimFeatureFlags;
@@ -138,7 +138,12 @@ function MoveItemPropertiesCtrl($sce, $q, dimStoreService, dimItemService, dimSe
   };
 
   vm.submitReview = function() {
+    var ranking = vm.item.dimInfo.ranking;
+    var review = vm.item.dimInfo.userReview;
 
+    //bugbug: nothing here now but the post and handle error and yadda yadda
+
+    return false;
   };
 
   vm.setItemState = function setItemState(item, type) {
