5e4ed2cee5908f03e0964e0d7c32be69c5d9b351
Dave Solomon <dave.c.solomon@gmail.com>
2017-03-14 19:01:42 -0400

    Colors were misleading when things got bad.

--- src/scripts/shell/dimAngularFilters.filter.js
+++ src/scripts/shell/dimAngularFilters.filter.js
@@ -220,11 +220,11 @@ mod.filter('dtrRatingColor', function() {
     property = property || 'background-color';
     var color = 0;
     if (value < 2) {
-      return { [property]: 'white' };
-    } else if (value <= 3) {
       color = 0;
+    } else if (value <= 3) {
+      color = 15;
     } else if (value <= 4) {
-      color = 20;
+      color = 30;
     } else if (value <= 4.4) {
       color = 60;
     } else if (value <= 4.8) {
