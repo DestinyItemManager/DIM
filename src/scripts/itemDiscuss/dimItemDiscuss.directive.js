d6f1af356d0bcbb91e5cb356c75cd115e2147e07
Dave Solomon <dave.c.solomon@gmail.com>
2017-04-14 17:37:10 -0400

    Let's get some things showing up in this discussion pane, yeah?

--- src/scripts/itemDiscuss/dimItemDiscuss.directive.js
+++ src/scripts/itemDiscuss/dimItemDiscuss.directive.js
@@ -17,10 +17,17 @@ function ItemDiscussCtrl($scope, $rootScope, toaster, dimItemDiscussService, dim
   var vm = this;
   vm.featureFlags = dimFeatureFlags;
   vm.show = dimItemDiscussService.dialogOpen;
+  vm.dtrRatingOptions = [1, 2, 3, 4, 5];
 
   $rootScope.$on('dim-store-item-discuss', function(event, item) {
     vm.show = true;
 
     vm.item = item.item;
   });
+
+  vm.cancel = function cancel() {
+    vm.loadout = angular.copy(vm.defaults);
+    dimItemDiscussService.dialogOpen = false;
+    vm.show = false;
+  };
 }
