b871343725ee4d9fcc2743aecf04d0acd946fd86
Dave Solomon <dave.c.solomon@gmail.com>
2017-04-14 18:35:55 -0400

    Attach pros and cons from the user.

--- src/scripts/services/dimDestinyTrackerService.factory.js
+++ src/scripts/services/dimDestinyTrackerService.factory.js
@@ -230,6 +230,8 @@ class reviewsFetcher {
     if (userReview) {
       item.userRating = userReview.rating;
       item.userReview = userReview.review;
+      item.userPros = userReview.pros;
+      item.userCons = userReview.cons;
     }
   }
 
