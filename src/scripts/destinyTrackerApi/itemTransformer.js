669c9f95208b1393e850317b2b1d4b4b1acd212a
Dave Solomon <dave.c.solomon@gmail.com>
2017-05-06 14:58:21 -0400

    JSDoc-ing.

--- src/scripts/destinyTrackerApi/itemTransformer.js
+++ src/scripts/destinyTrackerApi/itemTransformer.js
@@ -1,21 +1,45 @@
+/**
+ * Translates items from the objects that DIM has to the form that the DTR API expects.
+ * This is generally tailored towards working with weapons.
+ *
+ * @class ItemTransformer
+ */
 class ItemTransformer {
+  /**
+   * Translate a DIM weapon item into the basic form that the DTR understands a weapon to contain.
+   * This does not contain personally-identifying information.
+   *
+   * @param {dimItem} weapon
+   * @returns {dtrWeapon}
+   *
+   * @memberof ItemTransformer
+   */
   translateToDtrWeapon(weapon) {
     return {
       referenceId: weapon.hash,
-      roll: this.getDtrRoll(weapon)
+      roll: this._getDtrRoll(weapon)
     };
   }
 
+  /**
+   * Get the roll and perks for the selected DIM item (to send to the DTR API).
+   * Will contain personally-identifying information.
+   *
+   * @param {dimItem} weapon
+   * @returns {dtrWeaponWithIdentifyingInfo}
+   *
+   * @memberof ItemTransformer
+   */
   getRollAndPerks(weapon) {
     return {
-      roll: this.getDtrRoll(weapon),
-      selectedPerks: this.getDtrPerks(weapon),
+      roll: this._getDtrRoll(weapon),
+      selectedPerks: this._getDtrPerks(weapon),
       referenceId: weapon.hash,
       instanceId: weapon.id,
     };
   }
 
-  getDtrPerks(weapon) {
+  _getDtrPerks(weapon) {
     if (!weapon.talentGrid) {
       return null;
     }
@@ -23,7 +47,7 @@ class ItemTransformer {
     return weapon.talentGrid.dtrPerks;
   }
 
-  getDtrRoll(weapon) {
+  _getDtrRoll(weapon) {
     if (!weapon.talentGrid) {
       return null;
     }
