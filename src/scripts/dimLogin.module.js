8c98498baa5be1075e2e6bc7ebd4d96ac3a2f59c
Ben Hollis <ben@benhollis.net>
2017-06-29 00:55:06 -0700

    Move authReturn and login into their own cleaner modules

--- src/scripts/dimLogin.module.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-import { OAuthService } from './oauth/oauth.service';
-import { OAuthTokenService } from './oauth/oauth-token.service';
-
-angular.module('dimLogin', [])
-  .service('OAuthTokenService', OAuthTokenService)
-  .service('OAuthService', OAuthService);
