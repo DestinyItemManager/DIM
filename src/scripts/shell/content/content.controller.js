ef54f915ba3f29c86a8f4d78b704e7601b67650f
Ben Hollis <ben@benhollis.net>
2017-07-02 16:41:03 -0700

    Replace tagging hotkeys

--- src/scripts/shell/content/content.controller.js
+++ src/scripts/shell/content/content.controller.js
@@ -64,7 +64,7 @@ export default class ContentController {
       }
     });
 
-    if (vm.featureFlags.tagsEnabled) {
+    if ($featureFlags.tagsEnabled) {
       dimSettingsService.itemTags.forEach((tag) => {
         if (tag.hotkey) {
           hotkeys.add({
