d1812892bf60561bf0ec608a3428b9af60592e9d
Ben Hollis <ben@benhollis.net>
2017-07-09 11:57:06 -0700

    I misunderstood how GA custom dimensions work

--- src/scripts/google.js
+++ src/scripts/google.js
@@ -1,8 +1,8 @@
 window.ga = window.ga || function(...args) { (ga.q = ga.q || []).push(args); }; ga.l = Number(new Date());
 
-ga('create', 'UA-60316581-1', document.location.hostname);
-ga('set', 'DIMVersion', $DIM_VERSION);
-ga('set', 'DIMFlavor', $DIM_FLAVOR);
+ga('create', 'UA-60316581-1', 'auto');
+ga('set', 'dimension1', $DIM_VERSION);
+ga('set', 'dimension2', $DIM_FLAVOR);
 
 // If we're hooked into the router, let it send pageviews instead
 if (!$featureFlags.googleAnalyticsForRouter) {
