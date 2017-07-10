100ee871088bb7e2a55af1de5b8337bd18619f8e
delphiactual <delphi.actual@gmail.com>
2017-07-10 17:57:45 -0600

    added keys from b.net
    to avoid loading definitions inside settingsCtrl I pulled these strings
    from b.net manually...

--- src/scripts/settings/settings.component.js
+++ src/scripts/settings/settings.component.js
@@ -29,6 +29,41 @@ export function SettingsController(loadingTracker, dimSettingsService, $scope, d
   vm.vaultColOptions = _.range(5, 21).map((num) => ({ id: num, name: $translate.instant('Settings.ColumnSize', { num }) }));
   vm.vaultColOptions.unshift({ id: 999, name: $translate.instant('Settings.ColumnSizeAuto') });
 
+  vm.filters = {};
+
+  vm.filters.vendors = {
+    FWC: $translate.instant('Filter.Vendors.FWC'),
+    DO: $translate.instant('Filter.Vendors.DO'),
+    NM: $translate.instant('Filter.Vendors.NM'),
+    Speaker: $translate.instant('Filter.Vendors.Speaker'),
+    Shipwright: $translate.instant('Filter.Vendors.Shipwright'),
+    CQ: $translate.instant('Filter.Vendors.CQ'),
+    EV: $translate.instant('Filter.Vendors.EV'),
+    Gunsmith: $translate.instant('Filter.Vendors.Gunsmith'),
+  };
+
+  vm.filters.releases = {
+    Vanilla: $translate.instant('Filter.Releases.Vanilla'),
+    tTK: $translate.instant('Filter.Releases.tTK'),
+    RoI: $translate.instant('Filter.Releases.RoI')
+  };
+
+  vm.filters.activities = {
+    QW: $translate.instant('Filter.Activities.QW'),
+    IB: $translate.instant('Filter.Activities.IB'),
+    VoG: $translate.instant('Filter.Activities.VoG'),
+    CE: $translate.instant('Filter.Activities.CE'),
+    PoE: $translate.instant('Filter.Activities.PoE'),
+    ToO: $translate.instant('Filter.Activities.ToO'),
+    CoE: $translate.instant('Filter.Activities.CoE'),
+    KF: $translate.instant('Filter.Activities.KF'),
+    SRL: $translate.instant('Filter.Activities.SRL'),
+    CD: $translate.instant('Filter.Activities.CD'),
+    AF: $translate.instant('Filter.Activities.AF'),
+    WotM: $translate.instant('Filter.Activities.WotM'),
+    Dawning: $translate.instant('Filter.Activities.Dawning'),
+    AoT: $translate.instant('Filter.Activities.AoT')
+  };
   vm.languageOptions = {
     de: 'Deutsch',
     en: 'English',
