c65b971f30426597c5ad3281795d2875958e44f2
Jacob Roman <jacob.roman@deque.com>
2017-07-27 16:37:23 -0400

    Some small changes updated JS and removed cookie link

--- destinyitemmanager.com/scripts/main.js
+++ destinyitemmanager.com/scripts/main.js
@@ -1,21 +1,15 @@
 'use strict';
 
-var helloWorld = function helloWorld() {
-  return console.log('Hello World');
-};
-helloWorld();
+const mobileMenu = document.querySelector('.mobile-menu');
+const slidingPanelScreen = document.querySelector('.sliding-panel-fade-screen');
+const slidingPanelMenu = document.querySelector('.sliding-panel-content');
 
-var mobileMenu = document.querySelector('.mobile-menu');
-var slidingPanelScreen = document.querySelector('.sliding-panel-fade-screen');
-var slidingPanelMenu = document.querySelector('.sliding-panel-content');
-
-mobileMenu.addEventListener('click', function () {
+mobileMenu.addEventListener('click', () => {
   slidingPanelScreen.classList.add('is-visible');
   slidingPanelMenu.classList.add('is-visible');
 });
 
-slidingPanelScreen.addEventListener('click', function () {
+slidingPanelScreen.addEventListener('click', () => {
   slidingPanelScreen.classList.remove('is-visible');
   slidingPanelMenu.classList.remove('is-visible');
 });
-//# sourceMappingURL=main.js.map
