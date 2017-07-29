c05aa398ae3b7b1cb920b2dacf6826e8b692ea27
Ben Hollis <ben@benhollis.net>
2017-07-29 10:34:45 -0700

    Enable wasm in release

--- config/webpack.js
+++ config/webpack.js
@@ -195,7 +195,7 @@ module.exports = (env) => {
         '$featureFlags.gdrive': JSON.stringify(true),
         '$featureFlags.debugSync': JSON.stringify(false),
         // Use a WebAssembly version of SQLite, if possible (this crashes on Android Chrome right now)
-        '$featureFlags.wasm': `${JSON.stringify(env !== 'release')} && !window.navigator.userAgent.includes("Android")`,
+        '$featureFlags.wasm': '!window.navigator.userAgent.includes("Android")',
         // Enable color-blind a11y
         '$featureFlags.colorA11y': JSON.stringify(env !== 'release'),
         // Whether to log page views for router events
