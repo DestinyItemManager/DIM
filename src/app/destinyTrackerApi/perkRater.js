817c5d8009b97c472809f864b4b8139155ea1e9e
Dave Solomon <dave.c.solomon@gmail.com>
2017-09-04 11:01:44 -0400

    oh no copypasta

--- src/app/destinyTrackerApi/perkRater.js
+++ src/app/destinyTrackerApi/perkRater.js
@@ -3,15 +3,15 @@ import _ from 'underscore';
 /**
  * Rate perks on an item (based off of its attached user reviews).
  *
- * @class ReviewDataCache
+ * @class PerkRater
  */
 class PerkRater {
   /**
-     * Rate the perks on an item based off of its attached user reviews.
-     *
-     * @param {any} item
-     * @memberof PerkRater
-     */
+   * Rate the perks on an item based off of its attached user reviews.
+   *
+   * @param {any} item
+   * @memberof PerkRater
+   */
   ratePerks(item) {
     if ((!item.reviews) ||
         (!item.reviews.length)) {
