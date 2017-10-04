5cc381f32b8afdda0627a12a67030cd74ad87b45
Dave Solomon <dave.c.solomon@gmail.com>
2017-10-03 23:15:47 -0400

    Commented because that was inscrutable for a minute.

--- src/app/destinyTrackerApi/d2-reviewsFetcher.js
+++ src/app/destinyTrackerApi/d2-reviewsFetcher.js
@@ -74,7 +74,7 @@ class D2ReviewsFetcher {
 
     // TODO: reviewData has two very different shapes depending on whether it's from cache or from the service
     item.totalReviews = reviewData.totalReviews === undefined ? reviewData.ratingCount : reviewData.totalReviews;
-    item.writtenReviews = _.filter(reviewData.reviews, 'review');
+    item.writtenReviews = _.filter(reviewData.reviews, 'text'); // only attach reviews with text associated
 
     this._sortAndIgnoreReviews(item);
 
