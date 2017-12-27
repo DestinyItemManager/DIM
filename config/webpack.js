7acafec30c639e422a242a6d00fc6c9c6b3c86ab
Ben Hollis <ben@benhollis.net>
2017-12-26 19:28:01 -0800

    Set up webpack for React and TypeScript

--- config/webpack.js
+++ config/webpack.js
@@ -94,6 +94,21 @@ module.exports = (env) => {
             ],
             fallback: 'style-loader'
           })
+        },
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        {
+          test: /\.tsx?$/,
+          loader: "awesome-typescript-loader",
+          options: {
+            useBabel: true,
+            useCache: true
+          }
+        },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
         }
       ],
 
@@ -103,7 +118,7 @@ module.exports = (env) => {
     },
 
     resolve: {
-      extensions: ['.js', '.json'],
+      extensions: ['.js', '.json', '.ts', '.tsx', '.jsx'],
 
       alias: {
         app: path.resolve('./src')
