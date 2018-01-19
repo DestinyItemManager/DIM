e3cde8ba4362d603b200b3ccdbb586d6e8bec20f
Ben Hollis <ben@benhollis.net>
2018-01-18 22:08:57 -0800

    Forgot to commit

---
+++ src/app/rx-operators.ts
@@ -0,0 +1,12 @@
+// Spelling out each operator we need helps save on bundle size, but it's a pain
+// to type everywhere. So we keep them all here and you just have to import rx-operators.
+import 'rxjs/add/observable/defer';
+import 'rxjs/add/observable/fromEventPattern';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/publishReplay';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/subscribeOn';
