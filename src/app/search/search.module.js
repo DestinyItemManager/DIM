f0d188d14515dc8929e7ac4bbd61534bf212296f
Ben Hollis <ben@benhollis.net>
2018-01-22 19:59:42 -0800

    Revert "Revert "Revert "Revert "Upgrade to uglify-es""""
    This reverts commit 0fe536fd580b43e70f9a5ec589d32e7cfdbeaaec.

--- src/app/search/search.module.js
+++ src/app/search/search.module.js
@@ -3,12 +3,14 @@ import angular from 'angular';
 import { SearchFilterComponent } from './search-filter.component';
 import { FilterLinkComponent } from './filter-link.component';
 
+// a simple service to share the search query among components
+function SearchService() {
+  return { query: '' };
+}
+
 export default angular
   .module('searchModule', [])
   .component('dimSearchFilter', SearchFilterComponent)
   .component('dimFilterLink', FilterLinkComponent)
-  // a simple service to share the search query among components
-  .service('dimSearchService', () => {
-    return { query: '' };
-  })
+  .service('dimSearchService', SearchService)
   .name;
