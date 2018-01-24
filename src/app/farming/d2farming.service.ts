846f42cc3639b0eea73f6136000e8c0d8d7b0c18
Ben Hollis <ben@benhollis.net>
2018-01-23 23:00:34 -0800

    Typescriptify D2FarmingService

--- src/app/farming/d2farming.service.ts
+++ src/app/farming/d2farming.service.ts
@@ -1,23 +1,30 @@
-import _ from 'underscore';
+import * as _ from 'underscore';
 import { REP_TOKENS } from './rep-tokens';
+import { DimStore } from '../inventory/store/d2-store-factory.service';
+import { DimItem } from '../inventory/store/d2-item-factory.service';
+import { DimInventoryBucket, BucketsService } from '../destiny2/d2-buckets.service';
+import { StoreServiceType } from '../inventory/d2-stores.service';
+import { IIntervalService, IQService, IRootScopeService } from 'angular';
+import { DestinyAccount } from '../accounts/destiny-account.service';
 
 /**
  * A service for "farming" items by moving them continuously off a character,
  * so that they don't go to the Postmaster.
  */
-export function D2FarmingService($rootScope,
-                        $q,
-                        dimItemService,
-                        D2StoresService,
-                        $interval,
-                        toaster,
-                        $i18next,
-                        D2BucketsService,
-                        dimSettingsService) {
+export function D2FarmingService(
+  $rootScope: IRootScopeService,
+  $q: IQService,
+  dimItemService,
+  D2StoresService: StoreServiceType,
+  $interval: IIntervalService,
+  toaster,
+  $i18next,
+  D2BucketsService: BucketsService,
+  dimSettingsService
+) {
   'ngInject';
 
   let intervalId;
-  let cancelReloadListener;
   let subscription;
 
   const outOfSpaceWarning = _.throttle((store) => {
@@ -39,35 +46,34 @@ export function D2FarmingService($rootScope,
     movingItems: false,
     makingRoom: false,
     // Move all items on the selected character to the vault.
-    moveItemsToVault: function(store, items, makeRoomBuckets) {
+    moveItemsToVault(store: DimStore, items: DimItem[], makeRoomBuckets: DimInventoryBucket[]) {
       const reservations = {};
       // reserve one space in the active character
       reservations[store.id] = {};
       makeRoomBuckets.forEach((bucket) => {
-        reservations[store.id][bucket.type] = 1;
+        reservations[store.id][bucket.type!] = 1;
       });
 
       return _.reduce(items, (promise, item) => {
         // Move a single item. We do this as a chain of promises so we can reevaluate the situation after each move.
         return promise
           .then(() => {
-            const vault = D2StoresService.getVault();
+            const vault = D2StoresService.getVault()!;
             const vaultSpaceLeft = vault.spaceLeftForItem(item);
             if (vaultSpaceLeft <= 1) {
               // If we're down to one space, try putting it on other characters
-              const otherStores = _.select(D2StoresService.getStores(),
-                                            (s) => !s.isVault && s.id !== store.id);
-              const otherStoresWithSpace = _.select(otherStores, (store) => store.spaceLeftForItem(item));
+              const otherStores = D2StoresService.getStores().filter((s) => !s.isVault && s.id !== store.id);
+              const otherStoresWithSpace = otherStores.filter((store) => store.spaceLeftForItem(item));
 
               if (otherStoresWithSpace.length) {
                 if ($featureFlags.debugMoves) {
-                  console.log("Farming initiated move:", item.amount, item.name, item.type, 'to', otherStoresWithSpace[0].name, 'from', D2StoresService.getStore(item.owner).name);
+                  console.log("Farming initiated move:", item.amount, item.name, item.type, 'to', otherStoresWithSpace[0].name, 'from', D2StoresService.getStore(item.owner)!.name);
                 }
                 return dimItemService.moveTo(item, otherStoresWithSpace[0], false, item.amount, items, reservations);
               }
             }
             if ($featureFlags.debugMoves) {
-              console.log("Farming initiated move:", item.amount, item.name, item.type, 'to', vault.name, 'from', D2StoresService.getStore(item.owner).name);
+              console.log("Farming initiated move:", item.amount, item.name, item.type, 'to', vault.name, 'from', D2StoresService.getStore(item.owner)!.name);
             }
             return dimItemService.moveTo(item, vault, false, item.amount, items, reservations);
           })
@@ -83,15 +89,15 @@ export function D2FarmingService($rootScope,
     },
     // Ensure that there's one open space in each category that could
     // hold an item, so they don't go to the postmaster.
-    makeRoomForItems: function(store) {
+    makeRoomForItems(store: DimStore) {
       return getMakeRoomBuckets().then((makeRoomBuckets) => {
         // If any category is full, we'll move one aside
-        let itemsToMove = [];
+        let itemsToMove: DimItem[] = [];
         makeRoomBuckets.forEach((makeRoomBucket) => {
           const items = store.buckets[makeRoomBucket.id];
           if (items.length > 0 && items.length >= makeRoomBucket.capacity) {
             // We'll move the lowest-value item to the vault.
-            const itemToMove = _.min(_.select(items, { equipped: false, notransfer: false }), (i) => {
+            const itemToMove = _.min(items.filter((i) => !i.equipped && !i.notransfer), (i) => {
               let value = {
                 Common: 0,
                 Uncommon: 1,
@@ -101,11 +107,11 @@ export function D2FarmingService($rootScope,
               }[i.tier];
               // And low-stat
               if (i.primStat) {
-                value += i.primStat.value / 1000.0;
+                value += i.primStat.value / 1000;
               }
               return value;
             });
-            if (itemToMove !== Infinity) {
+            if (!_.isNumber(itemToMove)) {
               itemsToMove.push(itemToMove);
             }
           }
@@ -126,7 +132,7 @@ export function D2FarmingService($rootScope,
           });
       });
     },
-    start: function(account, storeId) {
+    start(account: DestinyAccount, storeId: string) {
       if (!this.active) {
         this.active = true;
         this.itemsMoved = 0;
@@ -150,12 +156,9 @@ export function D2FarmingService($rootScope,
         }, 60000);
       }
     },
-    stop: function() {
+    stop() {
       if (intervalId) {
         $interval.cancel(intervalId);
-      }
-      if (cancelReloadListener) {
-        cancelReloadListener();
       }
       if (subscription) {
         subscription.unsubscribe();
@@ -166,4 +169,3 @@ export function D2FarmingService($rootScope,
     }
   };
 }
-
