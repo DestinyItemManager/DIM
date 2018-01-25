4ddf602418f9ff0e725398d90a5c0c90fb74f703
Ben Hollis <ben@benhollis.net>
2018-01-24 22:33:20 -0800

    Revert "Revert "Revert UglifyES, again""
    This reverts commit 70b1abe39788c191b993bd8f65c00cd93de12cd8.

--- config/webpack.js
+++ config/webpack.js
@@ -7,7 +7,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
 // const Visualizer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -283,21 +282,11 @@ module.exports = (env) => {
     // The sql.js library doesnt work at all (reports no tables) when minified,
     // so we exclude it from the regular minification
     // FYI, uglification runs on final chunks rather than individual modules
-    config.plugins.push(new UglifyJsPlugin({
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
       exclude: [/-sqlLib-/, /sql-wasm/], // ensure the sqlLib chunk doesnt get minifed
-      uglifyOptions: {
-        compress: {
-          ecma: 6,
-          passes: 2,
-          inline: 1
-        },
-        output: {
-          ecma: 6
-        }
-      },
-      sourceMap: true,
-      cache: true,
-      parallel: true
+      compress: { warnings: false },
+      output: { comments: false },
+      sourceMap: true
     }));
 
     // Generate a service worker
