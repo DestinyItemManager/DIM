07decf9ea9c286ece304e674501f99c7b1c08703
Ben Hollis <ben@benhollis.net>
2018-02-13 23:49:40 -0800

    Remove unused state

--- src/app/state.js
+++ src/app/state.js
@@ -1,5 +1,11 @@
 export const dimState = {
-  membershipType: -1,
+  /**
+   * The active account - we put it here to break a circular dependency from
+   * dimPlatformService. It should only be used from bungie-api classes.
+   */
   active: null,
+  /**
+   * Whether we are in debug mode
+   */
   debug: $featureFlags.debugMode
 };
