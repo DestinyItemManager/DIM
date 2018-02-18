0b6b8d6df14a90901aabf0ba4897ec24d0290607
Ben Hollis <ben@benhollis.net>
2018-02-18 11:50:38 -0800

    Hound

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ polyfill({
 });
 
 // https://github.com/timruffles/ios-html5-drag-drop-shim/issues/77
-window.addEventListener('touchmove', () => { return; });
+window.addEventListener('touchmove', () => { });
 
 if ($DIM_FLAVOR !== 'dev' && navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js')
