23e8be2bd367abb05f7ec9ac9b7d4390e61af92d
Ben Hollis <ben@benhollis.net>
2018-02-19 15:24:23 -0800

    Fix filter page
    Fixes DIM-HN

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,12 +1,13 @@
 import { module } from 'angular';
 
-import { SettingsComponent } from './settings.component';
+import { SettingsComponent, SettingsController } from './settings.component';
 import SortOrderEditor from './sort-order-editor';
 import { react2angular } from 'react2angular';
 import { settings } from '../settings/settings';
 
 export default module('settingsModule', [])
   .component('settings', SettingsComponent)
+  .controller('dimSettingsCtrl', SettingsController)
   .component('sortOrderEditor', react2angular(SortOrderEditor, ['order', 'onSortOrderChanged']))
   .config(($stateProvider) => {
     'ngInject';
