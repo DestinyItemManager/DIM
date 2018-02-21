f5350811d5796a0d9f27ebc65ff5ce802355bb9a
Dave Solomon <dave.c.solomon@gmail.com>
2018-02-20 21:56:22 -0500

    Getting the rest of the API interface classes down.

--- src/app/destinyTrackerApi/d2-dtr-class-defs.ts
+++ src/app/destinyTrackerApi/d2-dtr-class-defs.ts
@@ -1,6 +1,54 @@
+export interface DtrVote {
+  upvotes: number;
+  downvotes: number;
+  total: number;
+  score: number;
+}
+
 export interface DtrItem {
   referenceId: number;
   instanceId?: string;
   attachedMods?: any[];
   selectedPerks?: any[];
+}
+
+export interface DtrItemWithVotes extends DtrItem {
+  votes?: DtrVote;
+}
+
+export interface DtrReviewRequest extends DtrItem {
+  selectedPerks?: number[];
+}
+
+export interface DtrReviewer {
+  membershipType: number;
+  membershipId: number;
+  displayName: string;
+}
+
+export interface DtrUserReviewRequest extends DtrItem {
+  reviewer: DtrReviewer;
+  voted: number;
+  pros: string;
+  cons: string;
+  text: string;
+}
+
+export interface DtrUserReviewResponse extends DtrUserReviewRequest {
+  id: string;
+  timestamp: Date;
+  isReviewer: boolean;
+  isHighlighted: boolean;
+  instanceId?: string;
+}
+
+export interface DtrReviewResponse extends DtrItem {
+  votes?: DtrVote;
+  totalReviews: number;
+  reviews: DtrUserReviewResponse[];
+}
+export interface DtrReviewReport {
+  reviewId: string;
+  reporter: DtrReviewer;
+  text: string;
 }
