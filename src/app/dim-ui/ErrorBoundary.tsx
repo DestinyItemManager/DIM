948f137fb27d01c745bb2395e3fa1cd841948ca3
Ben Hollis <ben@benhollis.net>
2018-05-19 22:38:40 -0700

    Add back emotes (via PlugSets) to collections page

---
+++ src/app/dim-ui/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { reportException } from '../exceptions';
+import { t } from 'i18next';
+import './ErrorBoundary.scss';
+
+interface Props {
+  name: string;
+}
+
+interface State {
+  error?: Error;
+}
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  componentDidCatch(error: Error) {
+    this.setState({ error });
+    reportException(this.props.name, error);
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div className="dim-error">
+          <h2>{t('ErrorBoundary.Title')}</h2>
+          <div>{this.state.error.message}</div>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
