eb2a532e15c656007b2a34cf2b423f1591294aea
Ben Hollis <ben@benhollis.net>
2018-06-16 11:31:09 -0700

    Generate content security policy config

---
+++ config/content-security-policy.js
@@ -0,0 +1,98 @@
+const csp = require('content-security-policy-builder');
+
+const SELF = "'self'";
+
+/**
+ * Generate a Content Security Policy directive for a particular DIM environment (beta, release)
+ */
+module.exports = function csp(env) {
+  const baseCSP = {
+    defaultSrc: ['none'],
+    scriptSrc: [
+      SELF,
+      // For Webpack?
+      "'unsafe-eval'",
+      "'unsafe-inline'",
+      'data:',
+      // Include a snippet of inline scripts
+      "'report-sample'",
+      // Google API (Drive)
+      'https://apis.google.com',
+      'https://www.google-analytics.com',
+      // Twitter Widget
+      'https://platform.twitter.com',
+      'https://cdn.syndication.twimg.com',
+    ],
+    styleSrc: [
+      SELF,
+      // For Webpack's inserted styles
+      "'unsafe-inline'",
+      // Google Fonts
+      "https://fonts.googleapis.com/css",
+      // Twitter Widget
+      "https://platform.twitter.com/css/",
+      "https://*.twimg.com/"
+    ],
+    connectSrc: [
+      SELF,
+      // Google Analytics
+      "https://www.google-analytics.com",
+      // Bungie.net API
+      "https://www.bungie.net",
+      // DTR Reviews API
+      "https://reviews-api.destinytracker.net",
+      "https://db-api.destinytracker.com"
+    ],
+    imgSrc: [
+      SELF,
+      // Webpack inlines some images
+      "data:",
+      // Bungie.net images
+      "https://www.bungie.net",
+      // Google analytics tracking
+      "https://ssl.google-analytics.com",
+      "https://www.google-analytics.com",
+      "https://csi.gstatic.com",
+      // OpenCollective backers
+      "https://opencollective.com",
+      // Twitter Widget
+      "https://syndication.twitter.com",
+      "https://platform.twitter.com",
+      "https://*.twimg.com/"
+    ],
+    fontSrc: [
+      SELF,
+      // Google Fonts
+      "https://fonts.gstatic.com"
+    ],
+    childSrc: [
+      SELF,
+      // Google Login
+      "https://accounts.google.com",
+      "https://content.googleapis.com"
+    ],
+    frameSrc: [
+      // Google Login
+      "https://accounts.google.com",
+      "https://content.googleapis.com",
+      // Twitter Widget
+      "https://syndication.twitter.com/",
+      "https://platform.twitter.com/"
+    ],
+    objectSrc: SELF,
+    // Web app manifest
+    manifestSrc: SELF
+  }
+
+  // Turn on reporting to sentry.io on beta only
+  if (env === 'beta') {
+    baseCSP.reportUri = "https://sentry.io/api/279673/csp-report/?sentry_key=1367619d45da481b8148dd345c1a1330"
+    baseCSP.connectSrc.push("https://sentry.io/api/279673/store/")
+  } else if (env === 'release') {
+    // Allow release to load updated classified item definitions and images from beta
+    baseCSP.connectSrc.push("https://beta.destinyitemmanager.com");
+    baseCSP.imgSrc.push("https://beta.destinyitemmanager.com");
+  }
+
+  return builder(baseCSP);
+}
