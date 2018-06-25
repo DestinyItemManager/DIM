8a843d3a375133706bf0f1d0e10aa0bcdea0b900
Ben Hollis <ben@benhollis.net>
2018-06-24 18:54:10 -0700

    Fix loading animation

--- src/app/dim-ui/Loading.tsx
+++ src/app/dim-ui/Loading.tsx
@@ -11,15 +11,15 @@ export function Loading() {
         x="0px"
         y="0px"
         viewBox="0 0 500 500"
-        enable-background="new 0 0 500 500"
+        enableBackground="new 0 0 500 500"
       >
         <polyline
           className="outer"
           fill="none"
           stroke="#efefef"
-          stroke-width="63"
-          stroke-linecap="square"
-          stroke-miterlimit="10"
+          strokeWidth="63"
+          strokeLinecap="square"
+          strokeMiterlimit="10"
           points="249.3,67.8
           431.7,250.2 249.3,432.6 66.8,250.2 157.3,159.4 "
         />
@@ -28,9 +28,9 @@ export function Loading() {
           className="inner"
           fill="none"
           stroke="#efefef"
-          stroke-width="63"
-          stroke-linecap="square"
-          stroke-miterlimit="10"
+          strokeWidth="63"
+          strokeLinecap="square"
+          strokeMiterlimit="10"
           x1="249.4"
           y1="251.5"
           x2="249.1"
