15464b3c94370816dc6eca6076ead28698d70d75
Ben Hollis <ben@benhollis.net>
2018-07-01 19:23:46 -0700

    Update Progress service to not use $q

--- src/app/progress/Progress.tsx
+++ src/app/progress/Progress.tsx
@@ -66,7 +66,7 @@ interface State {
   currentCharacterId: string;
 }
 
-export class Progress extends React.Component<Props, State> {
+export default class Progress extends React.Component<Props, State> {
   subscription: Subscription;
   mediaQuerySubscription: Subscription;
   private $scope = $rootScope.$new(true);
--- src/app/progress/routes.ts
+++ src/app/progress/routes.ts
@@ -1,4 +1,4 @@
-import { Progress } from "./Progress";
+import Progress from "./Progress";
 import { ReactStateDeclaration } from "@uirouter/react";
 
 export const states: ReactStateDeclaration[] = [{
