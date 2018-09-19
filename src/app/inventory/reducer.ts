99aaddb39670cbf393ad75c2ac4cba28cffcb0bb
Ben Hollis <ben@benhollis.net>
2018-09-19 00:37:44 -0700

    Only clear inventory on real platform change

--- src/app/inventory/reducer.ts
+++ src/app/inventory/reducer.ts
@@ -4,7 +4,6 @@ import { ActionType, getType } from 'typesafe-actions';
 import { DimStore } from './store-types';
 import { InventoryBuckets } from './inventory-buckets';
 import { DimItemInfo } from './dim-item-info';
-import { setCurrentAccount } from '../accounts/actions';
 import { AccountsAction } from '../accounts/reducer';
 import { RootState } from '../store/reducers';
 import { createSelector } from 'reselect';
@@ -59,11 +58,6 @@ export const inventory: Reducer<InventoryState, InventoryAction | AccountsAction
   action: InventoryAction | AccountsAction
 ) => {
   switch (action.type) {
-    // When the account changes, clear the inventory state
-    case getType(setCurrentAccount):
-      return {
-        ...initialInventoryState
-      };
     case getType(actions.update):
       // TODO: we really want to decompose these, drive out all deep mutation
       // TODO: mark DimItem, DimStore properties as Readonly
