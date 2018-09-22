776b5a17f21fbc798b67f1d347d6b68191875e98
Ben Hollis <ben@benhollis.net>
2018-09-22 09:46:25 -0700

    Drive CollapsibleTitle through redux

--- src/app/settings/actions.ts
+++ src/app/settings/actions.ts
@@ -1,10 +1,8 @@
 import { Settings } from './settings';
-import { createStandardAction, createAction } from 'typesafe-actions';
+import { createStandardAction } from 'typesafe-actions';
 
 /** Bulk update settings after they've been loaded. */
 export const loaded = createStandardAction('settings/LOADED')<Settings>();
 
-/** This one seems a bit like cheating, but it lets us set a specific property. */
-export const set = createAction('settings/SET', (resolve) => {
-  return (property: keyof Settings, value: any) => resolve({ property, value });
-});
+/** Update a collapsible section */
+export const toggleCollapsedSection = createStandardAction('settings/COLLAPSIBLE')<string>();
--- src/app/settings/reducer.ts
+++ src/app/settings/reducer.ts
@@ -4,8 +4,9 @@ import * as actions from './actions';
 import { ActionType, getType } from 'typesafe-actions';
 import { RootState } from '../store/reducers';
 
-export const characterOrderSelector = (state: RootState) =>
-  (state.settings.settings as Settings).characterOrder;
+export const settingsSelector = (state: RootState) => state.settings.settings as Settings;
+
+export const characterOrderSelector = (state: RootState) => settingsSelector(state).characterOrder;
 
 export interface SettingsState {
   readonly settings: Settings | {};
@@ -22,18 +23,27 @@ export const settings: Reducer<SettingsState, SettingsAction> = (
   state: SettingsState = initialSettingsState,
   action: SettingsAction
 ) => {
+  const settings = state.settings as Settings;
+
   switch (action.type) {
     case getType(actions.loaded):
       return {
+        ...state,
         settings: action.payload
       };
-    case getType(actions.set):
+
+    case getType(actions.toggleCollapsedSection):
+      settings.collapsedSections = {
+        ...settings.collapsedSections,
+        [action.payload.sectionId]: !settings.collapsedSections[action.payload.sectionId]
+      };
+
+      // Ugh
+      settings.save();
+
       return {
         ...state,
-        settings: {
-          ...state.settings,
-          [action.payload.property]: action.payload.value
-        }
+        settings
       };
     default:
       return state;
