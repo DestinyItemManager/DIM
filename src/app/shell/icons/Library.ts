613e81f7226d33885bebd29f80f49d761a774281
Liam Dawson <liam@ldaws.com>
2018-09-29 11:04:29 +1000

    remove fa from activities page

--- src/app/shell/icons/Library.ts
+++ src/app/shell/icons/Library.ts
@@ -5,8 +5,10 @@ import {
   faCog,
   faSearch,
   faSignOutAlt,
+  faStar,
   faTimes
 } from '@fortawesome/free-solid-svg-icons';
+import { faMinusSquare, faPlusSquare } from '@fortawesome/free-regular-svg-icons';
 
 library.add(faArrowCircleUp, faBars, faCog, faSearch, faSignOutAlt, faTimes);
 
@@ -14,7 +16,10 @@ export {
   faArrowCircleUp as updateIcon,
   faBars as menuIcon,
   faCog as settingsIcon,
+  faMinusSquare as collapseIcon,
+  faPlusSquare as expandIcon,
   faSearch as searchIcon,
   faSignOutAlt as signOutIcon,
+  faStar as starIcon,
   faTimes as closeIcon
 };
