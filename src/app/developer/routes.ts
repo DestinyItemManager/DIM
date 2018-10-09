bcf6922143dfeeddc82cf77ce3438ad956a7cec3
Ben Hollis <ben@benhollis.net>
2018-10-08 23:54:53 -0700

    Port developer page to React

--- src/app/developer/routes.ts
+++ src/app/developer/routes.ts
@@ -1,16 +1,13 @@
 import { ReactStateDeclaration } from '@uirouter/react';
-import { DeveloperComponent } from './developer.component';
-import { angular2react } from 'angular2react';
-import { lazyInjector } from '../../lazyInjector';
+import Developer from './Developer';
 
-export const states: ReactStateDeclaration[] = [
-  {
-    name: 'developer',
-    url: '/developer',
-    component: angular2react(
-      'dimDeveloper',
-      DeveloperComponent,
-      lazyInjector.$injector as angular.auto.IInjectorService
-    )
-  }
-];
+export const states: ReactStateDeclaration[] =
+  $DIM_FLAVOR === 'dev'
+    ? [
+        {
+          name: 'developer',
+          url: '/developer',
+          component: Developer
+        }
+      ]
+    : [];
