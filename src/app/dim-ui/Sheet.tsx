9d3ed43560adc3eb98f20a8615f4f7d5e8e0205b
Ben Hollis <ben@benhollis.net>
2018-10-27 21:37:43 -0700

    Start working on a Sheet component

---
+++ src/app/dim-ui/Sheet.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import './Sheet.scss';
+
+/**
+ * A Sheet is a mobile UI element that comes up from the bottom of the scren, and can be dragged to dismiss.
+ */
+export default class Sheet extends React.Component {
+  render() {
+    return (
+      <div className="sheet">
+        <div className="sheet-handle" />
+        <div className="sheet-contents">
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+          <div>This is content</div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/app/inventory/Inventory.tsx
+++ src/app/inventory/Inventory.tsx
@@ -17,6 +17,7 @@ import LoadoutDrawer from '../loadout/LoadoutDrawer';
 import { Subscriptions } from '../rx-utils';
 import { refresh$ } from '../shell/refresh';
 import Compare from '../compare/Compare';
+import Sheet from '../dim-ui/Sheet';
 
 const D1Farming = angular2react(
   'dimFarming',
@@ -85,6 +86,7 @@ class Inventory extends React.Component<Props> {
         {account.destinyVersion === 1 ? <D1Farming /> : <D2Farming />}
         <ClearNewItems account={account} />
         <RandomLoadoutButton destinyVersion={account.destinyVersion} />
+        <Sheet />
       </>
     );
   }
