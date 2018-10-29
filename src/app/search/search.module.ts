cb275ecf2f252eae435c8ddbec05d83e7fa73a7d
Ben Hollis <ben@benhollis.net>
2018-10-28 22:29:40 -0700

    Remove the filter-link component

--- src/app/search/search.module.ts
+++
@@ -1,5 +0,0 @@
-import { module } from 'angular';
-
-import { FilterLinkComponent } from './filter-link.component';
-
-export default module('searchModule', []).component('dimFilterLink', FilterLinkComponent).name;
