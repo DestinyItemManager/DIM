45af1d01255a2680c35359b6dc3d6c6208fa0ae6
Ben Hollis <ben@benhollis.net>
2018-11-17 21:28:13 -0800

    Guard just a tiny bit

--- src/app/inventory/dimItemService.factory.ts
+++ src/app/inventory/dimItemService.factory.ts
@@ -168,7 +168,7 @@ function ItemService(): ItemServiceType {
           )
         : [];
       // moveAmount could be more than maxStackSize if there is more than one stack on a character!
-      const moveAmount = amount || item.amount;
+      const moveAmount = amount || item.amount || 1;
       let addAmount = moveAmount;
       let removeAmount = moveAmount;
       let removedSourceItem = false;
