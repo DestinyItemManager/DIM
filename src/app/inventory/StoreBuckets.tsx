86f610106db4b8499de98e7352a9700a46fce6a3
Ben Hollis <ben@benhollis.net>
2018-11-24 21:33:15 -0500

    Tone down the changes a bit

--- src/app/inventory/StoreBuckets.tsx
+++ src/app/inventory/StoreBuckets.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { DimStore, DimVault } from './store-types';
+import { DimStore, DimVault, D2Store } from './store-types';
 import StoreBucket from './StoreBucket';
 import { InventoryBucket } from './inventory-buckets';
 import classNames from 'classnames';
@@ -44,14 +44,14 @@ export function StoreBuckets({
       </>
     );
   } else {
-    content = stores.map((store) => (
+    content = stores.map((store, index) => (
       <div
         key={store.id}
         className={classNames('store-cell', {
           vault: store.isVault,
           'no-badge': noBadges
         })}
-        style={store.isDestiny2() && store.color ? bgColor(store.color) : undefined}
+        style={store.isDestiny2() && store.color ? bgColor(store, index) : undefined}
       >
         {(!store.isVault || bucket.vaultBucket) && (
           <StoreBucket bucketId={bucket.id} storeId={store.id} />
@@ -66,6 +66,15 @@ export function StoreBuckets({
   return <div className="store-row items">{content}</div>;
 }
 
-function bgColor(color: DestinyColor) {
-  return { backgroundColor: `rgba(${color.red}, ${color.green}, ${color.blue}, 0.25)` };
+function bgColor(store: D2Store, index: number) {
+  if (index % 2 === 1 && !store.isVault) {
+    return {
+      backgroundColor: `rgba(${store.color.red}, ${store.color.green}, ${store.color.blue}, 0.25)`
+    };
+  } else {
+    return {
+      backgroundColor: `rgba(${store.color.red * 0.95}, ${store.color.green * 0.95}, ${store.color
+        .blue * 0.95}, 0.25)`
+    };
+  }
 }
