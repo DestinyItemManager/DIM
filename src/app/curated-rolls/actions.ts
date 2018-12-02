4a5fbb7bbc20056b5dc47b67cb2839b5ef2168b4
Dave Solomon <dave.c.solomon@gmail.com>
2018-12-02 10:06:13 -0500

    Less comment copypasta.

--- src/app/curated-rolls/actions.ts
+++ src/app/curated-rolls/actions.ts
@@ -1,9 +1,6 @@
 import { createStandardAction } from 'typesafe-actions';
 import { InventoryCuratedRoll } from './curatedRollService';
 
-/**
- * Reflect the old stores service data into the Redux store as a migration aid.
- */
 export const updateCurations = createStandardAction('curations/UPDATE')<{
   curationEnabled: boolean;
   inventoryCuratedRolls: InventoryCuratedRoll[];
