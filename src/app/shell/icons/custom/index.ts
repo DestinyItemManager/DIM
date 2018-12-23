17f1eed0a89b1a9b295e1ec554324ebab0895fbe
Ben Hollis <ben@benhollis.net>
2018-12-22 23:57:05 -0800

    Add class icons to all loadouts

--- src/app/shell/icons/custom/index.ts
+++ src/app/shell/icons/custom/index.ts
@@ -1,3 +1,6 @@
 export { dimPowerIcon } from './Power';
 export { dimPowerAltIcon } from './PowerAlt';
 export { dimEngramIcon } from './Engram';
+export { dimHunterIcon } from './Hunter';
+export { dimWarlockIcon } from './Warlock';
+export { dimTitanIcon } from './Titan';
