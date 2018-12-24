e80c3457b7430caedb021eccabde1de591f67548
Ben Hollis <ben@benhollis.net>
2018-12-24 13:54:54 -0800

    Revert "Revert "Shove the move popup in there""
    This reverts commit b54b9005966f4b78916c3606ff9b7acf49b8b0b7.

--- src/app/shell/Destiny.tsx
+++ src/app/shell/Destiny.tsx
@@ -6,6 +6,7 @@ import { hotkeys } from '../ngimport-more';
 import { t } from 'i18next';
 import { DestinyAccount } from '../accounts/destiny-account.service';
 import { itemTags } from '../inventory/dim-item-info';
+import ItemPopup from '../move-popup/ItemPopup';
 
 interface Props {
   account: DestinyAccount;
@@ -54,6 +55,7 @@ export default class Destiny extends React.Component<Props> {
         <div id="content">
           <UIView />
         </div>
+        <ItemPopup />
         <ManifestProgress destinyVersion={this.props.account.destinyVersion} />
       </>
     );
