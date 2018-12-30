da91f3e5910ea3fad6d96c60a1bea2afa5935715
Ben Hollis <ben@benhollis.net>
2018-12-29 22:10:31 -0800

    Implement actions

---
+++ src/app/item-popup/ItemActions.tsx
@@ -0,0 +1,217 @@
+import * as React from 'react';
+import { DimItem } from '../inventory/item-types';
+import { DimStore } from '../inventory/store-types';
+import { t } from 'i18next';
+import classNames from 'classnames';
+import './ItemActions.scss';
+import { hideItemPopup } from './item-popup';
+import { moveItemTo, consolidate, distribute } from '../inventory/dimItemMoveService.factory';
+import { ngDialog } from '../ngimport-more';
+import { RootState } from '../store/reducers';
+import { storesSelector } from '../inventory/reducer';
+import { connect } from 'react-redux';
+
+interface ProvidedProps {
+  item: DimItem;
+  amount: number;
+}
+
+interface StoreProps {
+  store?: DimStore;
+  stores: DimStore[];
+}
+
+function mapStateToProps(state: RootState, { item }: ProvidedProps): StoreProps {
+  return {
+    store: storesSelector(state).find((s) => s.id === item.owner),
+    stores: storesSelector(state)
+  };
+}
+
+type Props = ProvidedProps & StoreProps;
+
+class ItemActions extends React.Component<Props> {
+  render() {
+    const { item, store, stores } = this.props;
+
+    if (!store) {
+      return null;
+    }
+
+    const canConsolidate =
+      !item.notransfer && item.location.hasTransferDestination && item.maxStackSize > 1;
+    const canDistribute = item.isDestiny1() && !item.notransfer && item.maxStackSize > 1;
+
+    /*
+        <dim-move-amount
+          ng-if="maximum > 1 && !item.notransfer && !item.uniqueStack"
+          amount="moveAmount"
+          maximum="maximum"
+          max-stack-size="item.maxStackSize"
+    ></dim-move-amount>*/
+
+    return (
+      <div className="interaction">
+        {stores.map((store) => (
+          <div className="locations" key={store.id}>
+            {this.canShowVault(store) && (
+              <div
+                className="move-button move-vault"
+                title={store.name}
+                onClick={() => this.moveItemTo(store)}
+              >
+                <span>{t('MovePopup.Vault')}</span>
+              </div>
+            )}
+            {!(item.owner === store.id && item.equipped) && item.canBeEquippedBy(store) && (
+              <div
+                className="move-button move-equip"
+                title={store.name}
+                onClick={() => this.moveItemTo(store, true)}
+                style={{ backgroundImage: `url(${store.icon})` }}
+              >
+                <span>{t('MovePopup.Equip')}</span>
+              </div>
+            )}
+            {this.canShowStore(store) && (
+              <div
+                className="move-button move-store"
+                title={store.name}
+                onClick={() => this.moveItemTo(store)}
+                style={{ backgroundImage: `url(${store.icon})` }}
+              >
+                <span>{t('MovePopup.Store')}</span>
+              </div>
+            )}
+          </div>
+        ))}
+
+        {canConsolidate && (
+          <div
+            className="move-button move-consolidate"
+            title={t('MovePopup.Consolidate')}
+            onClick={this.consolidate}
+          >
+            <span>{t('MovePopup.Take')}</span>
+          </div>
+        )}
+        {canDistribute && (
+          <div
+            className="move-button move-distribute"
+            title={t('MovePopup.DistributeEvenly')}
+            onClick={this.distribute}
+          >
+            <span>{t('MovePopup.Split')}</span>
+          </div>
+        )}
+        {item.infusionFuel && (
+          <div className="locations">
+            <div
+              className={classNames('move-button', 'infuse-perk', item.bucket.sort, {
+                destiny2: item.isDestiny2()
+              })}
+              onClick={this.infuse}
+              title={t('Infusion.Infusion')}
+            >
+              <span>{t('MovePopup.Infuse')}</span>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  private canShowVault = (buttonStore: DimStore): boolean => {
+    const { item, store } = this.props;
+
+    // If my store is the vault, don't show a vault button.
+    // Can't vault a vaulted item.
+    if (!store || store.isVault) {
+      return false;
+    }
+
+    // If my buttonStore is not the vault, then show a vault button.
+    if (!buttonStore.isVault) {
+      return false;
+    }
+
+    // Can't move this item away from the current itemStore.
+    if (item.notransfer) {
+      return false;
+    }
+
+    if (item.location.inPostmaster) {
+      return false;
+    }
+
+    return true;
+  };
+  private canShowStore = (buttonStore: DimStore): boolean => {
+    const { item, store } = this.props;
+
+    // Can't store into a vault
+    if (buttonStore.isVault || !store) {
+      return false;
+    }
+
+    // Can pull items from the postmaster to the same character
+    if (item.location.inPostmaster && item.location.type !== 'Engrams') {
+      return store.id === buttonStore.id && item.isDestiny2() && item.canPullFromPostmaster;
+    } else if (item.notransfer) {
+      // Can store an equiped item in same itemStore
+      if (item.equipped && store.id === buttonStore.id) {
+        return true;
+      }
+    } else if (store.id !== buttonStore.id || item.equipped) {
+      // In Destiny2, only show one store for account wide items
+      if (item.isDestiny2() && item.bucket && item.bucket.accountWide && !buttonStore.current) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  };
+  private moveItemTo = (store: DimStore, equip = false) => {
+    const { item, amount } = this.props;
+    moveItemTo(item, store, equip, amount);
+    hideItemPopup();
+  };
+
+  /*
+   * Open up the dialog for infusion by passing
+   * the selected item
+   */
+  private infuse = (e: React.MouseEvent) => {
+    const { item } = this.props;
+    e.stopPropagation();
+
+    hideItemPopup();
+
+    // Open the infuse window
+    ngDialog.open({
+      template: '<infuse query="item"></infuse>',
+      className: 'app-settings',
+      appendClassName: 'modal-dialog',
+      controller($scope) {
+        'ngInject';
+        $scope.item = item;
+      }
+    });
+  };
+
+  private consolidate = () => {
+    const { item, store } = this.props;
+    hideItemPopup();
+    consolidate(item, store!);
+  };
+
+  private distribute = () => {
+    const { item } = this.props;
+    hideItemPopup();
+    distribute(item);
+  };
+}
+
+export default connect<StoreProps>(mapStateToProps)(ItemActions);
--- src/app/item-popup/ItemPopupBody.tsx
+++ src/app/item-popup/ItemPopupBody.tsx
@@ -5,6 +5,7 @@ import { percent } from '../inventory/dimPercentWidth.directive';
 import { settings } from '../settings/settings';
 import ItemOverview from './ItemDetails';
 import { ItemPopupExtraInfo } from './item-popup';
+import ItemActions from './ItemActions';
 
 export default function ItemPopupBody({
   item,
@@ -28,6 +29,9 @@ export default function ItemPopupBody({
   // TODO: remember page
   // let tab: 'default' | 'reviews' = 'default';
   // const setTab = (t) => (tab = t);
+
+  // TODO: handle this
+  const amount = item.amount;
 
   return (
     <div>
@@ -67,42 +71,7 @@ export default function ItemPopupBody({
           {/*{tab === 'actions' && <ItemActions item={item} />}*/}
           <ItemOverview item={item} extraInfo={extraInfo} />
 
-          {/*
-          <dim-move-amount
-            ng-if="vm.maximum > 1 && !vm.item.notransfer && !vm.item.uniqueStack"
-            amount="vm.moveAmount"
-            maximum="vm.maximum"
-            max-stack-size="vm.item.maxStackSize"
-          ></dim-move-amount>
-          <div className="interaction" ng-if="vm.store">
-            <dim-move-locations item="vm.item" amount="vm.moveAmount"></dim-move-locations>
-            <div
-              className="move-button move-consolidate"
-              ng-i18next="[title]MovePopup.Consolidate;[alt]MovePopup.Consolidate"
-              ng-if="!vm.item.notransfer && vm.item.location.hasTransferDestination && vm.item.maxStackSize > 1"
-              ng-click="vm.consolidate()"
-            >
-              <span ng-i18next="MovePopup.Take"></span>
-            </div>
-            <div
-              className="move-button move-distribute"
-              ng-i18next="[title]MovePopup.DistributeEvenly;[alt]MovePopup.DistributeEvenly"
-              ng-if="vm.item.destinyVersion === 1 && !vm.item.notransfer && vm.item.maxStackSize > 1"
-              ng-click="vm.distribute()"
-            >
-              <span ng-i18next="MovePopup.Split"></span>
-            </div>
-            <div className="locations" ng-if="vm.item.infusionFuel">
-              <div
-                className="move-button infuse-perk"
-                ng-className="[{ destiny2: vm.item.destinyVersion === 2}, vm.item.bucket.sort]"
-                ng-click="vm.infuse(vm.item, $event)"
-                ng-i18next="[title]Infusion.Infusion;[alt]Infusion.Calc"
-              >
-                <span ng-i18next="MovePopup.Infuse"></span>
-              </div>
-            </div>
-          </div> */}
+          <ItemActions item={item} amount={amount} />
         </div>
       )}
     </div>
