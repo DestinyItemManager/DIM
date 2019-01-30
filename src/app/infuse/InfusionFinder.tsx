7e4f56404a523cc588dd0086caf7750be7093648
Ben Hollis <ben@benhollis.net>
2019-01-29 23:13:02 -0800

    Finish it off

--- src/app/infuse/InfusionFinder.tsx
+++ src/app/infuse/InfusionFinder.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './infuse.scss';
+import './InfusionFinder.scss';
 import { DimItem } from '../inventory/item-types';
 import { showInfuse$ } from './infuse';
 import { Subscriptions } from '../rx-utils';
@@ -59,9 +59,9 @@ function mapStateToProps(state: RootState): StoreProps {
 type Props = ProvidedProps & StoreProps;
 
 enum Direction {
-  /** infuse the query into the target (query = source) */
-  INFUSE,
   /** infuse something into the query (query = target) */
+  INFUSE,
+  /** infuse the query into the target (query = source) */
   FUEL
 }
 
@@ -72,6 +72,7 @@ interface State {
   defs?: D1ManifestDefinitions;
   direction: Direction;
   filter: string;
+  height?: number;
 }
 
 class InfusionFinder extends React.Component<Props, State> {
@@ -84,12 +85,28 @@ class InfusionFinder extends React.Component<Props, State> {
   componentDidMount() {
     this.subscriptions.add(
       showInfuse$.subscribe(({ item }) => {
-        this.setState({
-          query: item,
-          source: item,
-          target: undefined,
-          direction: Direction.INFUSE
-        });
+        const hasInfusables = this.props.stores.some((store) =>
+          store.items.some((i) => Boolean(isInfusable(item, i)))
+        );
+        if (hasInfusables) {
+          this.setState({
+            query: item,
+            source: undefined,
+            target: item,
+            direction: Direction.INFUSE,
+            height: undefined,
+            filter: ''
+          });
+        } else {
+          this.setState({
+            query: item,
+            source: item,
+            target: undefined,
+            direction: Direction.FUEL,
+            height: undefined,
+            filter: ''
+          });
+        }
       })
     );
     this.unregisterTransitionHook = router.transitionService.onBefore({}, () => this.onClose());
@@ -99,7 +116,7 @@ class InfusionFinder extends React.Component<Props, State> {
     }
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps: Props) {
     if (prevProps.destinyVersion !== 1 && this.props.destinyVersion === 1 && !this.state.defs) {
       getDefinitions().then((defs) => this.setState({ defs }));
     }
@@ -118,52 +135,38 @@ class InfusionFinder extends React.Component<Props, State> {
   }
 
   render() {
-    // TODO: make a connected subcomponent
     const { stores, searchConfig, filters } = this.props;
-    const { source, query, direction, filter } = this.state;
-    let { target } = this.state;
+    const { query, direction, filter, height } = this.state;
+    let { target, source } = this.state;
 
     if (!query) {
       return null;
     }
 
-    // TODO: cache this?
-    let items: DimItem[] = [];
-    if (direction === Direction.INFUSE) {
-      const targetItems = _.flatMap(stores, (store) =>
-        store.items.filter((item) => isInfusable(query, item))
-      );
-      items = targetItems;
-    } else {
-      const sourceItems = _.flatMap(stores, (store) =>
-        store.items.filter((item) => isInfusable(item, query))
-      );
-      items = sourceItems;
-    }
-
-    const dupes = items.filter((item) => item.hash === query.hash);
-    items = items.filter((item) => item.hash !== query.hash);
-
     const filterFn = filters.filterFunction(filter);
 
-    items = items.filter(filterFn).sort(itemComparator);
+    let items = _.flatMap(stores, (store) =>
+      store.items.filter(
+        (item) =>
+          (direction === Direction.INFUSE ? isInfusable(query, item) : isInfusable(item, query)) &&
+          filterFn(item)
+      )
+    );
+
+    const dupes = items.filter((item) => item.hash === query.hash);
+    dupes.sort(itemComparator);
+    items = items.filter((item) => item.hash !== query.hash);
+    items.sort(itemComparator);
 
     target = target || items[0];
+    source = source || items[0];
 
-    let infused = target && target.primStat ? target.primStat.value : 0;
     let result: DimItem | undefined;
-
     if (source && target && source.primStat && target.primStat) {
-      if (source.isDestiny2()) {
-        infused = target.basePower + (source.primStat!.value - source.basePower);
-      }
-
-      result = copy(source);
-      // TODO:
+      const infused = source.primStat ? source.primStat.value : 0;
+      result = copy(target);
       (result as any).primStat.value = infused;
     }
-
-    console.log({ target, result });
 
     const missingItem = (
       <div className="item missingItem">
@@ -174,42 +177,39 @@ class InfusionFinder extends React.Component<Props, State> {
       </div>
     );
 
-    const header = (
+    const header = ({ onClose }: { onClose(): void }) => (
       <div className="infuseHeader">
         <h1>
           {t(direction === Direction.INFUSE ? 'Infusion.InfuseTarget' : 'Infusion.InfuseSource', {
             name: query.name
           })}
         </h1>
-        <div className="infusionEquation">
-          {source ? <ConnectedInventoryItem item={source} /> : missingItem}
-          {target ? (
-            <>
+        <div className="infusionControls">
+          <div className="infuseTopRow">
+            <div className="infusionEquation">
+              {target ? <ConnectedInventoryItem item={target} /> : missingItem}
               <div className="icon">
                 <AppIcon icon={plusIcon} />
               </div>
-              <ConnectedInventoryItem item={target} />
-              {result && (
-                <>
-                  <div className="icon">
-                    <AppIcon icon={faEquals} />
-                  </div>
-                  <ConnectedInventoryItem item={result} />
-                </>
-              )}
-            </>
-          ) : (
-            missingItem
-          )}
-          <div className="infuseActions">
-            <button className="dim-button" onClick={this.switchDirection}>
-              <AppIcon icon={faRandom} /> Switch Direction
-            </button>
-            {result && (
-              <button className="dim-button" onClick={this.transferItems}>
-                <AppIcon icon={faArrowCircleDown} /> Transfer
+              {source ? <ConnectedInventoryItem item={source} /> : missingItem}
+              <div className="icon">
+                <AppIcon icon={faEquals} />
+              </div>
+              {result ? <ConnectedInventoryItem item={result} /> : missingItem}
+            </div>
+            <div className="infuseActions">
+              <button className="dim-button" onClick={this.switchDirection}>
+                <AppIcon icon={faRandom} /> {t('Infusion.SwitchDirection')}
               </button>
-            )}
+              {result && source && target && (
+                <button
+                  className="dim-button"
+                  onClick={() => this.transferItems(onClose, source!, target!)}
+                >
+                  <AppIcon icon={faArrowCircleDown} /> {t('Infusion.TransferItems')}
+                </button>
+              )}
+            </div>
           </div>
           <div className="infuseSearch">
             <SearchFilterInput
@@ -224,7 +224,7 @@ class InfusionFinder extends React.Component<Props, State> {
 
     return (
       <Sheet onClose={this.onClose} header={header} sheetClassName="infuseDialog">
-        <div className="infuseSources">
+        <div className="infuseSources" ref={this.itemContainer} style={{ height }}>
           {items.length > 0 ? (
             <>
               <div className="itemGrid">
@@ -232,7 +232,7 @@ class InfusionFinder extends React.Component<Props, State> {
                   <div
                     key={item.id}
                     className={classNames({ 'infuse-selected': item === target })}
-                    onClick={() => this.setSourceAndTarget(query, item)}
+                    onClick={() => this.setSourceAndTarget(item, query)}
                   >
                     <ConnectedInventoryItem item={item} />
                   </div>
@@ -243,7 +243,7 @@ class InfusionFinder extends React.Component<Props, State> {
                   <div
                     key={item.id}
                     className={classNames({ 'infuse-selected': item === target })}
-                    onClick={() => this.setSourceAndTarget(query, item)}
+                    onClick={() => this.setSourceAndTarget(item, query)}
                   >
                     <ConnectedInventoryItem item={item} />
                   </div>
@@ -259,11 +259,21 @@ class InfusionFinder extends React.Component<Props, State> {
   }
 
   private onClose = () => {
-    this.setState({ query: undefined, source: undefined, target: undefined });
+    this.setState({
+      query: undefined,
+      source: undefined,
+      target: undefined,
+      height: undefined,
+      filter: ''
+    });
   };
 
   private setSourceAndTarget = (source: DimItem, target: DimItem) => {
-    this.setState({ source, target });
+    if (this.state.direction === Direction.INFUSE) {
+      this.setState({ source, target });
+    } else {
+      this.setState({ source: target, target: source });
+    }
   };
 
   private onQueryChanged = (filter: string) => {
@@ -274,13 +284,12 @@ class InfusionFinder extends React.Component<Props, State> {
     const direction = this.state.direction === Direction.INFUSE ? Direction.FUEL : Direction.INFUSE;
     this.setState({
       direction,
-      source: direction === Direction.INFUSE ? this.state.query : undefined,
-      target: direction === Direction.FUEL ? this.state.query : undefined
+      target: direction === Direction.INFUSE ? this.state.query : undefined,
+      source: direction === Direction.FUEL ? this.state.query : undefined
     });
   };
 
-  private transferItems = async () => {
-    const { source, target } = this.state;
+  private transferItems = async (onClose: () => void, source: DimItem, target: DimItem) => {
     if (!source || !target) {
       return;
     }
@@ -291,6 +300,8 @@ class InfusionFinder extends React.Component<Props, State> {
       toaster.pop('error', t('Infusion.NoTransfer', { target: name }));
       return;
     }
+
+    onClose();
 
     const store = source.getStoresService().getActiveStore()!;
     const items: { [key: string]: any[] } = {};
@@ -342,7 +353,7 @@ class InfusionFinder extends React.Component<Props, State> {
 
     const loadout = newLoadout(t('Infusion.InfusionMaterials'), items);
 
-    return dimLoadoutService.applyLoadout(store, loadout);
+    await dimLoadoutService.applyLoadout(store, loadout);
   };
 }
 
@@ -351,21 +362,21 @@ export default connect<StoreProps>(mapStateToProps)(InfusionFinder);
 /**
  * Can source be infused into target?
  */
-function isInfusable(source: DimItem, target: DimItem) {
-  if (!source.infusable || !target.infusionFuel) {
+function isInfusable(target: DimItem, source: DimItem) {
+  if (!target.infusable || !source.infusionFuel) {
     return false;
   }
 
-  if (source.isDestiny1() && target.isDestiny2()) {
-    return source.type === target.type && source.primStat!.value < target.primStat!.value;
+  if (source.isDestiny1() && target.isDestiny1()) {
+    return source.type === target.type && target.primStat!.value < source.primStat!.value;
   } else if (source.isDestiny2() && target.isDestiny2()) {
     return (
       source.infusionQuality &&
       target.infusionQuality &&
-      source.infusionQuality.infusionCategoryHashes.some((h) =>
-        target.infusionQuality!.infusionCategoryHashes.includes(h)
+      target.infusionQuality.infusionCategoryHashes.some((h) =>
+        source.infusionQuality!.infusionCategoryHashes.includes(h)
       ) &&
-      source.basePower < target.basePower
+      target.basePower < source.basePower
     );
   }
 
