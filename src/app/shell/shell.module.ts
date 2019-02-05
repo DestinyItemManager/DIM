82fc95c00521656ef0a09b7a4824c690daa07f85
Ben Hollis <ben@benhollis.net>
2019-02-05 01:04:48 -0800

    Remove a bunch of AngularJS stuff

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,11 +1,8 @@
 import { module } from 'angular';
 
-import { ClickAnywhereButHere } from './click-anywhere-but-here.directive';
-import { FontAwesomeIcon } from './font-awesome-icon.directive';
-import dimAngularFiltersModule from './dimAngularFilters.filter';
 import { ToasterContainerComponent } from './toaster-container.component';
 
-export const ShellModule = module('dimShell', [dimAngularFiltersModule])
-  .component('dimToasterContainer', ToasterContainerComponent)
-  .directive('fontAwesomeIcon', FontAwesomeIcon)
-  .directive('dimClickAnywhereButHere', ClickAnywhereButHere).name;
+export const ShellModule = module('dimShell', []).component(
+  'dimToasterContainer',
+  ToasterContainerComponent
+).name;
