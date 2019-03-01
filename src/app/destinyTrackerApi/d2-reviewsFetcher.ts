2932c2abf068590f2ec03e7a485bc298598db7ea
Ben Hollis <ben@benhollis.net>
2019-02-28 23:33:11 -0800

    Don't need "ActualD2ItemUserReview"

--- src/app/destinyTrackerApi/d2-reviewsFetcher.ts
+++ src/app/destinyTrackerApi/d2-reviewsFetcher.ts
@@ -5,87 +5,11 @@ import { loadingTracker } from '../shell/loading-tracker';
 import { handleD2Errors } from './d2-trackerErrorHandler';
 import { D2Item } from '../inventory/item-types';
 import { dtrFetch } from './dtr-service-helper';
-import {
-  D2ItemReviewResponse,
-  D2ItemUserReview,
-  DtrD2Vote,
-  DtrD2ActivityModes
-} from '../item-review/d2-dtr-api-types';
+import { D2ItemReviewResponse, D2ItemUserReview } from '../item-review/d2-dtr-api-types';
 import { getRollAndPerks } from './d2-itemTransformer';
 import { ratePerks } from './d2-perkRater';
 import { conditionallyIgnoreReviews } from './userFilter';
 import { toUtcTime } from './util';
-import { DtrReviewer } from '../item-review/dtr-api-types';
-
-interface ActualD2ItemUserReview {
-  /** The DTR review ID. */
-  id: string;
-  /**
-   * This is not returned from DTR, it's calculated on our end.
-   * Will be set on reviews associated with any other reviwer that the user reports.
-   */
-  isIgnored?: boolean;
-  /**
-   * Is this reviewer a featured reviewer?
-   * Broken in D2.
-   */
-  isHighlighted: boolean;
-  /**
-   * Was this review written by the DIM user that requested the reviews?
-   * Broken in D2.
-   */
-  isReviewer: boolean;
-  /**
-   * Pros.
-   * Shouldn't be present (yet).
-   */
-  pros: string;
-  /**
-   * Cons.
-   * shouldn't be present (yet).
-   */
-  cons: string;
-  /** The instance ID for the item reviewed. */
-  instanceId?: string;
-  /** What was their vote? Should be -1 or 1. */
-  voted: number;
-  /**
-   * Review text.
-   * Optional to send, optional to receive.
-   */
-  text?: string;
-  /** What perks did the user have selected on this item? */
-  selectedPerks: number[];
-  /** If it's a random roll, what's the complete list of (random) perks on it? */
-  availablePerks?: number[];
-  /** What power mods did the user have attached to this item? */
-  attachedMods: number[];
-  /** What play mode is this for? */
-  mode: DtrD2ActivityModes;
-  /**
-   * Sandbox season (1 was the first, 2 is the March 2018 "go fast" update).
-   * Not enumerating these values here because we're not using this and who wants to update this with a new sandbox?
-   */
-  sandbox: number;
-  timestamp: string;
-  /** Who reviewed it? */
-  reviewer: DtrReviewer;
-}
-
-interface ActualD2ItemReviewResponse {
-  /** Reference ID (hash ID). */
-  referenceId: number;
-  /** The votes for a single item. */
-  votes: DtrD2Vote;
-  /** The total number of reviews. */
-  totalReviews: number;
-  /**
-   * Reviews for the item.
-   * More particulars - they return a maximum of 25 text reviews per item, newest first, and we can page through them.
-   * Don't tell anyone I haven't bothered building pagination out yet.
-   */
-  reviews: ActualD2ItemUserReview[];
-}
 
 /**
  * Get the community reviews from the DTR API for a specific item.
@@ -101,7 +25,7 @@ class D2ReviewsFetcher {
     item,
     platformSelection: number,
     mode: number
-  ): Promise<ActualD2ItemReviewResponse> {
+  ): Promise<D2ItemReviewResponse> {
     const dtrItem = getRollAndPerks(item);
 
     const queryString = `page=1&platform=${platformSelection}&mode=${mode}`;
@@ -184,8 +108,8 @@ class D2ReviewsFetcher {
     return bDate - aDate;
   }
 
-  _translateReview(returnedUserReview: ActualD2ItemUserReview): D2ItemUserReview {
-    const timestamp = toUtcTime(returnedUserReview.timestamp);
+  _translateReview(returnedUserReview: D2ItemUserReview): D2ItemUserReview {
+    const timestamp = toUtcTime((returnedUserReview.timestamp as any) as string);
 
     return {
       ...returnedUserReview,
@@ -193,7 +117,7 @@ class D2ReviewsFetcher {
     };
   }
 
-  _translateReviewResponse(actualResponse: ActualD2ItemReviewResponse): D2ItemReviewResponse {
+  _translateReviewResponse(actualResponse: D2ItemReviewResponse): D2ItemReviewResponse {
     const reviews = actualResponse.reviews.map((review) => this._translateReview(review));
 
     return { ...actualResponse, reviews };
