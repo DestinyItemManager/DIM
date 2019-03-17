fe0a59efeefbd137a131e0eaf86c1f909f8f5dd9
Ben Hollis <ben@benhollis.net>
2019-03-16 21:16:58 -0700

    Set up Travis to be able to build PRs

---
+++ build/travis_build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash -e
+
+set -o pipefail
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  # Master builds release to Beta
+  openssl aes-256-cbc -K $encrypted_472c4900477c_key -iv $encrypted_472c4900477c_iv -in config/dim_travis.rsa.enc -out config/dim_travis.rsa -d && chmod 600 config/dim_travis.rsa
+  yarn run publish-beta
+  VERSION=$(node -p -e "require('./package.json').version + '.' + process.env.TRAVIS_BUILD_NUMBER")
+  npx sentry-cli releases new "$VERSION" --finalize
+  npx sentry-cli releases set-commits "$VERSION" --auto
+else
+  # PRs should just check if the app builds
+  yarn run build-beta
+fi
