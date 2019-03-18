07a5d3e4505231044282b393ed87f6e8fde12621
Ben Hollis <ben@benhollis.net>
2019-03-17 18:41:25 -0700

    Run lint in CI

--- build/travis_build.sh
+++ build/travis_build.sh
@@ -5,11 +5,13 @@ set -o pipefail
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   # Master builds release to Beta
   openssl aes-256-cbc -K $encrypted_472c4900477c_key -iv $encrypted_472c4900477c_iv -in config/dim_travis.rsa.enc -out config/dim_travis.rsa -d && chmod 600 config/dim_travis.rsa
+  yarn run lint
   yarn run publish-beta
   VERSION=$(node -p -e "require('./package.json').version + '.' + process.env.TRAVIS_BUILD_NUMBER")
   npx sentry-cli releases new "$VERSION" --finalize
   npx sentry-cli releases set-commits "$VERSION" --auto
 else
   # PRs should just check if the app builds
+  yarn run lint
   yarn run build-beta
 fi
