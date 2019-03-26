e1ea48f88e47f40f8221e16fbe3e4dbbab92f765
Rob Jones <delphi.actual@gmail.com>
2019-03-25 19:59:46 -0600

    missed check.svg conversion

--- src/app/vendors/D1VendorItem.tsx
+++ src/app/vendors/D1VendorItem.tsx
@@ -5,7 +5,8 @@ import ConnectedInventoryItem from '../inventory/ConnectedInventoryItem';
 import ItemPopupTrigger from '../inventory/ItemPopupTrigger';
 import '../progress/milestone.scss';
 import { VendorSaleItem, VendorCost } from './vendor.service';
-import checkMark from '../../images/check.svg';
+import { AppIcon } from '../shell/icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
   saleItem: VendorSaleItem;
@@ -25,7 +26,7 @@ export default class D1VendorItem extends React.Component<Props> {
           unavailable: !saleItem.unlocked
         })}
       >
-        {owned && <img className="owned-icon" src={checkMark} />}
+        {owned && <AppIcon className="owned-icon" icon={faCheck} />}
         <ItemPopupTrigger
           item={saleItem.item}
           extraData={{ failureStrings: [saleItem.failureStrings] }}
