28a66a7ad32ae2874b84343e50e22a4835ef0236
Ben Hollis <ben@benhollis.net>
2019-04-07 09:48:37 -0700

    Fix build script

--- build/travis_build.sh
+++ build/travis_build.sh
@@ -5,9 +5,9 @@ set -o pipefail
 find . -type d -name "node_modules" -prune -o -type f -iregex '.*.ts\|.*.js\|.*.tsx\|.*.jsx' -exec ./build/set-mtime-to-md5.sh {} \; && echo Set mtime to MD5 complete!
 yarn run lint-check
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   # Master builds release to Beta
-  openssl aes-256-cbc -K $encrypted_472c4900477c_key -iv $encrypted_472c4900477c_iv -in config/dim_travis.rsa.enc -out config/dim_travis.rsa -d && chmod 600 config/dim_travis.rsa
+  openssl aes-256-cbc -K "$encrypted_472c4900477c_key" -iv "$encrypted_472c4900477c_iv" -in config/dim_travis.rsa.enc -out config/dim_travis.rsa -d && chmod 600 config/dim_travis.rsa
   yarn run publish-beta
   VERSION=$(node -p -e "require('./package.json').version + '.' + process.env.TRAVIS_BUILD_NUMBER")
   npx sentry-cli releases new "$VERSION" --finalize
