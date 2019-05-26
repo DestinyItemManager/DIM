824b58a711dd5aef6e51ca2a8481dfe358387e24
Ben Hollis <ben@benhollis.net>
2019-05-25 19:48:09 -0700

    Properly memoize character order selector

--- src/app/settings/character-sort.ts
+++ src/app/settings/character-sort.ts
@@ -2,60 +2,68 @@ import { RootState } from '../store/reducers';
 import { DimStore } from '../inventory/store-types';
 import _ from 'lodash';
 import { DestinyCharacterComponent } from 'bungie-api-ts/destiny2';
+import { createSelector } from 'reselect';
 
-export const characterSortSelector = (state: RootState) => {
-  const order = state.settings.characterOrder;
+const characterOrderSelector = (state: RootState) => state.settings.characterOrder;
+const customCharacterSortSelector = (state: RootState) => state.settings.customCharacterSort;
 
-  switch (order) {
-    case 'mostRecent':
-      return (stores: DimStore[]) => _.sortBy(stores, (store) => store.lastPlayed).reverse();
+export const characterSortSelector = createSelector(
+  characterOrderSelector,
+  customCharacterSortSelector,
+  (order, customCharacterSort) => {
+    switch (order) {
+      case 'mostRecent':
+        return (stores: DimStore[]) => _.sortBy(stores, (store) => store.lastPlayed).reverse();
 
-    case 'mostRecentReverse':
-      return (stores: DimStore[]) =>
-        _.sortBy(stores, (store) => {
-          if (store.isVault) {
-            return Infinity;
-          } else {
-            return store.lastPlayed;
-          }
-        });
+      case 'mostRecentReverse':
+        return (stores: DimStore[]) =>
+          _.sortBy(stores, (store) => {
+            if (store.isVault) {
+              return Infinity;
+            } else {
+              return store.lastPlayed;
+            }
+          });
 
-    case 'custom': {
-      const customSortOrder = state.settings.customCharacterSort;
-      return (stores: DimStore[]) =>
-        _.sortBy(stores, (s) => (s.isVault ? 999 : customSortOrder.indexOf(s.id)));
+      case 'custom': {
+        const customSortOrder = customCharacterSort;
+        return (stores: DimStore[]) =>
+          _.sortBy(stores, (s) => (s.isVault ? 999 : customSortOrder.indexOf(s.id)));
+      }
+
+      default:
+      case 'fixed': // "Age"
+        // https://github.com/Bungie-net/api/issues/614
+        return (stores: DimStore[]) => _.sortBy(stores, (s) => s.id);
     }
-
-    default:
-    case 'fixed': // "Age"
-      // https://github.com/Bungie-net/api/issues/614
-      return (stores: DimStore[]) => _.sortBy(stores, (s) => s.id);
   }
-};
+);
 
-export const characterComponentSortSelector = (state: RootState) => {
-  const order = state.settings.characterOrder;
+export const characterComponentSortSelector = createSelector(
+  characterOrderSelector,
+  customCharacterSortSelector,
+  (order, customCharacterSort) => {
+    switch (order) {
+      case 'mostRecent':
+        return (stores: DestinyCharacterComponent[]) =>
+          _.sortBy(stores, (store) => new Date(store.dateLastPlayed)).reverse();
 
-  switch (order) {
-    case 'mostRecent':
-      return (stores: DestinyCharacterComponent[]) =>
-        _.sortBy(stores, (store) => new Date(store.dateLastPlayed)).reverse();
+      case 'mostRecentReverse':
+        return (stores: DestinyCharacterComponent[]) =>
+          _.sortBy(stores, (store) => {
+            return new Date(store.dateLastPlayed);
+          });
 
-    case 'mostRecentReverse':
-      return (stores: DestinyCharacterComponent[]) =>
-        _.sortBy(stores, (store) => {
-          return new Date(store.dateLastPlayed);
-        });
+      case 'custom': {
+        const customSortOrder = customCharacterSort;
+        return (stores: DestinyCharacterComponent[]) =>
+          _.sortBy(stores, (s) => customSortOrder.indexOf(s.characterId));
+      }
 
-    case 'custom': {
-      const customSortOrder = state.settings.customCharacterSort;
-      return (stores: DestinyCharacterComponent[]) =>
-        _.sortBy(stores, (s) => customSortOrder.indexOf(s.characterId));
+      default:
+      case 'fixed': // "Age"
+        // https://github.com/Bungie-net/api/issues/614
+        return (stores: DestinyCharacterComponent[]) => _.sortBy(stores, (s) => s.characterId);
     }
-
-    default:
-    case 'fixed': // "Age"
-      // https://github.com/Bungie-net/api/issues/614
-      return (stores: DestinyCharacterComponent[]) => _.sortBy(stores, (s) => s.characterId);
   }
-};
+);
