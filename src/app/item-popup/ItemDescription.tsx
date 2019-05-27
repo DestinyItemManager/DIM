8e03e854a433d8c50409986ad2ce6e42e9009f97
Ben Hollis <ben@benhollis.net>
2019-05-27 15:39:02 -0700

    Factor out an ItemDescription component

---
+++ src/app/item-popup/ItemDescription.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { DimItem } from 'app/inventory/item-types';
+import NotesForm from './NotesForm';
+import ExternalLink from 'app/dim-ui/ExternalLink';
+import { t } from 'app/i18next-t';
+import ishtarLogo from '../../images/ishtar-collective.svg';
+import styles from './ItemDescription.m.scss';
+import { AppIcon } from 'app/shell/icons';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { connect } from 'react-redux';
+import { getNotes } from 'app/inventory/dim-item-info';
+import { RootState } from 'app/store/reducers';
+
+interface ProvidedProps {
+  item: DimItem;
+}
+
+interface StoreProps {
+  notes?: string;
+}
+
+function mapStateToProps(state: RootState, props: ProvidedProps): StoreProps {
+  return { notes: getNotes(props.item, state.inventory.itemInfos) };
+}
+
+type Props = ProvidedProps & StoreProps;
+
+function ItemDescription({ item, notes }: Props) {
+  const showDescription = Boolean(item.description && item.description.length);
+
+  const loreLink = item.loreHash
+    ? `http://www.ishtar-collective.net/entries/${item.loreHash}`
+    : undefined;
+
+  const [notesOpen, setNotesOpen] = useState(false);
+
+  // TODO: close notes button
+
+  return (
+    <>
+      {showDescription && <div className={styles.officialDescription}>{item.description}</div>}
+      {notesOpen ? (
+        <NotesForm item={item} notes={notes} />
+      ) : (
+        notes && (
+          <div
+            className={[styles.addNote, styles.description].join(' ')}
+            role="button"
+            onClick={() => setNotesOpen(true)}
+            tabIndex={0}
+          >
+            <AppIcon icon={faPencilAlt} /> Notes: {notes}
+          </div>
+        )
+      )}
+
+      {!notesOpen && (loreLink || (item.taggable && !notes)) && (
+        <div className={styles.descriptionTools}>
+          {item.taggable && !notes && (
+            <div
+              role="button"
+              className={styles.addNote}
+              onClick={() => setNotesOpen(true)}
+              tabIndex={0}
+            >
+              <AppIcon icon={faPencilAlt} /> Add a note
+            </div>
+          )}
+          {loreLink && (
+            <div className={styles.lore}>
+              <ExternalLink href={loreLink}>
+                <img src={ishtarLogo} height="16" width="16" />
+              </ExternalLink>{' '}
+              <ExternalLink href={loreLink}>{t('MovePopup.ReadLore')}</ExternalLink>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default connect<StoreProps>(mapStateToProps)(ItemDescription);
