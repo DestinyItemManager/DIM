27820c5dd81a1b241f607b668c6199bcac6ffe87
Rob Jones <delphi.actual@gmail.com>
2019-07-04 10:48:17 -0600

    better blacklist

--- src/app/inventory/store/d2-item-factory.service.ts
+++ src/app/inventory/store/d2-item-factory.service.ts
@@ -65,15 +65,36 @@ import D2Events from 'data/d2-events.json';
 const D2SeasonsItemCategoryHashBlacklist = [
   16, // Quest Steps
   18, // Currencies
+  34, // Engrams
   40, // Material
   53, // Quests
-  58, // Clan Banners
+  58, // Clan Banner
+  268598612, // Packages
+  303512563, // Bonus Mods
+  444756050, // Weapon Mods: Bow strings
+  945330047, // Weapon Mods: Gameplay
+  1334054322, // Weapon Mods: Batteries
+  1449602859, // Ghost Mods
+  1576735337, // Clan Banner: Perks
+  1709863189, // Weapon Mods: Sword Blades
   1784235469, // Bounties
   2005599723, // Prophecy Offerings
+  2076918099, // Weapon Mods: Launch Tubes
   2150402250, // Gags
+  2237038328, // Weapon Mods: Intrinsic
   2250046497, // Prophecy Tablets
   2253669532, // Treasure Maps
-  3109687656 // Dummies
+  2411768833, // Weapon Mods: Scopes
+  2423200735, // Item Sets
+  3055157023, // Weapon Mods: Stocks
+  3072652064, // Weapon Mods: Sword Guards
+  3085181971, // Weapon Mods: Barrels
+  3109687656, // Dummies
+  3360831066, // Weapon Mods: Arrows
+  3708671066, // Weapon Mods: Frames
+  3836367751, // Weapon Mods: Grips
+  3866509906, // Weapon Mods: Sights
+  4184407433 // Weapon Mods: Magazines
 ];
 
 // Maps tierType to tierTypeName in English
