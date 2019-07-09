1391c31bfa84bc95c74d9ff430b198acf6f71930
Ben Hollis <ben@benhollis.net>
2019-07-08 21:05:15 -0700

    More stuff to transpile

--- config/webpack.js
+++ config/webpack.js
@@ -115,8 +115,17 @@ module.exports = (env) => {
       rules: [
         {
           test: /\.js$/,
-          // Update with npx are-you-es5 check -r .
-          exclude: /\/node_modules\/(?!(idb-keyval|react-dnd|react-dnd-html5-backend|react-with-gesture))/,
+          include: [
+            path.resolve('src'),
+            // These dependencies have es6 syntax which edge doesn't like.
+            // Update with npx are-you-es5 check -r .
+            // https://github.com/babel/babel-loader/issues/171
+            path.resolve('node_modules/idb-keyval'),
+            path.resolve('node_modules/react-dnd'),
+            path.resolve('node_modules/dnd-core'),
+            path.resolve('node_modules/react-dnd-html5-backend'),
+            path.resolve('node_modules/react-with-gesture')
+          ],
           loader: 'babel-loader',
           options: {
             cacheDirectory: true
