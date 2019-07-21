a5bc2ae936f2be6c775794e1a15e4a92f11ab263
Ben Hollis <ben@benhollis.net>
2019-07-21 00:18:49 -0700

    Add current season resets to ranks

--- src/app/progress/CrucibleRank.tsx
+++ src/app/progress/CrucibleRank.tsx
@@ -38,6 +38,11 @@ export function CrucibleRank(props: CrucibleRankProps) {
             pct: Math.round((progress.currentProgress / rankTotal) * 100)
           })}
         </div>
+        {!!progress.currentResetCount && (
+          <div className="faction-level">
+            {t('Progress.Resets', { count: progress.currentResetCount })}
+          </div>
+        )}
       </div>
     </div>
   );
