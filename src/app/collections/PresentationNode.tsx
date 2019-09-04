de31ebec72d38ed7db8b392d947da54f71da30cd
sundevour <31990469+sundevour@users.noreply.github.com>
2019-09-03 19:52:52 -0700

    apply "collapsed" class&color to collapsed nodes

--- src/app/collections/PresentationNode.tsx
+++ src/app/collections/PresentationNode.tsx
@@ -172,7 +172,9 @@ export class PresentationNode extends React.Component<Props> {
       >
         {!rootNodes.includes(presentationNodeHash) && !onlyChild && (
           <div
-            className={defaultExpanded ? 'leaf-node' : 'title'}
+            className={classNames(defaultExpanded ? 'leaf-node' : 'title', {
+              collapsed: !childrenExpanded
+            })}
             onClick={this.expandChildren}
             ref={this.headerRef}
           >
