9c6c903dad76bef5f6e0b28bf955ae4706bd0cb2
Rob Jones <delphi.actual@gmail.com>
2019-09-14 23:16:31 -0600

    fa deep link faLevelUpAlt missed faClone Revert "fa deep link"
    This reverts commit 93461a0716ecde9f71f24b82880d696071aeebe1.
    babel-plugin-transform-imports
    simplify
    .
    .
    .
    regex
    .
    Revert "regex"
    
    This reverts commit b1a92afc013f98a3fecbb1a4c2fcd273931bbaa2.

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,23 @@ module.exports = function(api) {
       {
         useESModules: true
       }
+    ],
+    [
+      'transform-imports',
+      {
+        '@fortawesome/free-brands-svg-icons': {
+          transform: (importName) => `@fortawesome/free-brands-svg-icons/${importName}`,
+          preventFullImport: true
+        },
+        '@fortawesome/free-regular-svg-icons': {
+          transform: (importName) => `@fortawesome/free-regular-svg-icons/${importName}`,
+          preventFullImport: true
+        },
+        '@fortawesome/free-solid-svg-icons': {
+          transform: (importName) => `@fortawesome/free-solid-svg-icons/${importName}`,
+          preventFullImport: true
+        }
+      }
     ]
   ];
 
