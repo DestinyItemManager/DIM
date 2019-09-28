02e454b48bba89adb4716c1eefd7c2dfedb1b69d
Dave <dave.c.solomon@gmail.com>
2019-09-27 21:19:13 -0400

    Added a tip for when creating a review.

--- src/app/item-review/ItemReviews.tsx
+++ src/app/item-review/ItemReviews.tsx
@@ -182,7 +182,7 @@ class ItemReviews extends React.Component<Props, State> {
           <div className="community-review--details">
             {isD2UserReview(item, userReview) && (
               <div className="community-review--mode">
-                <label htmlFor="reviewMode">{t('DtrReview.ForGameMode', { mode: null })}</label>{' '}
+                <label htmlFor="reviewMode">{t('DtrReview.ForGameModeTip')}</label>{' '}
                 <select name="reviewMode" onChange={this.changeMode}>
                   {reviewModeOptions &&
                     reviewModeOptions.map((reviewModeOption) => (
