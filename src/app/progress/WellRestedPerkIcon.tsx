bec3491788aabcc6508b79b6053677dca70d5da8
Ben Hollis <ben@benhollis.net>
2019-10-02 11:25:22 -0700

    Fix well rested perk icon
    Fixes #4334 but may require more work

--- src/app/progress/WellRestedPerkIcon.tsx
+++ src/app/progress/WellRestedPerkIcon.tsx
@@ -17,7 +17,12 @@ export default function WellRestedPerkIcon({
     return null;
   }
   const formatter = new Intl.NumberFormat(window.navigator.language);
-  const perkDisplay = defs.SandboxPerk.get(1519921522).displayProperties;
+  const wellRestedPerk = defs.SandboxPerk.get(2352765282);
+  if (!wellRestedPerk) {
+    console.error("Couldn't find Well Rested perk in manifest");
+    return null;
+  }
+  const perkDisplay = wellRestedPerk.displayProperties;
   return (
     <div className="well-rested milestone-quest">
       <div className="milestone-icon">
