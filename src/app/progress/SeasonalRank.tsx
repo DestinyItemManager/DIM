65bd6de0e1eeefc8808edfbbc233f0ad4d4ed233
Ben Hollis <ben@benhollis.net>
2019-10-10 20:32:53 -0700

    Seasonal rank tweaks

--- src/app/progress/SeasonalRank.tsx
+++ src/app/progress/SeasonalRank.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import idx from 'idx';
-import classNames from 'classnames';
 import { D2ManifestDefinitions } from '../destiny2/d2-definitions';
 import BungieImage from '../dim-ui/BungieImage';
 import {
@@ -14,6 +13,7 @@ import { settings } from 'app/settings/settings';
 import { t } from 'app/i18next-t';
 import styles from './PursuitItem.m.scss';
 import { percent } from 'app/shell/filters';
+import clsx from 'clsx';
 
 export default function SeasonalRank({
   defs,
@@ -35,14 +35,17 @@ export default function SeasonalRank({
   // Get season details
   const seasonNameDisplay = season.displayProperties.name;
   const seasonPassProgressionHash = season.seasonPassProgressionHash;
+  if (!seasonPassProgressionHash) {
+    return null;
+  }
   const seasonEnd = season.endDate;
   const currentSeasonHash = season.hash;
   const seasonHashes = idx(profileInfo, (p) => p.profile.data.seasonHashes) || [];
 
   // Get seasonal character progressions
-  const seasonProgress = characterProgressions.progressions[seasonPassProgressionHash!];
+  const seasonProgress = characterProgressions.progressions[seasonPassProgressionHash];
   const { level: seasonalRank, progressToNextLevel, nextLevelAt } = seasonProgress;
-  const { rewardItems } = defs.Progression.get(seasonPassProgressionHash!);
+  const { rewardItems } = defs.Progression.get(seasonPassProgressionHash);
 
   // Get the reward item for the next progression level
   const nextRewardItems = rewardItems
@@ -54,41 +57,41 @@ export default function SeasonalRank({
 
   return (
     <div
-      className={classNames('seasonal-rank', 'milestone-quest', {
+      className={clsx('seasonal-rank', 'milestone-quest', {
         'has-premium-rewards': hasPremiumRewards
       })}
     >
       <div className="milestone-icon">
-        {nextRewardItems.map((item) => {
-          // Don't show premium reward if player doesn't own the season pass
-          if (!hasPremiumRewards && item.uiDisplayStyle === 'premium') {
-            return;
-          }
+        <div className="seasonal-rewards">
+          {nextRewardItems.map((item) => {
+            // Don't show premium reward if player doesn't own the season pass
+            if (!hasPremiumRewards && item.uiDisplayStyle === 'premium') {
+              return;
+            }
 
-          // Get the item info for UI display
-          const itemInfo = defs.InventoryItem.get(item.itemHash);
+            // Get the item info for UI display
+            const itemInfo = defs.InventoryItem.get(item.itemHash);
 
-          return (
-            <div
-              className={classNames('seasonal-reward-wrapper', styles.pursuit, {
-                free: item.uiDisplayStyle === 'free',
-                premium: item.uiDisplayStyle === 'premium'
-              })}
-              key={itemInfo.hash}
-            >
-              <BungieImage
-                className="perk milestone-img"
-                src={itemInfo.displayProperties.icon}
-                title={itemInfo.displayProperties.description}
-              />
-              {item.quantity > 1 && (
-                <div className={classNames(styles.amount)}>{item.quantity}</div>
-              )}
-            </div>
-          );
-        })}
+            return (
+              <div
+                className={clsx('seasonal-reward-wrapper', styles.pursuit, {
+                  free: item.uiDisplayStyle === 'free',
+                  premium: item.uiDisplayStyle === 'premium'
+                })}
+                key={itemInfo.hash}
+              >
+                <BungieImage
+                  className="perk milestone-img"
+                  src={itemInfo.displayProperties.icon}
+                  title={itemInfo.displayProperties.description}
+                />
+                {item.quantity > 1 && <div className={clsx(styles.amount)}>{item.quantity}</div>}
+              </div>
+            );
+          })}
+        </div>
         <div className="progress">
-          <div className={classNames(styles.progress, 'custom-progress-bar')}>
+          <div className={clsx(styles.progress, 'custom-progress-bar')}>
             <div
               className={styles.progressAmount}
               style={{ width: percent(progressToNextLevel / nextLevelAt) }}
@@ -103,15 +106,16 @@ export default function SeasonalRank({
       </div>
       <div className="milestone-info">
         <span className="milestone-name">
-          {t('Milestone.SeasonalRank')} {seasonalRank}
+          {t('Milestone.SeasonalRank', { rank: seasonalRank })}
         </span>
         <div className="milestone-description">
           {seasonNameDisplay}
-          <br />
-          <div className="season-end">
-            <span className="season-end-title">{t('Milestone.SeasonEnds')}</span>
-            <Countdown endTime={new Date(seasonEnd!)} compact={true} />
-          </div>
+          {seasonEnd && (
+            <div className="season-end">
+              {t('Milestone.SeasonEnds')}
+              <Countdown endTime={new Date(seasonEnd)} compact={true} />
+            </div>
+          )}
         </div>
       </div>
     </div>
