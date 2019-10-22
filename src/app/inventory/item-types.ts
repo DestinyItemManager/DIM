872fbd642a6f7ae3d62d7ad99744dcb96281152d
sundevour <31990469+sundevour@users.noreply.github.com>
2019-10-22 02:23:25 -0700

    clean up more

--- src/app/inventory/item-types.ts
+++ src/app/inventory/item-types.ts
@@ -199,8 +199,6 @@ export interface D1Item extends DimItem {
  * A Destiny 2 item. Use this type when you need specific D2 properties.
  */
 export interface D2Item extends DimItem {
-  /** D2 items store a base stat for comparing unmodified armor */
-  stats: DimStat[] | null;
   /** D2 items use sockets and plugs to represent everything from perks to mods to ornaments and shaders. */
   sockets: DimSockets | null;
   /** Some items have a "flavor objective", such as emblems that track stats. */
