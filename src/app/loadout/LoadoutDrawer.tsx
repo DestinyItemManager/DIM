0b48dd6c96b8ffd8b0a87ff8299e2781d18188fa
Ryan Rushton <ryan.rushton79@gmail.com>
2020-02-15 10:29:51 +1100

    Moved the reopen functionality to a notification.

--- src/app/loadout/LoadoutDrawer.tsx
+++ src/app/loadout/LoadoutDrawer.tsx
@@ -418,8 +418,17 @@ class LoadoutDrawer extends React.Component<Props, State> {
 
   private handleLoadOutSaveResult = (clashingLoadout?: Loadout) => {
     if (clashingLoadout) {
-      this.setState({ show: true, clashingLoadout: copy(clashingLoadout) });
-      loadoutDialogOpen = true;
+      showNotification({
+        type: 'error',
+        title: t('Loadouts.ClashingTitle'),
+        body: t('Loadouts.ClashingDescription', {
+          loadoutName: clashingLoadout.name
+        }),
+        onClick: () => {
+          this.setState({ show: true, clashingLoadout: copy(clashingLoadout) });
+          loadoutDialogOpen = true;
+        }
+      });
     }
   };
 
