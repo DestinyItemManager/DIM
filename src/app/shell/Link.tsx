ff70024063203019de17b999eeffb11ab53ad068
Ben Hollis <ben@benhollis.net>
2020-02-17 21:52:09 -0800

    Convert Link to hooks

--- src/app/shell/Link.tsx
+++ src/app/shell/Link.tsx
@@ -1,60 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Link.m.scss';
 import { DestinyAccount } from '../accounts/destiny-account';
 import { UISrefActive, UISref } from '@uirouter/react';
 import { router } from '../router';
 
-interface Props {
+export default function Link({
+  state,
+  account,
+  children,
+  text,
+  showWhatsNew
+}: {
   account?: DestinyAccount;
   state: string;
   text?: string;
   children?: React.ReactChild;
   showWhatsNew?: boolean;
-}
+}) {
+  // This should be a really simple component, but because of https://github.com/ui-router/react/issues/204
+  // it can't handle lazy states, and we need to use "key" to nuke the whole component tree on updates.
+  const [generation, setGeneration] = useState(0);
+  useEffect(() => router.stateRegistry.onStatesChanged(() => setGeneration((g) => g + 1)), []);
 
-interface State {
-  generation: number;
-}
-
-export default class Link extends React.Component<Props, State> {
-  // tslint:disable-next-line:ban-types
-  private unregisterTransitionHooks: Function[] = [];
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      generation: 0
-    };
-  }
-
-  componentDidMount() {
-    this.unregisterTransitionHooks = [
-      router.stateRegistry.onStatesChanged(() => {
-        this.setState((state) => ({ generation: state.generation + 1 }));
-      })
-    ];
-  }
-
-  componentWillUnmount() {
-    this.unregisterTransitionHooks.forEach((f) => f());
-  }
-
-  render() {
-    const { state, account, children, text, showWhatsNew } = this.props;
-
-    // This should be a really simple component, but because of https://github.com/ui-router/react/issues/204
-    // it can't handle lazy states, and we need to use "key" to nuke the whole component tree on updates.
-
-    return (
-      <UISrefActive key={this.state.generation} class="active">
-        <UISref to={state} params={account}>
-          <a className="link">
-            {showWhatsNew && <span className={styles.badgeNew} />}
-            {children}
-            {text}
-          </a>
-        </UISref>
-      </UISrefActive>
-    );
-  }
+  return (
+    <UISrefActive key={generation} class="active">
+      <UISref to={state} params={account}>
+        <a className="link">
+          {showWhatsNew && <span className={styles.badgeNew} />}
+          {children}
+          {text}
+        </a>
+      </UISref>
+    </UISrefActive>
+  );
 }
