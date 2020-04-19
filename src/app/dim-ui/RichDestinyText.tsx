265f1def67ce039ccbf09be0d8992346554b84b0
sundevour <31990469+sundevour@users.noreply.github.com>
2020-04-18 23:35:12 -0700

    tidy up, account for possible hash lookup failure

--- src/app/dim-ui/RichDestinyText.tsx
+++ src/app/dim-ui/RichDestinyText.tsx
@@ -1,124 +1,105 @@
-import React from 'react';
-import _ from 'lodash';
 import { D1ManifestDefinitions } from '../destiny1/d1-definitions';
 import { D2ManifestDefinitions } from '../destiny2/d2-definitions';
-
-import richTexts from 'data/d2/objective-richTexts.json';
-
-import dmgArc from 'destiny-icons/weapons/damage_arc.svg';
-import dmgVoid from 'destiny-icons/weapons/damage_void.svg';
-import dmgSolar from 'destiny-icons/weapons/damage_solar.svg';
-import dmgKinetic from 'destiny-icons/weapons/environment_hazard.svg';
-
-import headshot from 'destiny-icons/weapons/headshot.svg';
-import melee from 'destiny-icons/weapons/melee.svg';
-import grenade from 'destiny-icons/weapons/grenade.svg';
-
+import React from 'react';
+import _ from 'lodash';
 import autoRifle from 'destiny-icons/weapons/auto_rifle.svg';
-import pulseRifle from 'destiny-icons/weapons/pulse_rifle.svg';
-import scoutRifle from 'destiny-icons/weapons/scout_rifle.svg';
-import sniperRifle from 'destiny-icons/weapons/sniper_rifle.svg';
-import fusionRifle from 'destiny-icons/weapons/fusion_rifle.svg';
-import traceRifle from 'destiny-icons/weapons/beam_weapon.svg';
-import lFusionRifle from 'destiny-icons/weapons/wire_rifle.svg';
-
-import handCannon from 'destiny-icons/weapons/hand_cannon.svg';
-import shotgun from 'destiny-icons/weapons/shotgun.svg';
-import smg from 'destiny-icons/weapons/smg.svg';
 import bow from 'destiny-icons/weapons/bow.svg';
-import sidearm from 'destiny-icons/weapons/sidearm.svg';
-import gLauncherFF from 'destiny-icons/weapons/grenade_launcher-field_forged.svg';
-
+import dmgArc from 'destiny-icons/weapons/damage_arc.svg';
+import dmgKinetic from 'destiny-icons/weapons/environment_hazard.svg';
+import dmgSolar from 'destiny-icons/weapons/damage_solar.svg';
+import dmgVoid from 'destiny-icons/weapons/damage_void.svg';
+import fusionRifle from 'destiny-icons/weapons/fusion_rifle.svg';
 import gLauncher from 'destiny-icons/weapons/grenade_launcher.svg';
-import rLauncher from 'destiny-icons/weapons/rocket_launcher.svg';
-import machinegun from 'destiny-icons/weapons/machinegun.svg';
-import sword from 'destiny-icons/weapons/sword_heavy.svg';
-
-import scorchCannon from 'destiny-icons/weapons/spear_launcher.svg';
-
-import smlBlocker from 'destiny-icons/gambit/blocker_small.svg';
-import medBlocker from 'destiny-icons/gambit/blocker_medium.svg';
+import gLauncherFF from 'destiny-icons/weapons/grenade_launcher-field_forged.svg';
+import grenade from 'destiny-icons/weapons/grenade.svg';
+import handCannon from 'destiny-icons/weapons/hand_cannon.svg';
+import headshot from 'destiny-icons/weapons/headshot.svg';
+import lFusionRifle from 'destiny-icons/weapons/wire_rifle.svg';
 import lrgBlocker from 'destiny-icons/gambit/blocker_large.svg';
-
-import questMarker from 'destiny-icons/explore/quest.svg';
-
+import machinegun from 'destiny-icons/weapons/machinegun.svg';
+import medBlocker from 'destiny-icons/gambit/blocker_medium.svg';
+import melee from 'destiny-icons/weapons/melee.svg';
 import overload from 'destiny-icons/breakers/overload.svg';
 import pierce from 'destiny-icons/breakers/pierce.svg';
+import pulseRifle from 'destiny-icons/weapons/pulse_rifle.svg';
+import questMarker from 'destiny-icons/explore/quest.svg';
+import rLauncher from 'destiny-icons/weapons/rocket_launcher.svg';
+import rT from 'data/d2/objective-richTexts.json';
+import scorchCannon from 'destiny-icons/weapons/spear_launcher.svg';
+import scoutRifle from 'destiny-icons/weapons/scout_rifle.svg';
+import shotgun from 'destiny-icons/weapons/shotgun.svg';
+import sidearm from 'destiny-icons/weapons/sidearm.svg';
+import smg from 'destiny-icons/weapons/smg.svg';
+import smlBlocker from 'destiny-icons/gambit/blocker_small.svg';
+import sniperRifle from 'destiny-icons/weapons/sniper_rifle.svg';
 import stagger from 'destiny-icons/breakers/stagger.svg';
+import sword from 'destiny-icons/weapons/sword_heavy.svg';
+import traceRifle from 'destiny-icons/weapons/beam_weapon.svg';
 
 // matches a bracketed thing in the string, or certain private unicode characters
 const iconPlaceholder = /(\[[^\]]+\]|[\uE000-\uF8FF])/g;
+
 // prettier-ignore
 // this table is too perfect to ruin
 const baseConversionTable: {
-  objectiveHash: number;
+  objectiveHash?: number;
   unicode: string;
   substring: string | null;
   icon: string;
 }[] = [
   // Damage Types
-  { objectiveHash: richTexts['[Arc]'],   unicode: '', substring: null, icon: dmgArc     },
-  { objectiveHash: richTexts['[Void]'],  unicode: '', substring: null, icon: dmgVoid    },
-  { objectiveHash: richTexts['[Solar]'], unicode: '', substring: null, icon: dmgSolar   },
-  { objectiveHash: richTexts['[Kill]'],  unicode: '', substring: null, icon: dmgKinetic },
+  { unicode: '', icon: dmgArc,       objectiveHash: rT['[Arc]']                 },
+  { unicode: '', icon: dmgVoid,      objectiveHash: rT['[Void]']                },
+  { unicode: '', icon: dmgSolar,     objectiveHash: rT['[Solar]']               },
+  { unicode: '', icon: dmgKinetic,   objectiveHash: rT['[Kill]']                },
   // Precision
-  { objectiveHash: richTexts['[Headshot]'], unicode: '', substring: null, icon: headshot },
+  { unicode: '', icon: headshot,     objectiveHash: rT['[Headshot]']            },
   // Abilities
-  { objectiveHash: richTexts['[Melee]'],   unicode: '', substring: null, icon: melee   },
-  { objectiveHash: richTexts['[Grenade]'], unicode: '', substring: null, icon: grenade },
+  { unicode: '', icon: melee,        objectiveHash: rT['[Melee]']               },
+  { unicode: '', icon: grenade,      objectiveHash: rT['[Grenade]']             },
   // All Rifle-class
-  { objectiveHash: richTexts['[Auto Rifle]'],   unicode: '', substring: null, icon: autoRifle   },
-  { objectiveHash: richTexts['[Pulse Rifle]'],  unicode: '', substring: null, icon: pulseRifle  },
-  { objectiveHash: richTexts['[Scout Rifle]'],  unicode: '', substring: null, icon: scoutRifle  },
-  { objectiveHash: richTexts['[Sniper Rifle]'], unicode: '', substring: null, icon: sniperRifle },
-  { objectiveHash: richTexts['[Fusion Rifle]'], unicode: '', substring: null, icon: fusionRifle },
-  { objectiveHash: richTexts['[Trace Rifle]'],  unicode: '', substring: null, icon: traceRifle  },
-  {
-    objectiveHash: richTexts['[Linear Fusion Rifle]'],
-    unicode: '',
-    substring: null,
-    icon: lFusionRifle
-  },
+  { unicode: '', icon: autoRifle,    objectiveHash: rT['[Auto Rifle]']          },
+  { unicode: '', icon: pulseRifle,   objectiveHash: rT['[Pulse Rifle]']         },
+  { unicode: '', icon: scoutRifle,   objectiveHash: rT['[Scout Rifle]']         },
+  { unicode: '', icon: sniperRifle,  objectiveHash: rT['[Sniper Rifle]']        },
+  { unicode: '', icon: fusionRifle,  objectiveHash: rT['[Fusion Rifle]']        },
+  { unicode: '', icon: traceRifle,   objectiveHash: rT['[Trace Rifle]']         },
+  { unicode: '', icon: lFusionRifle, objectiveHash: rT['[Linear Fusion Rifle]'] },
   // Remaining weapons, that are not heavy
-  { objectiveHash: richTexts['[Hand Cannon]'], unicode: '', substring: null, icon: handCannon   },
-  { objectiveHash: richTexts['[Shotgun]'],     unicode: '', substring: null, icon: shotgun      },
-  { objectiveHash: richTexts['[SMG]'],         unicode: '', substring: null, icon: smg          },
-  { objectiveHash: richTexts['[Bow]'],         unicode: '', substring: null, icon: bow          },
-  { objectiveHash: richTexts['[Sidearm]'],     unicode: '', substring: null, icon: sidearm      },
-  { objectiveHash: richTexts[''],             unicode: '', substring: null, icon: gLauncherFF },
+  { unicode: '', icon: handCannon,   objectiveHash: rT['[Hand Cannon]']         },
+  { unicode: '', icon: shotgun,      objectiveHash: rT['[Shotgun]']             },
+  { unicode: '', icon: smg,          objectiveHash: rT['[SMG]']                 },
+  { unicode: '', icon: bow,          objectiveHash: rT['[Bow]']                 },
+  { unicode: '', icon: sidearm,      objectiveHash: rT['[Sidearm]']             },
+  { unicode: '', icon: gLauncherFF,  objectiveHash: rT['']                     },
   // Heavy Weapons
-  {
-    objectiveHash: richTexts['[Grenade Launcher]'],
-    unicode: '',
-    substring: null,
-    icon: gLauncher
-  },
-  { objectiveHash: richTexts['[Rocket Launcher]'], unicode: '', substring: null, icon: rLauncher  },
-  { objectiveHash: richTexts['[Machine Gun]'],     unicode: '', substring: null, icon: machinegun },
-  { objectiveHash: richTexts['[Sword]'],           unicode: '', substring: null, icon: sword      },
+  { unicode: '', icon: gLauncher,    objectiveHash: rT['[Grenade Launcher]']    },
+  { unicode: '', icon: rLauncher,    objectiveHash: rT['[Rocket Launcher]']     },
+  { unicode: '', icon: machinegun,   objectiveHash: rT['[Machine Gun]']         },
+  { unicode: '', icon: sword,        objectiveHash: rT['[Sword]']               },
   // Artifacts that can be picked up and used as weapons
-  { objectiveHash: richTexts[''], unicode: '', substring: null, icon: scorchCannon },
+  { unicode: '', icon: scorchCannon, objectiveHash: rT['']                     },
   // Gambit - Blockers
-  { objectiveHash: richTexts['[Small Blocker]'],  unicode: '', substring: null, icon: smlBlocker },
-  { objectiveHash: richTexts['[Medium Blocker]'], unicode: '', substring: null, icon: medBlocker },
-  { objectiveHash: richTexts['[Large Blocker]'],  unicode: '', substring: null, icon: lrgBlocker },
+  { unicode: '', icon: smlBlocker,   objectiveHash: rT['[Small Blocker]']       },
+  { unicode: '', icon: medBlocker,   objectiveHash: rT['[Medium Blocker]']      },
+  { unicode: '', icon: lrgBlocker,   objectiveHash: rT['[Large Blocker]']       },
   // Quest Markers
-  { objectiveHash: richTexts['[Quest]'], unicode: '', substring: null, icon: questMarker },
+  { unicode: '', icon: questMarker,  objectiveHash: rT['[Quest]']               },
   // Breakers
-  { objectiveHash: richTexts['[Disruption]'],      unicode: '', substring: null, icon: overload },
-  { objectiveHash: richTexts['[Shield-Piercing]'], unicode: '', substring: null, icon: pierce   },
-  { objectiveHash: richTexts['[Stagger]'],         unicode: '', substring: null, icon: stagger  }
-];
+  { unicode: '', icon: overload,     objectiveHash: rT['[Disruption]']          },
+  { unicode: '', icon: pierce,       objectiveHash: rT['[Shield-Piercing]']     },
+  { unicode: '', icon: stagger,      objectiveHash: rT['[Stagger]']             }
+].map((c) => ({ ...c, substring: null }));
 
 /**
  * given defs, uses known examples from the manifest
  * and returns a localized string-to-icon conversion table
- *           "[Rocket Launcher]" -> <svg>
+ *           "[Rocket Launcher]" -> '<svg>'
  */
 const generateConversionTable = _.once((defs: D2ManifestDefinitions) => {
   // loop through conversionTable entries to update them with manifest string info
   baseConversionTable.forEach((iconEntry) => {
-    const objectiveDef = defs.Objective.get(iconEntry.objectiveHash);
+    const objectiveDef = iconEntry.objectiveHash && defs.Objective.get(iconEntry.objectiveHash);
     if (!objectiveDef) {
       return;
     }
