e09a28140a26b65c8a78b14556c4ad280c20e88c
Rob Jones <delphi.actual@gmail.com>
2020-05-16 20:40:51 -0600

    sort json in Github Action

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,38 +50,6 @@ module.exports = function(grunt) {
       }
     },
 
-    'crowdin-request': {
-        options: {
-            'api-key': process.env.CROWDIN_API,
-            'project-identifier': 'destiny-item-manager',
-            filename: 'dim.json'
-        },
-        upload: {
-            srcFile: 'src/locale/dim.json'
-        },
-        download: {
-            outputDir: 'src/locale'
-        }
-    },
-
-    sortJSON: {
-      all: [
-        'src/locale/dim.json',
-        'src/locale/de/dim.json',
-        'src/locale/es-ES/dim.json',
-        'src/locale/fr/dim.json',
-        'src/locale/it/dim.json',
-        'src/locale/ja/dim.json',
-        'src/locale/pt-BR/dim.json',
-        'src/locale/es-MX/dim.json',
-        'src/locale/ko/dim.json',
-        'src/locale/pl/dim.json',
-        'src/locale/ru/dim.json',
-        'src/locale/zh-CN/dim.json',
-        'src/locale/zh-TW/dim.json',
-      ],
-      en: ['src/locale/dim.json']
-    }
   });
 
   grunt.loadNpmTasks('grunt-rsync');
@@ -136,21 +104,7 @@ module.exports = function(grunt) {
     }
   );
 
-  grunt.registerTask('lintJSON-en', [
-    'sortJSON:en'
-  ]);
-
-  grunt.registerTask('lintJSON-all', [
-    'sortJSON:all'
-  ]);
-
-  grunt.registerTask('download_translations', [
-    'crowdin-request:download',
-    'sortJSON:all'
-  ]);
-
   grunt.registerTask('publish_beta', [
-    'sortJSON:all',
     'log_beta_version',
     'precompress',
     'rsync:app_content',
