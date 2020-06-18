d69cf92d9fd90f10a1020414e9fd82872e264d89
Ben Hollis <ben@benhollis.net>
2020-06-17 23:06:16 -0700

    Add separate columns for ghosts

--- src/app/settings/initial-settings.ts
+++ src/app/settings/initial-settings.ts
@@ -1,45 +1,13 @@
 import { Settings as DimApiSettings, defaultSettings } from '@destinyitemmanager/dim-api-types';
 import { defaultLanguage } from 'app/i18n';
-import { KeyedStatHashLists } from 'app/dim-ui/CustomStatTotal';
 
 export interface Settings extends DimApiSettings {
-  /** list of stat hashes of interest, keyed by class enum */
-  readonly customTotalStatsByClass: KeyedStatHashLists;
-
   /** Selected columns for the Vault Organizer */
-  readonly organizerColumnsWeapons: string[];
-  readonly organizerColumnsArmor: string[];
+  readonly organizerColumnsGhost: string[];
 }
 
 export const initialSettingsState: Settings = {
   ...defaultSettings,
   language: defaultLanguage(),
-  customTotalStatsByClass: {},
-  organizerColumnsWeapons: [
-    'icon',
-    'name',
-    'dmg',
-    'power',
-    'locked',
-    'tag',
-    'wishList',
-    'archetype',
-    'perks',
-    'notes',
-  ],
-  organizerColumnsArmor: [
-    'icon',
-    'name',
-    'power',
-    'dmg',
-    'energy',
-    'locked',
-    'tag',
-    'ghost',
-    'modslot',
-    'perks',
-    'stats',
-    'customstat',
-    'notes',
-  ],
+  organizerColumnsGhost: ['icon', 'name', 'locked', 'tag', 'ghost', 'perks', 'notes'],
 };
