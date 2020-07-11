01bcca93121f2739c23c9d3133e2e7db5637d8e1
Ben Hollis <ben@benhollis.net>
2020-07-11 14:54:13 -0700

    Progress on lexer

--- src/app/search/query-parser.test.ts
+++ src/app/search/query-parser.test.ts
@@ -1,4 +1,6 @@
-import { parseQuery } from './query-parser';
+import { parseQuery, lexer, Token } from './query-parser';
+
+// TODO: crazy whitespace combinations
 
 // Some of these are contrived, some are from past search parsing issues
 const cases = [
@@ -15,8 +17,12 @@ const cases = [
   ['not witherhoard'],
   ['-witherhoard'],
   ['perk:수집가'],
+  ['perk:"수집가"'],
+  ['"수집가"'],
+  ['수집가'],
   ['is:rocketlauncher -"cluster" -"tracking module"'],
   ['is:rocketlauncher -"cluster" -\'tracking module\''],
+  ['is:rocketlauncher (perk:"cluster" or perk:"tracking module")'],
   ['(is:hunter power:>=540) or (is:warlock power:>=560)'],
   ['"grenade launcher reserves"'],
   // These test copy/pasting from somewhere that automatically converts quotes to "smart quotes"
@@ -24,7 +30,14 @@ const cases = [
   ['‘grenade launcher reserves’'],
 ];
 
-test.each(cases)('should parse %s', (query) => {
-  const parsed = parseQuery(query);
-  expect(parsed).toMatchSnapshot('lexer');
+test.each(cases)('parse |%s|', (query) => {
+  // Test just the lexer
+  const tokens: Token[] = [];
+  for (const t of lexer(query)) {
+    tokens.push(t);
+  }
+  expect(tokens).toMatchSnapshot('lexer');
+
+  // Test the full parse tree
+  parseQuery(query);
 });
--- src/app/search/query-parser.ts
+++ src/app/search/query-parser.ts
@@ -28,8 +28,12 @@ is:blue is:haspower -is:maxpower
 <string> ::= WORD | "\"" WORD {" " WORD} "\"" | "'" WORD {" " WORD} "'\"'"
 */
 
-type Token = [TokenType] | [TokenType, string];
+export type Token = [TokenType] | [TokenType, string];
 
+/**
+ * The query parser first lexes the string, then parses it into an AST representing the logical
+ * structure of the query.
+ */
 export function parseQuery(query: string) {
   query = query.trim().toLowerCase();
 
@@ -51,8 +55,12 @@ export function parseQuery(query: string) {
   return tokens;
 }
 
-type TokenType = '(' | ')' | 'not' | 'str';
+type TokenType = '(' | ')' | 'not' | 'or' | 'and' | 'str' | 'word';
 
+/**
+ * The lexer yields a series of tokens representing the linear structure of the search query.
+ * This throws an exception if it finds an invalid input.
+ */
 export function* lexer(query: string): Generator<Token> {
   query = query.trim().toLowerCase();
 
@@ -60,46 +68,99 @@ export function* lexer(query: string): Generator<Token> {
   query = query.replace(/[\u2018|\u2019|\u201A]/g, "'");
   query = query.replace(/[\u201C|\u201D|\u201E]/g, '"');
 
+  let match: string | undefined;
   let i = 0;
+
+  const consume = (str: string) => (i += str.length);
+
   while (i < query.length) {
     const char = query[i];
+    const startingIndex = i;
 
-    switch (char) {
-      case '(':
-      case ')':
-        yield [char];
-        break;
-      case '"':
-      case "'":
-        {
-          const quote = char;
-          const remain = query.slice(i + 1);
-          const re = new RegExp('(.*?)' + quote, 'g');
-          re.lastIndex = i + 1;
-          if (re.test(query)) {
-            const str = query.slice(i + 1, re.lastIndex - 1);
-            console.log(i, remain, str);
-            if (str) {
-              i += str.length + 1;
-              yield ['str', str];
-            } else {
-              throw new Error('Unterminated quotes: |' + remain + '| ' + i);
-            }
-          }
-        }
-        break;
-      case '-':
-        yield ['not'];
-        break;
-      case ' ':
-        break;
+    if (char === '(' || char === ')') {
+      // Start/end group
+      // TODO: use whitespace tolerant regex??
+      consume(char);
+      yield [char];
+    } else if (char === '"' || char === "'") {
+      // Quoted string
+      consume(char);
+      const quote = char;
+      // TODO: cache regexp?
+      if ((match = extract(query, i, new RegExp('(.*?)' + quote, 'y'))) !== undefined) {
+        consume(match);
+        // Slice off the last character
+        yield ['str', match.slice(0, match.length - 1)];
+      } else {
+        throw new Error('Unterminated quotes: |' + query.slice(i) + '| ' + i);
+      }
+    } else if (char === '-') {
+      // TODO: whitespace-tolerant regex
+      // minus sign is the same as "not"
+      consume(char);
+      yield ['not'];
+    } else if ((match = extract(query, i, /(not|or|and)/y)) !== undefined) {
+      // boolean keywords
+      consume(match);
+      yield [match as TokenType];
+    } else if ((match = extract(query, i, /[^\s)]+/y)) !== undefined) {
+      // bare words that aren't keywords
+      consume(match);
+      yield ['word', match];
+    } else if ((match = extract(query, i, /\s+/y)) !== undefined) {
+      consume(match);
+      yield ['and'];
+    } else {
+      throw new Error(
+        'unrecognized: |' +
+          query.slice(i) +
+          '| ' +
+          i +
+          ' wtf: ' +
+          /^[^\s)]+/g.test(query.slice(i)) +
+          '|' +
+          (match = extract(query, i, /^[^\s)]+/g)) +
+          '|' +
+          ' :: ' +
+          /^\s+/g.test(query.slice(i)) +
+          '|' +
+          (match = extract(query, i, /^\s+/g)) +
+          '|'
+      );
     }
 
-    i++;
+    if (startingIndex === i) {
+      throw new Error('bug: forgot to consume characters');
+    }
+  }
+}
+
+/**
+ * If `str` matches `re` starting at `index`, return the matched portion of the string. Otherwise return undefined.
+ * This avoids having to make slices of strings just to start the regex in the middle of a string.
+ *
+ * Note that regexes passed to this must have the "sticky" flag set (y) and should not use ^, which will match the
+ * beginning of the string, ignoring the index we want to start from. The sticky flag ensures our regex will match
+ * based on the beginning of the string.
+ */
+function extract(str: string, index: number, re: RegExp): string | undefined {
+  // These checks only run in unit tests
+  if ($DIM_FLAVOR === 'test') {
+    if (!re.sticky) {
+      throw new Error('regexp must be sticky');
+    }
+    if (re.source.startsWith('^')) {
+      throw new Error('regexp cannot start with ^ and be repositioned');
+    }
   }
 
-  // \S*?(["']).*?\1 -> match `is:"stuff here"` or `is:'stuff here'`
-  // [^\s"']+ -> match is:stuff
-  //const searchTerms = query.match(/\S*?(["']).*?\1|[^\s"']+/g) || [];
-  //return searchTerms;
+  re.lastIndex = index;
+  const match = re.exec(str);
+  if (match) {
+    const result = match[0];
+    if (result.length > 0) {
+      return result;
+    }
+  }
+  return undefined;
 }
