08a92558eb25d10533187482f05853adf99e652c
Ben Hollis <ben@benhollis.net>
2020-07-18 18:39:28 -0700

    It only works for quests

--- src/app/inventory/store/d2-item-factory.ts
+++ src/app/inventory/store/d2-item-factory.ts
@@ -381,7 +381,7 @@ export function makeItem(
     breakerType: null,
     visible: true,
     lockable: item.lockable,
-    trackable: item.itemInstanceId && normalBucket.hash === 1345459588,
+    trackable: Boolean(item.itemInstanceId && itemDef.objectives?.questlineItemHash),
     tracked: Boolean(item.state & ItemState.Tracked),
     locked: Boolean(item.state & ItemState.Locked),
     masterwork: Boolean(item.state & ItemState.Masterwork) && itemType !== 'Class',
