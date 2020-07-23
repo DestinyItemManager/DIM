4d26106d81fe8e783548b981c21079c798404a0a
sundevour <31990469+sundevour@users.noreply.github.com>
2020-07-23 11:52:52 -0700

    gearpower styling and i18n

--- src/app/gear-power/GearPower.tsx
+++ src/app/gear-power/GearPower.tsx
@@ -1,44 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { showGearPower$ } from './gear-power';
 import Sheet from '../dim-ui/Sheet';
 import { storesSelector } from '../inventory/selectors';
 import { D2Store } from '../inventory/store-types';
 import { RootState } from '../store/reducers';
-
 import './GearPower.scss';
-
-import { connect, useSelector } from 'react-redux';
-// import { connect, useDispatch, useSelector } from 'react-redux';
-// import { t } from 'app/i18next-t';
-// import clsx from 'clsx';
-
+import { useSelector } from 'react-redux';
+import { t } from 'app/i18next-t';
 import { useSubscription } from 'app/utils/hooks';
-import { useLocation } from 'react-router';
 import { maxLightItemSet } from 'app/loadout/auto-loadouts';
 import { getLight } from 'app/loadout/loadout-utils';
 import BucketIcon from 'app/dim-ui/svgs/BucketIcon';
 import BungieImage from 'app/dim-ui/BungieImage';
 import { itemPop } from 'app/dim-ui/scroll';
 import { FractionalPowerLevel } from 'app/dim-ui/FractionalPowerLevel';
-// import { settingsSelector } from 'app/settings/reducer';
-// import { setSetting } from 'app/settings/actions';
+import clsx from 'clsx';
 
-interface StoreProps {
-  stores: D2Store[];
-}
-
-function mapStateToProps(state: RootState): StoreProps {
-  const stores = storesSelector(state) as D2Store[];
-  return {
-    stores,
-  };
-}
-
-function GearPower() {
-  const [selectedStore, setSelectedStore] = useState<D2Store | undefined>();
-  const reset = () => setSelectedStore(undefined);
-
+export default function GearPower() {
   const stores = useSelector<RootState, D2Store[]>((state) => storesSelector(state) as D2Store[]);
+  const [selectedStore, setSelectedStore] = useState<D2Store | undefined>();
+  const reset = () => {
+    setSelectedStore(undefined);
+  };
 
   useSubscription(() =>
     showGearPower$.subscribe(({ selectedStoreId }) => {
@@ -53,9 +36,6 @@ function GearPower() {
   // const setShowPowerMaxAsEquippable = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   dispatch(setSetting('showPowerMaxAsEquippable', e.target.value === 'equippable'));
   // };
-
-  const { pathname } = useLocation();
-  useEffect(reset, [pathname]);
 
   if (!selectedStore) {
     return null;
@@ -104,37 +84,38 @@ function GearPower() {
         //   drop max
         // </label>
       }
-      <div className="gearGrid">
-        {unrestricted.map((i) => {
-          // (showPowerMaxAsEquippable ? equippable : unrestricted).map((i) => {
-          const powerDiff = (powerFloor - (i.primStat?.value ?? 0)) * -1;
-          const diffSymbol = powerDiff > 0 ? '+' : '';
-          const diffClass = powerDiff > 0 ? 'positive' : 'negative';
-          return (
-            <div key={i.id} className={i.type}>
-              <BucketIcon className="invert" item={i} />
-              <span onClick={() => itemPop(i)}>
-                <BungieImage src={i.icon} />
-              </span>
-              {i.primStat?.value}
-              <span className={diffClass}>
-                ({diffSymbol}
-                {powerDiff})
-              </span>
-            </div>
-          );
-        })}
-      </div>
-      <div className="footNote">
-        Maximum Power isn't limited by the "One Exotic" rule when determining the Power of your
-        drops/powerfuls/pinnacle rewards. Multiple Exotic items were used to calculate your Maximum
-        Power, so the number shown may not be achievable when equipping your items in-game.
+      <div className="gearPowerSheetContent">
+        <div className="gearGrid">
+          {unrestricted.map((i) => {
+            // (showPowerMaxAsEquippable ? equippable : unrestricted).map((i) => {
+            const powerDiff = (powerFloor - (i.primStat?.value ?? 0)) * -1;
+            const diffSymbol = powerDiff >= 0 ? '+' : '';
+            const diffClass = powerDiff > 0 ? 'positive' : powerDiff < 0 ? 'negative' : 'neutral';
+            return (
+              <div key={i.id} className={clsx(i.type, 'gearItem')}>
+                <div onClick={() => itemPop(i)}>
+                  <BungieImage src={i.icon} className="itemImage" />
+                </div>
+                <div className="gearItemInfo">
+                  <div className="primStat">{i.primStat?.value}</div>
+                  <div className="statMeta">
+                    <BucketIcon className="bucketImage" item={i} />
+                    <div className={diffClass}>
+                      {diffSymbol}
+                      {powerDiff}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="footNote">* {t('Loadouts.EquippableDifferent1')}</div>
+        <div className="footNote">{t('Loadouts.EquippableDifferent2')}</div>
       </div>
     </Sheet>
   );
 }
-
-export default connect<StoreProps>(mapStateToProps)(GearPower);
 
 // implement this once item popup & sheet coexist more peacefully
 //
