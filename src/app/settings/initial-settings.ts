cd3f07fc801602033050e8014000c65f06608b22
sundevour <31990469+sundevour@users.noreply.github.com>
2020-07-24 00:44:31 -0700

    forgot to remove equippability from state/settings

--- src/app/settings/initial-settings.ts
+++ src/app/settings/initial-settings.ts
@@ -6,17 +6,11 @@ export interface Settings extends DimApiSettings {
   readonly organizerColumnsGhost: string[];
   readonly loMinPower: number;
   readonly loMinStatTotal: number;
-  /**
-   * if true, a character's "Max Power" is shown using the no-two-exotics rule
-   * if false, it uses the highest items in all slots (how the game determines drop levels)
-   */
-  readonly showPowerMaxAsEquippable: boolean;
 }
 
 export const initialSettingsState: Settings = {
   ...defaultSettings,
   language: defaultLanguage(),
-  showPowerMaxAsEquippable: false,
   loMinPower: 750,
   loMinStatTotal: 55,
   organizerColumnsGhost: ['icon', 'name', 'locked', 'tag', 'ghost', 'perks', 'notes'],
