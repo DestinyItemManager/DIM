243a222e4b5525d6f65f53ef12b53f756952b37b
Kyle Shay <kyle.shay@ymail.com>
2020-08-10 14:58:59 -0700

    Move D1 XP bar styling to the CharacterHeaderXP component

--- src/app/character-tile/CharacterHeaderXP.tsx
+++ src/app/character-tile/CharacterHeaderXP.tsx
@@ -7,6 +7,8 @@ import PressTip from '../dim-ui/PressTip';
 import { percent } from '../shell/filters';
 import { D1ProgressionHashes } from 'app/search/d1-known-values';
 
+import styles from './CharacterHeaderXP.m.scss';
+
 function getLevelBar(store: D1Store) {
   const prestige = store.progression?.progressions.find(
     (p) => p.progressionHash === D1ProgressionHashes.Prestige
@@ -30,15 +32,17 @@ function getLevelBar(store: D1Store) {
 export default function CharacterHeaderXPBar({ store }: { store: D1Store }) {
   const { levelBar, xpTillMote } = getLevelBar(store);
   return (
-    <PressTip tooltip={xpTillMote}>
-      <div className="level-bar">
-        <div
-          className={clsx('level-bar-progress', {
-            'mote-progress': !store.percentToNextLevel,
-          })}
-          style={{ width: percent(levelBar) }}
-        />
-      </div>
-    </PressTip>
+    <div className={styles.xpBar}>
+      <PressTip tooltip={xpTillMote}>
+        <div className={styles.levelBar}>
+          <div
+            className={clsx(styles.levelBarProgress, {
+              [styles.moteProgress]: !store.percentToNextLevel,
+            })}
+            style={{ width: percent(levelBar) }}
+          />
+        </div>
+      </PressTip>
+    </div>
   );
 }
