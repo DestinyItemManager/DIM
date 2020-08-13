0f0b07d90480226d115284f51c93e015840a15e2
Ben Hollis <ben@benhollis.net>
2020-08-12 22:22:08 -0700

    Introduce an inlined marker function for translation labels

--- src/app/i18next-t.ts
+++ src/app/i18next-t.ts
@@ -6,3 +6,13 @@ import i18next, { TOptions } from 'i18next';
  */
 export const t = (key: string | string[], options?: string | TOptions | undefined) =>
   i18next.t(key, options || { skipInterpolation: true });
+
+/**
+ * This is a "marker function" that tells our i18next-scanner that you will translate this string later (tl = translate later).
+ * This way you don't need to pre-translate everything or include redundant comments. This function is inlined and
+ * has no runtime presence.
+ */
+/*@__INLINE__*/
+export function tl<T extends string>(key: T): T {
+  return key;
+}
