fc5b9f32899bda669bc40f77f6fd28ad312b6b52
kyle shay <kyle.shay@ymail.com>
2020-08-31 20:24:07 -0700

    Revert "Fetch stores only if needed"

--- src/app/inventory/Inventory.tsx
+++ src/app/inventory/Inventory.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { DestinyAccount } from '../accounts/destiny-account';
 import Stores from './Stores';
+import { D1StoresService } from './d1-stores';
+import { D2StoresService } from './d2-stores';
+import { connect } from 'react-redux';
+import { RootState } from 'app/store/types';
 import ClearNewItems from './ClearNewItems';
 import StackableDragHelp from './StackableDragHelp';
 import LoadoutDrawer from '../loadout/LoadoutDrawer';
+import { refresh$ } from '../shell/refresh';
 import Compare from '../compare/Compare';
 import D2Farming from '../farming/D2Farming';
 import D1Farming from '../farming/D1Farming';
 import InfusionFinder from '../infuse/InfusionFinder';
 import GearPower from '../gear-power/GearPower';
+import { queueAction } from './action-queue';
 import ErrorBoundary from 'app/dim-ui/ErrorBoundary';
 import DragPerformanceFix from 'app/inventory/DragPerformanceFix';
+import { storesLoadedSelector, isPhonePortraitSelector } from './selectors';
+import { useSubscription } from 'app/utils/hooks';
+import ShowPageLoading from 'app/dim-ui/ShowPageLoading';
 import DragGhostItem from './DragGhostItem';
+import { t } from 'app/i18next-t';
 import MobileInspect from 'app/mobile-inspect/MobileInspect';
 
-interface Props {
+interface ProvidedProps {
   account: DestinyAccount;
+}
+
+interface StoreProps {
+  storesLoaded: boolean;
   isPhonePortrait: boolean;
 }
 
-export default function Inventory({ account, isPhonePortrait }: Props) {
+type Props = ProvidedProps & StoreProps;
+
+function mapStateToProps(state: RootState): StoreProps {
+  return {
+    storesLoaded: storesLoadedSelector(state),
+    isPhonePortrait: isPhonePortraitSelector(state),
+  };
+}
+
+function getStoresService(account: DestinyAccount) {
+  return account.destinyVersion === 1 ? D1StoresService : D2StoresService;
+}
+
+function Inventory({ storesLoaded, account, isPhonePortrait }: Props) {
+  useSubscription(() => {
+    const storesService = getStoresService(account);
+    return refresh$.subscribe(() => queueAction(() => storesService.reloadStores()));
+  });
+
+  useEffect(() => {
+    const storesService = getStoresService(account);
+    if (!storesLoaded) {
+      // TODO: Dispatch an action to load stores instead
+      storesService.getStoresStream(account);
+    }
+  }, [account, storesLoaded]);
+
+  if (!storesLoaded) {
+    return <ShowPageLoading message={t('Loading.Profile')} />;
+  }
+
   return (
     <ErrorBoundary name="Inventory">
       <Stores />
@@ -36,3 +80,5 @@ export default function Inventory({ account, isPhonePortrait }: Props) {
     </ErrorBoundary>
   );
 }
+
+export default connect<StoreProps>(mapStateToProps)(Inventory);
