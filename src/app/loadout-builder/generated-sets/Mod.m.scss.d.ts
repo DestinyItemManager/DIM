55eb39a08ac9b227250e9168841e10998ebf079b
Ben Hollis <ben@benhollis.net>
2020-09-19 18:12:46 -0700

    More cleanup

--- src/app/loadout-builder/generated-sets/Mod.m.scss.d.ts
+++ src/app/loadout-builder/generated-sets/Mod.m.scss.d.ts
@@ -2,10 +2,7 @@
 // Please do not change this file!
 interface CssExports {
   'clickable': string;
-  'energyCost': string;
   'item': string;
-  'largeEnergyCost': string;
-  'largeEnergyCostOverlay': string;
   'largeItem': string;
   'perk': string;
 }
