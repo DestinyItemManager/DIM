44c38365e177cfac1bb90c521a953834c78b29d2
Ben Hollis <ben@benhollis.net>
2020-10-14 18:19:50 -0700

    Enable gradient background, sidecar, mobile inspect on prod

--- config/webpack.js
+++ config/webpack.js
@@ -380,15 +380,15 @@ module.exports = (env) => {
         // Show the triage tab in the item popup
         '$featureFlags.triage': JSON.stringify(env.dev),
         // Drag and drop mobile inspect
-        '$featureFlags.mobileInspect': JSON.stringify(!env.release),
+        '$featureFlags.mobileInspect': JSON.stringify(true),
         // Rearrange buckets in categories
         '$featureFlags.newArrangement': JSON.stringify(true),
         // New background design
-        '$featureFlags.gradientBackground': JSON.stringify(!env.release),
+        '$featureFlags.gradientBackground': JSON.stringify(true),
         // Move the pull from button
         '$featureFlags.movePullFromButton': JSON.stringify(env.dev),
         // Move the item popup actions
-        '$featureFlags.newItemPopupActions': JSON.stringify(!env.release),
+        '$featureFlags.newItemPopupActions': JSON.stringify(true),
         // Enable move amounts
         '$featureFlags.moveAmounts': JSON.stringify(env.release),
         // Enable alternative inventory mode
