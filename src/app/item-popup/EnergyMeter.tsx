b3c4518759c3a467968dd0d14a6c46e76bca1ee8
Ben Hollis <ben@benhollis.net>
2020-10-18 17:13:24 -0700

    Start on editable energy meter

--- src/app/item-popup/EnergyMeter.tsx
+++ src/app/item-popup/EnergyMeter.tsx
@@ -1,10 +1,18 @@
 import ElementIcon from 'app/dim-ui/ElementIcon';
 import { t } from 'app/i18next-t';
 import { DimItem } from 'app/inventory/item-types';
-import { energyNamesByEnum } from 'app/search/d2-known-values';
-import React from 'react';
+import { DestinyEnergyType } from 'bungie-api-ts/destiny2';
+import clsx from 'clsx';
+import { SocketCategoryHashes } from 'data/d2/generated-enums';
+import React, { useState } from 'react';
 import { D2ManifestDefinitions } from '../destiny2/d2-definitions';
-import './EnergyMeter.scss';
+import styles from './EnergyMeter.m.scss';
+
+export const energyStyles = {
+  [DestinyEnergyType.Arc]: styles.arc,
+  [DestinyEnergyType.Thermal]: styles.solar,
+  [DestinyEnergyType.Void]: styles.void,
+};
 
 export default function EnergyMeter({
   defs,
@@ -13,33 +21,61 @@ export default function EnergyMeter({
   defs: D2ManifestDefinitions;
   item: DimItem;
 }) {
+  const [energyCapacity, setEnergyCapacity] = useState(item.energy?.energyCapacity || 0);
+  const [previewCapacity, setPreviewCapacity] = useState<number>();
+
   if (!item.energy) {
     return null;
   }
   const energyTypeHash = item.energy?.energyTypeHash;
   const energyType = (energyTypeHash !== undefined && defs.EnergyType.get(energyTypeHash)) || null;
-  const energyCapacityElementClass = energyNamesByEnum[item.energy.energyType] || null;
 
   // layer in possible total slots, then earned slots, then currently used slots
   const meterIncrements = Array(10)
-    .fill('disabled')
-    .fill('unused', 0, item.energy.energyCapacity)
-    .fill('used', 0, item.energy.energyUsed);
+    .fill(styles.disabled)
+    .fill(styles.unused, 0, Math.max(energyCapacity, previewCapacity || 0))
+    .fill(styles.used, 0, item.energy.energyUsed);
+
+  const onMouseOver = (i: number) => {
+    setEnergyCapacity(Math.max(i, item.energy!.energyCapacity));
+  };
+
+  const onMouseOut = () => {
+    setEnergyCapacity(item.energy!.energyCapacity);
+  };
+
+  const previewUpgrade = (i: number) => {
+    setPreviewCapacity((pc) => (pc ? undefined : i));
+  };
+
+  const tierSockets = item.sockets!.categories.find(
+    (c) => c.category.hash === SocketCategoryHashes.ArmorTier
+  )!;
+  console.log(tierSockets.sockets);
 
   return (
     defs && (
-      <div className="energymeter">
+      <div className={styles.energyMeter}>
         <div className="item-socket-category-name">
           <div>
             <b>{item.energy.energyCapacity}</b> <span>{t('EnergyMeter.Energy')}</span>
           </div>
         </div>
-        <div className={`inner-energymeter ${energyCapacityElementClass}`}>
-          <div className="energymeter-icon">{<ElementIcon element={energyType} />}</div>
+        <div className={clsx(styles.inner, energyStyles[item.energy.energyType])}>
+          <ElementIcon className={styles.icon} element={energyType} />
           {meterIncrements.map((incrementStyle, i) => (
-            <div key={i} className={`energymeter-increments ${incrementStyle}`} />
+            <div
+              key={i}
+              className={clsx(styles.increments, incrementStyle, {
+                [styles.clickable]: i + 1 > item.energy!.energyCapacity,
+              })}
+              onMouseOver={() => onMouseOver(i + 1)}
+              onMouseOut={onMouseOut}
+              onClick={() => previewUpgrade(i + 1)}
+            />
           ))}
         </div>
+        {previewCapacity && <div>Preview {previewCapacity}</div>}
       </div>
     )
   );
