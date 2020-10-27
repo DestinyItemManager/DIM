e63e6989b9a6ad5aeda47fa1825c430b2db80cb3
nev <gh@f-m.fm>
2020-10-27 03:06:12 -0700

    fix comments, use shared utils for bounty expiry

--- src/app/utils/time.ts
+++ src/app/utils/time.ts
@@ -3,7 +3,7 @@ import { t } from 'app/i18next-t';
 /**
  * splits a number of milliseconds into [days, hours, minutes, seconds, milliseconds]
  *
- * negative durations return 0
+ * negative durations are treated as 0
  */
 export function durationFromMs(ms: number) {
   ms = Math.floor(Math.max(0, ms));
@@ -14,14 +14,14 @@ export function durationFromMs(ms: number) {
   const minutes = Math.floor(ms / 60000); // 60000 ms per minute
   ms %= 60000; // ms now has full minutes taken out
   const seconds = Math.floor(ms / 1000); // 1000 ms per second
-  ms %= 1000; // ms now has full minutes taken out
+  ms %= 1000; // ms now has full seconds taken out
   return [days, hours, minutes, seconds, ms];
 }
 
 /**
- * print a number of milliseconds as d:h:m:s:m
+ * print a number of milliseconds as d:h:m:s
  *
- * negative durations return 0
+ * negative durations are treated as 0
  */
 export function timerDurationFromMs(milliseconds: number) {
   const duration = durationFromMs(milliseconds).slice(0, -1);
@@ -31,8 +31,15 @@ export function timerDurationFromMs(milliseconds: number) {
   return duration.map((u, i) => `${u}`.padStart(i === 0 ? 0 : 2, '0')).join(':');
 }
 
-export function i15dDurationFromSeconds(_seconds: number, compact = false) {
-  const [days, hours, minutes] = durationFromMs(_seconds * 1000);
+/**
+ * print a number of milliseconds as something like "4d 0:51",
+ * containing days, minutes, and hours.
+ * uses i18n to choose an appropriate substitute for that "d"
+ *
+ * negative durations are treated as 0
+ */
+export function i15dDurationFromMs(milliseconds: number, compact = false) {
+  const [days, hours, minutes] = durationFromMs(milliseconds);
   const hhMM = `${hours}:${`${minutes}`.padStart(2, '0')}`;
   return days
     ? `${t('Countdown.Days', {
