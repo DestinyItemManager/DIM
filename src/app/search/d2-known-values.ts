b579333049ba8d478fab73020102dcf2b45a0c09
Ben Hollis <ben@benhollis.net>
2020-11-14 21:47:43 -0800

    Misc mod/perk picker cleanups

--- src/app/search/d2-known-values.ts
+++ src/app/search/d2-known-values.ts
@@ -210,8 +210,6 @@ export const ENCOUNTERS_COMPLETED_OBJECTIVE = 3133307686;
 
 export const ARMSMASTER_ACTIVITY_MODIFIER = 3704166961;
 
-export const TRACTION_PERK = 1818103563;
-
 // unfortunately the API's raid .order attribute is odd
 export const raidOrder = [
   3660836525, // levi
