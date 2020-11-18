bd7e6ea6d0c1bcaa104249ed6944835d831aecff
Ben Hollis <ben@benhollis.net>
2020-11-17 18:37:47 -0800

    Fix it up

--- src/app/progress/BountyGuide.tsx
+++ src/app/progress/BountyGuide.tsx
@@ -1,11 +1,20 @@
 import { D2ManifestDefinitions } from 'app/destiny2/d2-definitions';
 import BungieImage from 'app/dim-ui/BungieImage';
+import { t } from 'app/i18next-t';
+import { DimItem } from 'app/inventory/item-types';
+import { moveItemTo } from 'app/inventory/move-item';
 import { DimStore } from 'app/inventory/store-types';
 import { findItemsByBucket } from 'app/inventory/stores-helpers';
+import { showItemPicker } from 'app/item-picker/item-picker';
 import { itemCategoryIcons } from 'app/organizer/item-category-icons';
+import { ThunkDispatchProp } from 'app/store/types';
+import { chainComparator, compareBy, reverseComparator } from 'app/utils/comparators';
+import { itemCanBeEquippedBy } from 'app/utils/item-utils';
+import clsx from 'clsx';
 import { BucketHashes, ItemCategoryHashes } from 'data/d2/generated-enums';
 import pursuitsInfo from 'data/d2/pursuits.json';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import styles from './BountyGuide.m.scss';
 
 /**
@@ -20,6 +29,8 @@ export default function BountyGuide({
   store: DimStore;
   defs: D2ManifestDefinitions;
 }) {
+  const dispatch = useDispatch<ThunkDispatchProp['dispatch']>();
+
   // We only care about bounties - longer-term quests are filtered out.
   const bounties = findItemsByBucket(store, BucketHashes.Quests).filter((item) => {
     const itemDef = defs.InventoryItem.get(item.hash);
@@ -36,73 +47,119 @@ export default function BountyGuide({
     return true;
   });
 
-  const mapped = {};
-  const annotated = bounties.map((i) => {
+  const pullItemCategory = async (itemCategory: number) => {
+    try {
+      const { item } = await showItemPicker({
+        filterItems: (item) =>
+          item.itemCategoryHashes.includes(itemCategory) && itemCanBeEquippedBy(item, store),
+        prompt: t('MovePopup.PullItem', {
+          bucket: defs.ItemCategory.get(itemCategory).displayProperties.name,
+          store: store.name,
+        }),
+      });
+
+      await dispatch(moveItemTo(item, store));
+    } catch (e) {}
+  };
+
+  type DefType = 'ActivityMode' | 'Place' | 'DamageType' | 'ItemCategory';
+
+  const mapped: { [type in DefType]: { [key: number]: DimItem[] } } = {
+    ActivityMode: {},
+    Place: {},
+    DamageType: {},
+    ItemCategory: {},
+  };
+  for (const i of bounties) {
     const info = pursuitsInfo[i.hash];
     if (info) {
       for (const key in info) {
         for (const value of info[key]) {
-          mapped[key] ||= {};
           mapped[key][value] ||= [];
           mapped[key][value].push(i);
         }
       }
     }
-    return info;
-  });
+  }
 
-  console.log(annotated, mapped);
+  const flattened: { type: DefType; value: number; bounties: DimItem[] }[] = Object.entries(
+    mapped
+  ).flatMap(([type, mapping]: [DefType, { [key: number]: DimItem[] }]) =>
+    Object.entries(mapping).map(([value, bounties]) => ({
+      type,
+      value: parseInt(value, 10),
+      bounties,
+    }))
+  );
+
+  flattened.sort(
+    chainComparator(
+      compareBy((f) => f.type),
+      reverseComparator(compareBy((f) => f.bounties.length))
+    )
+  );
 
   return (
     <div className={styles.guide}>
-      {Object.entries(mapped).map(([type, mapping]) =>
-        Object.entries(mapping).map(([value, bounties]) => (
-          <div key={type + value} className={styles.pill}>
-            {(() => {
-              switch (type) {
-                case 'ActivityMode':
-                  return (
-                    <>
+      {flattened.map(({ type, value, bounties }) => (
+        <div
+          key={type + value}
+          className={clsx(styles.pill, { [styles.clickable]: type === 'ItemCategory' })}
+          onClick={type === 'ItemCategory' ? () => pullItemCategory(value) : undefined}
+        >
+          {(() => {
+            switch (type) {
+              case 'ActivityMode':
+                return (
+                  <>
+                    {defs.ActivityMode[value].displayProperties.hasIcon && (
                       <BungieImage
                         height="16"
                         src={defs.ActivityMode[value].displayProperties.icon}
-                      />{' '}
-                      {defs.ActivityMode[value].displayProperties.name} ({bounties.length})
-                    </>
-                  );
-                case 'Place':
-                  return (
-                    <>
+                      />
+                    )}
+                    {defs.ActivityMode[value].displayProperties.name}
+                  </>
+                );
+              case 'Place':
+                return (
+                  <>
+                    {defs.Place.get(value).displayProperties.hasIcon && (
                       <BungieImage height="16" src={defs.Place.get(value).displayProperties.icon} />
-                      {defs.Place.get(value)?.displayProperties.name} ({bounties.length})
-                    </>
-                  );
-                case 'DamageType':
-                  return (
-                    <>
+                    )}
+                    {defs.Place.get(value)?.displayProperties.name}
+                  </>
+                );
+              case 'DamageType':
+                return (
+                  <>
+                    {defs.DamageType.get(value).displayProperties.hasIcon && (
                       <BungieImage
                         height="16"
                         src={defs.DamageType.get(value).displayProperties.icon}
-                      />{' '}
-                      {defs.DamageType.get(value)?.displayProperties.name} ({bounties.length})
-                    </>
-                  );
-                case 'ItemCategory':
-                  return (
-                    <>
+                      />
+                    )}
+                    {defs.DamageType.get(value)?.displayProperties.name}
+                  </>
+                );
+              case 'ItemCategory':
+                return (
+                  <>
+                    {itemCategoryIcons[value] && (
                       <img
                         className={styles.itemCategoryIcon}
                         height="16"
                         src={itemCategoryIcons[value]}
-                      />{' '}
-                      {defs.ItemCategory.get(value)?.displayProperties.name} ({bounties.length})
-                    </>
-                  );
-              }
-            })()}
-          </div>
-        ))
-      )}
+                      />
+                    )}
+                    {defs.ItemCategory.get(value)?.displayProperties.name}
+                  </>
+                );
+            }
+          })()}
+          <span className={styles.count}>({bounties.length})</span>
+        </div>
+      ))}
     </div>
   );
 
