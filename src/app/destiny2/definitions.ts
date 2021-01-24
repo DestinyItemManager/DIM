7d5ffe3ab7c5a6b1a9db3d94c1b31701d8f3c86e
Ben Hollis <ben@benhollis.net>
2021-01-24 00:30:38 -0800

    Update src/app/destiny2/definitions.ts
    Co-authored-by: nev-r <gh@f-m.fm>

--- src/app/destiny2/definitions.ts
+++ src/app/destiny2/definitions.ts
@@ -11,7 +11,6 @@ export class HashLookupFailure extends Error {
   table: string;
   id: number;
 
-  /** Pass in just a message key to set the message to the localized version of that key, or override with the second parameter. */
   constructor(table: string, id: number) {
     super(`hashLookupFailure: ${table}[${id}]`);
     this.table = table;
