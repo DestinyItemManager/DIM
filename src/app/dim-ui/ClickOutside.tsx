27eb0c126e8d557e026ae70f03c6596163949b0e
Ben Hollis <ben@benhollis.net>
2021-03-14 23:21:35 -0700

    Special case body clicks

--- src/app/dim-ui/ClickOutside.tsx
+++ src/app/dim-ui/ClickOutside.tsx
@@ -1,6 +1,6 @@
 import { useEventBusListener } from 'app/utils/hooks';
 import { EventBus } from 'app/utils/observable';
-import React, { useCallback, useContext, useRef } from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 
 export const ClickOutsideContext = React.createContext(new EventBus<React.MouseEvent>());
 
@@ -8,7 +8,7 @@ type Props = React.HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
   /** An optional second ref that will be excluded from being considered "outside". This is good for preventing the triggering button from double-counting clicks. */
   extraRef?: React.RefObject<HTMLElement>;
-  onClickOutside(event: React.MouseEvent): void;
+  onClickOutside(event: React.MouseEvent | MouseEvent): void;
 };
 
 /**
@@ -19,7 +19,7 @@ type Props = React.HTMLAttributes<HTMLDivElement> & {
  * spawned through portals from the item popup.
  */
 export default React.forwardRef(function ClickOutside(
-  { onClickOutside, children, extraRef, ...other }: Props,
+  { onClickOutside, children, extraRef, onClick, ...other }: Props,
   ref: React.RefObject<HTMLDivElement> | null
 ) {
   const localRef = useRef<HTMLDivElement>(null);
@@ -41,6 +41,17 @@ export default React.forwardRef(function ClickOutside(
   );
 
   useEventBusListener(mouseEvents, handleClickOutside);
+
+  // Handle clicks directly on the body as always outside. This handles the case where the ClickoutsideRoot doesn't cover the whole screen.
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (e.target === document.body) {
+        onClickOutside(e);
+      }
+    };
+    document.addEventListener('click', handler);
+    return () => document.removeEventListener('click', handler);
+  });
 
   return (
     <div ref={wrapperRef} {...other}>
