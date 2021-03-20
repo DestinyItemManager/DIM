d724e9d25cb477b59cbc24315cd8dee6f261259f
Ben Hollis <ben@benhollis.net>
2021-03-20 16:22:50 -0700

    Optimize set tracker a bit

--- src/app/loadout-builder/processWorker/set-tracker.ts
+++ src/app/loadout-builder/processWorker/set-tracker.ts
@@ -3,7 +3,12 @@ import { IntermediateProcessArmorSet } from './types';
 
 interface TierSet {
   tier: number;
-  statMixes: { statMix: string; armorSets: IntermediateProcessArmorSet[] }[];
+  // Stat mixes ordered by decreasing lexical order of the statMix string
+  statMixes: {
+    statMix: string;
+    // Armor sets ordered by decreasing power
+    armorSets: IntermediateProcessArmorSet[];
+  }[];
 }
 
 /**
@@ -11,10 +16,10 @@ interface TierSet {
  * as we process new sets with an insertion sort algorithm.
  */
 export class SetTracker {
+  // Tiers ordered by decreasing tier
   tiers: TierSet[] = [];
   totalSets = 0;
-  lowestTier = 100;
-  capacity: number;
+  readonly capacity: number;
 
   constructor(capacity: number) {
     this.capacity = capacity;
@@ -24,87 +29,79 @@ export class SetTracker {
    * A short-circuit helper to check if inserting a set at this total tier could possibly be accepted.
    */
   couldInsert(totalTier: number) {
-    return totalTier >= this.lowestTier || this.totalSets < this.capacity;
+    const lowestKnownTier = this.tiers.length ? this.tiers[this.tiers.length - 1].tier : 0;
+    return totalTier >= lowestKnownTier || this.totalSets < this.capacity;
   }
 
   /**
    * Insert this set into the tracker. If the tracker is at capacity this set or another one may be dropped.
    */
   insert(tier: number, statMix: string, armorSet: IntermediateProcessArmorSet) {
-    if (tier < this.lowestTier) {
-      this.lowestTier = tier;
-    }
     if (this.tiers.length === 0) {
       this.tiers.push({ tier, statMixes: [{ statMix, armorSets: [armorSet] }] });
-      this.totalSets++;
-      return;
-    }
+    } else {
+      outer: for (let tierIndex = 0; tierIndex < this.tiers.length; tierIndex++) {
+        const currentTier = this.tiers[tierIndex];
 
-    let inserted = false;
-    // TODO: reverse order!
-    outer: for (let tierIndex = 0; tierIndex < this.tiers.length; tierIndex++) {
-      const currentTier = this.tiers[tierIndex];
+        if (tier > currentTier.tier) {
+          this.tiers.splice(tierIndex, 0, {
+            tier,
+            statMixes: [{ statMix, armorSets: [armorSet] }],
+          });
+          break outer;
+        }
 
-      if (tier > currentTier.tier) {
-        this.tiers.splice(tierIndex, 0, { tier, statMixes: [{ statMix, armorSets: [armorSet] }] });
-        this.totalSets++;
-        inserted = true;
-        break outer;
-      }
+        if (tier === currentTier.tier) {
+          const currentStatMixes = currentTier.statMixes;
 
-      if (tier === currentTier.tier) {
-        const currentStatMixes = currentTier.statMixes;
+          for (let statMixIndex = 0; statMixIndex < currentStatMixes.length; statMixIndex++) {
+            const currentStatMix = currentStatMixes[statMixIndex];
 
-        for (let statMixIndex = 0; statMixIndex < currentStatMixes.length; statMixIndex++) {
-          const currentStatMix = currentStatMixes[statMixIndex];
+            if (statMix > currentStatMix.statMix) {
+              currentStatMixes.splice(statMixIndex, 0, { statMix, armorSets: [armorSet] });
+              break outer;
+            }
 
-          if (statMix > currentStatMix.statMix) {
-            currentStatMixes.splice(statMixIndex, 0, { statMix, armorSets: [armorSet] });
-            this.totalSets++;
-            inserted = true;
-            break outer;
-          }
-
-          if (currentStatMix.statMix === statMix) {
-            for (
-              let armorSetIndex = 0;
-              armorSetIndex < currentStatMix.armorSets.length;
-              armorSetIndex++
-            ) {
-              if (
-                getPower(armorSet.armor) > getPower(currentStatMix.armorSets[armorSetIndex].armor)
+            if (currentStatMix.statMix === statMix) {
+              const armorSetPower = getPower(armorSet.armor);
+              for (
+                let armorSetIndex = 0;
+                armorSetIndex < currentStatMix.armorSets.length;
+                armorSetIndex++
               ) {
-                currentStatMix.armorSets.splice(armorSetIndex, 0, armorSet);
-              } else {
-                currentStatMix.armorSets.push(armorSet);
+                if (armorSetPower > getPower(currentStatMix.armorSets[armorSetIndex].armor)) {
+                  currentStatMix.armorSets.splice(armorSetIndex, 0, armorSet);
+                  break outer;
+                }
+                if (armorSetIndex === currentStatMix.armorSets.length - 1) {
+                  currentStatMix.armorSets.push(armorSet);
+                  break outer;
+                }
               }
-              this.totalSets++;
-              inserted = true;
+            }
+
+            // This is the worst mix for this tier we've seen, but it could still be better than something at a lower tier
+            if (statMixIndex === currentStatMixes.length - 1) {
+              currentStatMixes.push({ statMix, armorSets: [armorSet] });
               break outer;
             }
           }
+        }
 
-          if (statMixIndex === currentStatMixes.length - 1) {
-            currentStatMixes.push({ statMix, armorSets: [armorSet] });
-            this.totalSets++;
-            inserted = true;
+        // This is lower tier than our previous lowest tier
+        if (tierIndex === this.tiers.length - 1) {
+          if (this.totalSets < this.capacity) {
+            this.tiers.push({ tier, statMixes: [{ statMix, armorSets: [armorSet] }] });
             break outer;
+          } else {
+            // Don't bother inserting it at all
+            return false;
           }
         }
       }
-
-      // This is larger than the largest tier we know about, insert it
-      if (tierIndex === this.tiers.length - 1) {
-        this.tiers.push({ tier, statMixes: [{ statMix, armorSets: [armorSet] }] });
-        this.totalSets++;
-        inserted = true;
-        break outer;
-      }
     }
 
-    if (!inserted) {
-      throw new Error('failed to insert');
-    }
+    this.totalSets++;
 
     return this.trimWorstSet();
   }
@@ -126,7 +123,6 @@ export class SetTracker {
         this.tiers.pop();
       }
     }
-    this.lowestTier = this.tiers[this.tiers.length - 1].tier;
     this.totalSets--;
     return false;
   }
