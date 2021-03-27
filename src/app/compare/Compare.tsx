f2f303f867123db13e145442c7ae159e1c798e8e
Ben Hollis <ben@benhollis.net>
2021-03-27 12:03:08 -0700

    Highlight the name of the initial item in Compare

--- src/app/compare/Compare.tsx
+++ src/app/compare/Compare.tsx
@@ -3,6 +3,7 @@ import { itemPop } from 'app/dim-ui/scroll';
 import { t } from 'app/i18next-t';
 import { setSetting } from 'app/settings/actions';
 import Checkbox from 'app/settings/Checkbox';
+import { AppIcon, faAngleLeft, faAngleRight } from 'app/shell/icons';
 import { RootState, ThunkDispatchProp } from 'app/store/types';
 import { emptyArray } from 'app/utils/empty';
 import { DestinyDisplayPropertiesDefinition } from 'bungie-api-ts/destiny2';
@@ -221,7 +222,9 @@ function Compare(
                 onClick={() => sort(stat.id)}
               >
                 {stat.displayProperties.name}{' '}
-                {stat.id === sortedHash && (sortBetterFirst ? '>' : '<')}
+                {stat.id === sortedHash && (
+                  <AppIcon icon={sortBetterFirst ? faAngleRight : faAngleLeft} />
+                )}
               </div>
             ))}
           </div>
@@ -239,6 +242,7 @@ function Compare(
                 adjustedItemPlugs={adjustedPlugs?.[item.id]}
                 adjustedItemStats={adjustedStats?.[item.id]}
                 compareBaseStats={doCompareBaseStats}
+                isInitialItem={session?.initialItemId === item.id}
               />
             ))}
           </div>
