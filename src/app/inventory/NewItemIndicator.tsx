f989e0865fa3421b660000d07d423c36ae390ce4
Ben Hollis <ben@benhollis.net>
2021-03-27 15:28:03 -0700

    Fix new item indicator to always show on settings button

--- src/app/inventory/NewItemIndicator.tsx
+++ src/app/inventory/NewItemIndicator.tsx
@@ -2,6 +2,12 @@ import clsx from 'clsx';
 import React from 'react';
 import styles from './NewItemIndicator.m.scss';
 
-export default function NewItemIndicator({ className }: { className?: string }) {
-  return <div className={clsx(styles.newItem, className)} />;
+export default function NewItemIndicator({
+  className,
+  alwaysShow = false,
+}: {
+  className?: string;
+  alwaysShow?: boolean;
+}) {
+  return <div className={clsx(styles.newItem, className, { [styles.alwaysShow]: alwaysShow })} />;
 }
