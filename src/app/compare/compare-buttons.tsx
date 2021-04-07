3b600fff5ec5e04932717efcf6271eea3e0ed43b
Rob Jones <delphi.actual@gmail.com>
2021-04-07 15:39:29 -0600

    clean-up

--- src/app/compare/compare-buttons.tsx
+++ src/app/compare/compare-buttons.tsx
@@ -110,7 +110,6 @@ const getRpm = (i: DimItem) => {
 export function findSimilarWeapons(exampleItem: DimItem): CompareButton[] {
   const intrinsic = getWeaponArchetype(exampleItem);
   const intrinsicName = intrinsic?.displayProperties.name || t('Compare.Archetype');
-  const removeAdeptString = exampleItem.name.replace(t('Filter.Adept'), '').trim();
 
   let comparisonSets: CompareButton[] = _.compact([
     // same weapon type
@@ -150,7 +149,7 @@ export function findSimilarWeapons(exampleItem: DimItem): CompareButton[] {
     // exact same weapon, judging by name. might span multiple expansions.
     {
       buttonLabel: exampleItem.name,
-      query: `name:"${removeAdeptString}"`,
+      query: `name:"${exampleItem.name.replace(t('Filter.Adept'), '').trim()}"`,
     },
   ]);
 
