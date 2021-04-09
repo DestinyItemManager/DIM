e06383082cb44ed395602c5f14e5b3b9a99166db
Rob Jones <delphi.actual@gmail.com>
2021-04-09 14:01:20 -0600

    cleanup

--- src/app/item-popup/ItemSocketsGeneral.tsx
+++ src/app/item-popup/ItemSocketsGeneral.tsx
@@ -72,16 +72,14 @@ function ItemSocketsGeneral({
     return null;
   }
 
-  const exoticArmorPluggable =
-    item.bucket.inArmor &&
-    item.sockets.allSockets.find((socket) =>
-      socket.plugged?.plugDef.plug.plugCategoryIdentifier.match(/enhancements\.exotic/i)
-    );
-
   const exoticArmorPerk =
     item.sockets.categories.find(
       (c) => c.category.hash === SocketCategoryHashes.ArmorPerks_LargePerk
-    )?.sockets[0] || exoticArmorPluggable;
+    )?.sockets[0] ||
+    (item.bucket.inArmor &&
+      item.sockets.allSockets.find((socket) =>
+        socket.plugged?.plugDef.plug.plugCategoryIdentifier.match(/enhancements\.exotic/i)
+      ));
 
   // special top level class for styling some specific items' popups differently
   const itemSpecificClass = synthesizerHashes.includes(item.hash)
