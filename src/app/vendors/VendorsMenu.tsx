3b6a85831730eaf0f736488169e35a9384fa6012
Ben Hollis <ben@benhollis.net>
2021-05-02 15:59:12 -0700

    Remove unused css

--- src/app/vendors/VendorsMenu.tsx
+++ src/app/vendors/VendorsMenu.tsx
@@ -1,6 +1,5 @@
 import BungieImage from 'app/dim-ui/BungieImage';
 import PageWithMenu from 'app/dim-ui/PageWithMenu';
-import menuStyles from 'app/dim-ui/PageWithMenu.m.scss';
 import React from 'react';
 import { D2VendorGroup } from './d2-vendors';
 
@@ -11,11 +10,7 @@ export default function VendorsMenu({ groups }: { groups: readonly D2VendorGroup
         <React.Fragment key={group.def.hash}>
           <PageWithMenu.MenuHeader>{group.def.categoryName}</PageWithMenu.MenuHeader>
           {group.vendors.map((vendor) => (
-            <PageWithMenu.MenuButton
-              className={menuStyles.withEngram}
-              anchor={vendor.def.hash.toString()}
-              key={vendor.def.hash}
-            >
+            <PageWithMenu.MenuButton anchor={vendor.def.hash.toString()} key={vendor.def.hash}>
               <BungieImage
                 src={
                   vendor.def.displayProperties.icon ||
