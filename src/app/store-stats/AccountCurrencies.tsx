f1bb9fa62ee18f8fe541811bd21ac26a9a92a162
Ben Hollis <ben@benhollis.net>
2021-05-25 23:13:19 -0700

    Sadmerge

--- src/app/store-stats/AccountCurrencies.tsx
+++ src/app/store-stats/AccountCurrencies.tsx
@@ -26,7 +26,7 @@ export default React.memo(function AccountCurrency() {
       {[other, synth].map(
         (currencyGroup, ci) =>
           currencyGroup.length > 0 && (
-            <React.Fragment key={index}>
+            <React.Fragment key={ci}>
               {currencyGroup.map((currency) => (
                 <React.Fragment key={currency.itemHash}>
                   <BungieImage
