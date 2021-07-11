7de44ccaba191b17b6b546e2678669026dd55e37
Ben Hollis <ben@benhollis.net>
2021-07-11 00:10:18 -0700

    Switch jest to babel from ts-jest, way faster

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
-
 module.exports = {
-  transform: { '\\.(t|j)s$': ['ts-jest'] },
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   verbose: true,
   testTimeout: 60000,
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.js',
-    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-    '^.+\\.scss$': 'identity-obj-proxy',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^data/(.*)$': '<rootDir>/src/data/$1',
+    '^images/(.*)$': '<rootDir>/src/images/$1',
+    '^.+\\.s?css$': 'identity-obj-proxy',
   },
   transformIgnorePatterns: ['node_modules/?!(bungie-api-ts)'],
   globals: {
@@ -21,18 +18,6 @@ module.exports = {
     $DIM_API_KEY: 'xxx',
     $featureFlags: {
       dimApi: true,
-    },
-    'ts-jest': {
-      diagnostics: {
-        warnOnly: true,
-        pathRegex: /\.(spec|test)\.ts$/,
-      },
-      tsconfig: {
-        target: 'ES2015',
-        jsx: 'react',
-        allowJs: true,
-        lib: ['dom', 'esnext'],
-      },
     },
   },
 };
