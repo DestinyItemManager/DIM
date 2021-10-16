4fb6eafc6c2d54abf0dc44e7f17bd9a7ef5703cb
Ben Hollis <ben@benhollis.net>
2021-10-16 15:40:25 -0700

    Group exotics only with eachother, is:customstatlower

--- src/app/search/search-filters/dupes.tsx
+++ src/app/search/search-filters/dupes.tsx
@@ -4,6 +4,7 @@ import { getTag, ItemInfos } from 'app/inventory/dim-item-info';
 import { DimItem } from 'app/inventory/item-types';
 import { getSeason } from 'app/inventory/store/season';
 import { StatsSet } from 'app/loadout-builder/process-worker/stats-set';
+import { Settings } from 'app/settings/initial-settings';
 import { BucketHashes, ItemCategoryHashes } from 'data/d2/generated-enums';
 import _ from 'lodash';
 import { chainComparator, compareBy, reverseComparator } from '../../utils/comparators';
@@ -157,6 +158,14 @@ const dupeFilters: FilterDefinition[] = [
       return (item) => item.bucket.inArmor && duplicates.has(item.id);
     },
   },
+  {
+    keywords: 'customstatlower',
+    description: tl('Filter.CustomStatLower'),
+    filter: ({ allItems, customStats }) => {
+      const duplicates = computeStatDupeLower(allItems, customStats);
+      return (item) => item.bucket.inArmor && duplicates.has(item.id);
+    },
+  },
 ];
 
 export default dupeFilters;
@@ -176,20 +185,27 @@ export function checkIfIsDupe(
   );
 }
 
-function computeStatDupeLower(allItems: DimItem[]) {
+function computeStatDupeLower(
+  allItems: DimItem[],
+  customStats: Settings['customTotalStatsByClass'] = {}
+) {
   const armor = allItems.filter((i) => i.bucket.inArmor);
 
-  const grouped = Object.values(_.groupBy(armor, (i) => `${i.bucket.hash}-${i.classType}`));
+  // Group by class and armor type. Also, compare exotics with each other, not the general pool.
+  const grouped = Object.values(
+    _.groupBy(armor, (i) => `${i.bucket.hash}-${i.classType}-${i.isExotic ? i.hash : ''}`)
+  );
 
   const statsCache = new Map<DimItem, number[]>();
   const dupes = new Set<string>();
 
   for (const item of armor) {
-    if (item.stats) {
+    if (item.stats && item.basePower) {
+      const statsToConsider = customStats[item.classType] ?? armorStats;
       statsCache.set(
         item,
         _.sortBy(
-          item.stats.filter((s) => armorStats.includes(s.statHash)),
+          item.stats.filter((s) => statsToConsider.includes(s.statHash)),
           (s) => s.statHash
         ).map((s) => s.base)
       );
