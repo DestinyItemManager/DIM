639d94eda6a6456300107ae931f2fcbe92d911fb
Ben Hollis <ben@benhollis.net>
2021-11-11 16:43:00 -0800

    Switch D1 loadout builder dnd to hooks

--- src/app/destiny1/loadout-builder/ExcludeItemsDropTarget.tsx
+++ src/app/destiny1/loadout-builder/ExcludeItemsDropTarget.tsx
@@ -1,58 +1,31 @@
+import { DragObject } from 'app/inventory/DraggableInventoryItem';
 import clsx from 'clsx';
 import React from 'react';
-import {
-  ConnectDropTarget,
-  DropTarget,
-  DropTargetConnector,
-  DropTargetMonitor,
-  DropTargetSpec,
-} from 'react-dnd';
+import { useDrop } from 'react-dnd';
 import { DimItem } from '../../inventory/item-types';
 
-interface ExternalProps {
+interface Props {
   className?: string;
   children?: React.ReactNode;
   onExcluded(lockedItem: DimItem): void;
 }
 
-// These are all provided by the DropTarget HOC function
-interface InternalProps {
-  connectDropTarget: ConnectDropTarget;
-  isOver: boolean;
-  canDrop: boolean;
-}
-
-type Props = InternalProps & ExternalProps;
-
-// This determines the behavior of dropping on this target
-const dropSpec: DropTargetSpec<Props> = {
-  drop(props, monitor) {
-    const item = monitor.getItem().item as DimItem;
-    props.onExcluded(item);
-  },
-};
-
-// This forwards drag and drop state into props on the component
-function collect(connect: DropTargetConnector, monitor: DropTargetMonitor): InternalProps {
-  return {
-    // Call this function inside render()
-    // to let React DnD handle the drag events:
-    connectDropTarget: connect.dropTarget(),
-    // You can ask the monitor about the current drag state:
-    isOver: monitor.isOver(),
-    canDrop: monitor.canDrop(),
-  };
-}
-
-function ExcludeItemsDropTarget({
-  className,
-  connectDropTarget,
-  children,
-  isOver,
-  canDrop,
-}: Props) {
-  return connectDropTarget(
+export default function ExcludeItemsDropTarget({ className, children, onExcluded }: Props) {
+  const [{ isOver, canDrop }, dropRef] = useDrop<
+    DragObject,
+    unknown,
+    { isOver: Boolean; canDrop: boolean }
+  >(
+    () => ({
+      accept: ['Helmet', 'Gauntlets', 'Chest', 'Leg', 'ClassItem', 'Artifact', 'Ghost'],
+      collect: (monitor) => ({ isOver: monitor.isOver(), canDrop: monitor.canDrop() }),
+      drop: ({ item }) => onExcluded(item),
+    }),
+    [onExcluded]
+  );
+  return (
     <div
+      ref={dropRef}
       className={clsx(className, {
         'on-drag-hover': canDrop && isOver,
         'on-drag-enter': canDrop,
@@ -62,9 +35,3 @@ function ExcludeItemsDropTarget({
     </div>
   );
 }
-
-export default DropTarget<ExternalProps, InternalProps>(
-  ['Helmet', 'Gauntlets', 'Chest', 'Leg', 'ClassItem', 'Artifact', 'Ghost'],
-  dropSpec,
-  collect
-)(ExcludeItemsDropTarget);
