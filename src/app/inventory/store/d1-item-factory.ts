8fac0969fee67393f003f21771131b21a019970c
Rob Jones <delphi.actual@gmail.com>
2021-11-21 17:51:35 -0700

    bungie spelling

--- src/app/inventory/store/d1-item-factory.ts
+++ src/app/inventory/store/d1-item-factory.ts
@@ -497,7 +497,7 @@ function buildTalentGrid(
     }
 
     // Only one node in this column can be selected (scopes, etc)
-    const exclusiveInColumn = Boolean(talentNodeGroup.exclusiveWithNodes?.length);
+    const exclusiveInColumn = Boolean(talentNodeGroup.exlusiveWithNodes?.length);
 
     // Unlocked is whether or not the material cost has been paid
     // for the node
@@ -508,7 +508,7 @@ function buildTalentGrid(
       // paid once per row.
       (exclusiveInColumn &&
         _.some(
-          talentNodeGroup.exclusiveWithNodes,
+          talentNodeGroup.exlusiveWithNodes,
           (nodeIndex: number) => item.nodes[nodeIndex].isActivated
         ));
 
