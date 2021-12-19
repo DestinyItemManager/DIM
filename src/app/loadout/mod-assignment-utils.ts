4c65fef70acab30c7a195decb02e392a178b4f9e
Ryan Rushton <ryan.rushton79@gmail.com>
2021-12-19 14:03:17 +1100

    Update src/app/loadout/mod-assignment-utils.ts
    Co-authored-by: Ben Hollis <ben@benhollis.net>

--- src/app/loadout/mod-assignment-utils.ts
+++ src/app/loadout/mod-assignment-utils.ts
@@ -572,7 +572,7 @@ function isCombatModValid(
  * For example
  * - if an item needs to be upgraded to fit the mod in, it will be the number of energy levels the user
  * buys
- * - if an items energy needs to be changed, it will be the energy needed for the mods plus
+ * - if an items energy element needs to be changed, it will be the energy needed for the mods plus
  * energy the item originally had (the energy wasted).
  */
 function calculateEnergyChange(
