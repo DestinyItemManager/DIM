aa581408094725b8d86838242d294717e65b11ee
Ben Hollis <ben@benhollis.net>
2021-12-31 13:51:40 -0500

    Disable immer auto-freeze to fix that issue trying to update frozen stats

--- src/app/loadout-drawer/loadout-apply-state.ts
+++ src/app/loadout-drawer/loadout-apply-state.ts
@@ -2,7 +2,10 @@
 
 import { DimItem } from 'app/inventory/item-types';
 import { Observable } from 'app/utils/observable';
-import produce from 'immer';
+import produce, { setAutoFreeze } from 'immer';
+
+// Immer's auto-freeze can get us in trouble because we do sometimes modify the produced item.
+setAutoFreeze(false);
 
 /**
  * What part of the loadout application process are we currently in?
