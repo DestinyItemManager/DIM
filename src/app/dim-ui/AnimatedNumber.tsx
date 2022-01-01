570558ed8e93dc62094f5cc0d79ced4861d962d0
Ben Hollis <ben@benhollis.net>
2022-01-01 14:04:23 -0500

    Switch AnimatedValue to Framer Motion

--- src/app/dim-ui/AnimatedNumber.tsx
+++ src/app/dim-ui/AnimatedNumber.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import { animated, config, useSpring } from 'react-spring';
+import { animate, Spring, useMotionValue } from 'framer-motion';
+import React, { useEffect, useRef } from 'react';
 
-const spring = {
-  ...config.stiff,
-  clamp: true,
+const spring: Spring = {
+  type: 'spring',
+  duration: 0.3,
+  bounce: 0,
 };
 
 /**
  * A number that animates between values.
  */
 export default function AnimatedNumber({ value }: { value: number }) {
-  const animatedValue = useSpring<{ val: number }>({ val: value, config: spring });
-  return (
-    <animated.span>{animatedValue.val.interpolate((val: number) => Math.floor(val))}</animated.span>
-  );
+  const ref = useRef<HTMLSpanElement>(null);
+  const val = useMotionValue(value);
+
+  useEffect(() => {
+    const unsubscribe = val.onChange(
+      (value) => ref.current && (ref.current.textContent = Math.floor(value).toLocaleString())
+    );
+    return unsubscribe;
+  }, [val]);
+
+  useEffect(() => {
+    const controls = animate(val, value, spring);
+    return controls.stop;
+  }, [val, value]);
+
+  return <span ref={ref}>{Math.floor(value).toLocaleString()}</span>;
 }
