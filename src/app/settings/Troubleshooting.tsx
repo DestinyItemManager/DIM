973e6fec46032b1dd3d39b6665ca566b46f10ca2
nev <gh@f-m.fm>
2022-01-17 22:45:02 -0800

    allow uploaded mock inventory response

---
+++ src/app/settings/Troubleshooting.tsx
@@ -0,0 +1,66 @@
+import { currentAccountSelector } from 'app/accounts/selectors';
+import { getStores } from 'app/bungie-api/destiny2-api';
+import FileUpload from 'app/dim-ui/FileUpload';
+import { t } from 'app/i18next-t';
+import { setMockProfileResponse } from 'app/inventory/actions';
+import { useThunkDispatch } from 'app/store/thunk-dispatch';
+import { ThunkResult } from 'app/store/types';
+import { download } from 'app/utils/util';
+import React from 'react';
+import { DropzoneOptions } from 'react-dropzone';
+import { useSelector } from 'react-redux';
+import './settings.scss';
+
+export function TroubleshootingSettings() {
+  const currentAccount = useSelector(currentAccountSelector);
+  const dispatch = useThunkDispatch();
+
+  const saveProfileResponse = async () => {
+    if (currentAccount) {
+      download(
+        JSON.stringify(await getStores(currentAccount), null, '\t'),
+        'profile-data.json',
+        'application/json'
+      );
+    }
+  };
+
+  const importMockProfile: DropzoneOptions['onDrop'] = async (files) => {
+    if (files.length !== 1) {
+      return;
+    }
+
+    try {
+      await dispatch(importMockProfileResponse(files[0]));
+      alert('succeeded');
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  return (
+    <section id="troubleshooting">
+      <div className="setting">
+        <button type="button" className="dim-button" onClick={saveProfileResponse}>
+          {t('Settings.ExportProfile')}
+        </button>
+      </div>
+      {$DIM_FLAVOR === 'dev' && (
+        <div className="setting">
+          <FileUpload
+            title="Upload Profile Response JSON"
+            accept=".json"
+            onDrop={importMockProfile}
+          />
+        </div>
+      )}
+    </section>
+  );
+}
+
+function importMockProfileResponse(file: File): ThunkResult {
+  return async (dispatch) => {
+    const fileText = await file.text();
+    dispatch(setMockProfileResponse(fileText));
+  };
+}
