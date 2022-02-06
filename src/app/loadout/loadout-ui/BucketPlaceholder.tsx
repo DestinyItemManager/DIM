3cb7ec55f95ca9387b8f6e4bce959decc66b260a
Ben Hollis <ben@benhollis.net>
2022-02-05 17:14:15 -0800

    More icons

--- src/app/loadout/loadout-ui/BucketPlaceholder.tsx
+++ src/app/loadout/loadout-ui/BucketPlaceholder.tsx
@@ -1,10 +1,16 @@
+import { bucketsSelector } from 'app/inventory/selectors';
 import { bucketHashToItemCategoryHash, itemCategoryIcons } from 'app/organizer/item-category-icons';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './BucketPlaceholder.m.scss';
 
 export function BucketPlaceholder({ bucketHash }: { bucketHash: number }) {
+  const buckets = useSelector(bucketsSelector)!;
+
+  const bucket = buckets.byHash[bucketHash];
+
   return (
-    <div className={styles.empty}>
+    <div className={styles.empty} title={bucket.name}>
       {bucketHashToItemCategoryHash[bucketHash] && (
         <img
           className={styles.placeholder}
