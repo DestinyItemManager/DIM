54f142208ab748e09b68179077ff02d4d8a1d9d2
Ben Hollis <ben@benhollis.net>
2022-02-07 23:54:33 -0800

    Forgot LoadoutView

--- src/app/loadout/LoadoutView.m.scss.d.ts
+++ src/app/loadout/LoadoutView.m.scss.d.ts
@@ -6,13 +6,7 @@ interface CssExports {
   'contents': string;
   'loadout': string;
   'loadoutNotes': string;
-  'missingItem': string;
   'missingItems': string;
-  'mods': string;
-  'modsGrid': string;
-  'modsPlaceholder': string;
-  'placeholder': string;
-  'showModPlacementButton': string;
   'title': string;
 }
 export const cssExports: CssExports;
