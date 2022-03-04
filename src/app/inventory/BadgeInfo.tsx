93179daa13aea73e070d3506c10f4d429c0a1e33
Vivek Hari <vivek.hari@outlook.co.nz>
2022-03-04 23:27:09 +1300

    Bake shaped SVG icon into a PNG.

--- src/app/inventory/BadgeInfo.tsx
+++ src/app/inventory/BadgeInfo.tsx
@@ -3,7 +3,7 @@ import { InventoryWishListRoll, toUiWishListRoll } from 'app/wishlists/wishlists
 import { DamageType, DestinyEnergyType } from 'bungie-api-ts/destiny2';
 import clsx from 'clsx';
 import { BucketHashes } from 'data/d2/generated-enums';
-import shapedIcon from 'images/shaped.svg';
+import shapedIcon from 'images/shaped.png';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import ElementIcon from '../dim-ui/ElementIcon';
