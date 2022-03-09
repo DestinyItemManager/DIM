9bd72e9b64a1445d61b398bb43484815075c93a7
Rob Jones <delphi.actual@gmail.com>
2022-03-09 12:08:27 -0700

    add drowned alloy

--- src/app/store-stats/MaterialCounts.tsx
+++ src/app/store-stats/MaterialCounts.tsx
@@ -12,7 +12,7 @@ const retiredMats = [31293053, 49145143, 1305274547, 2014411539];
 const showMats = spiderMats.filter((m) => !retiredMats.includes(m));
 const goodMats = [2979281381, 4257549984, 3853748946, 4257549985, 3702027555];
 const seasonal = [747321467, 178490507];
-const crafting = [2497395625, 353704689];
+const crafting = [2497395625, 353704689, 2708128607];
 
 export function MaterialCounts() {
   const allMats = useSelector(materialsSelector);
