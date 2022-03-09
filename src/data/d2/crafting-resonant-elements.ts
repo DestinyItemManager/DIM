8057643f3d59ffdc74605052103807f7053ced81
delphiactual <delphiactual@users.noreply.github.com>
2022-03-09 16:19:53 +0000

    D2AI Build Update - 1869a272

--- src/data/d2/crafting-resonant-elements.ts
+++ src/data/d2/crafting-resonant-elements.ts
@@ -5,3 +5,15 @@ export const resonantElementTagsByObjectiveHash: Record<number, string> = {
   2215515947: 'energetic', // Energetic Element
   3934906303: 'drowned', // Drowned Element
 } as const;
+
+export const resonantMaterialStringVarHashes: {
+  currentCountHash: number;
+  maxCapacityHash: number;
+  materialHash: number;
+}[] = [
+  { currentCountHash: 2653558736, maxCapacityHash: 1852772272, materialHash: 163842160 }, // Ruinous Element
+  { currentCountHash: 2829303739, maxCapacityHash: 107662911, materialHash: 163842161 }, // Adroit Element
+  { currentCountHash: 1178490630, maxCapacityHash: 2677935394, materialHash: 163842162 }, // Mutable Element
+  { currentCountHash: 1238436609, maxCapacityHash: 2920404369, materialHash: 163842163 }, // Energetic Element
+  { currentCountHash: 3436494927, maxCapacityHash: 1317327163, materialHash: 3077242389 }, // Drowned Element
+];
