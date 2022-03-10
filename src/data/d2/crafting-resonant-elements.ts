5b2918b6436b515c7917f95edbd287e103e1e39f
nev-r <nev-r@users.noreply.github.com>
2022-03-10 09:23:22 +0000

    D2AI Build Update - 7707992f

--- src/data/d2/crafting-resonant-elements.ts
+++ src/data/d2/crafting-resonant-elements.ts
@@ -11,9 +11,10 @@ export const resonantMaterialStringVarHashes: {
   maxCapacityHash: number;
   materialHash: number;
 }[] = [
-  { currentCountHash: 2653558736, maxCapacityHash: 1852772272, materialHash: 163842160 }, // Ruinous Element
   { currentCountHash: 2829303739, maxCapacityHash: 107662911, materialHash: 163842161 }, // Adroit Element
-  { currentCountHash: 1178490630, maxCapacityHash: 2677935394, materialHash: 163842162 }, // Mutable Element
-  { currentCountHash: 1238436609, maxCapacityHash: 2920404369, materialHash: 163842163 }, // Energetic Element
   { currentCountHash: 3436494927, maxCapacityHash: 1317327163, materialHash: 3077242389 }, // Drowned Element
+  { currentCountHash: 1238436609, maxCapacityHash: 2920404369, materialHash: 163842163 }, // Energetic Element
+  { currentCountHash: 1178490630, maxCapacityHash: 2677935394, materialHash: 163842162 }, // Mutable Element
+  { currentCountHash: 2653558736, maxCapacityHash: 1852772272, materialHash: 163842160 }, // Ruinous Element
+  { currentCountHash: 2747150405, maxCapacityHash: 2029874829, materialHash: 3491404510 }, // Neutral Element
 ];
