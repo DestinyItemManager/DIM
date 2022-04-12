23e9e82bc8fb24728430b896514bef0fc4c89bf9
robojumper <robojumper@gmail.com>
2022-04-12 22:40:13 +0200

    Fix LO lock armor element option

--- src/app/loadout-builder/process/mappers.ts
+++ src/app/loadout-builder/process/mappers.ts
@@ -1,9 +1,10 @@
 import { AssumeArmorMasterwork, LockArmorEnergyType } from '@destinyitemmanager/dim-api-types';
-import { calculateAssumedItemEnergy, isArmorEnergyLocked } from 'app/loadout/armor-upgrade-utils';
+import { calculateAssumedItemEnergy } from 'app/loadout/armor-upgrade-utils';
 import {
   activityModPlugCategoryHashes,
   knownModPlugCategoryHashes,
 } from 'app/loadout/known-values';
+import { getItemEnergyType } from 'app/loadout/mod-utils';
 import { MAX_ARMOR_ENERGY_CAPACITY, modsWithConditionalStats } from 'app/search/d2-known-values';
 import { chargedWithLightPlugCategoryHashes } from 'app/search/specialty-modslots';
 import {
@@ -114,6 +115,11 @@ export function getTotalModStatChanges(
   return totals;
 }
 
+/**
+ * Turns a real DimItem, armor upgrade rules, and bucket specific mods into the bits of
+ * information relevant for LO. This requires that bucket specific mods have been validated
+ * before.
+ */
 export function mapDimItemToProcessItem({
   dimItem,
   assumeArmorMasterwork,
@@ -145,14 +151,8 @@ export function mapDimItemToProcessItem({
     ? _.sumBy(modsForSlot, (mod) => mod.plug.energyCost?.energyCost || 0)
     : 0;
 
-  // If we have slot specifc mods an energy type has effectively been chosen.
-  let energyType = modsForSlot?.find(
-    (mod) => mod.plug.energyCost?.energyType !== DestinyEnergyType.Any
-  )?.plug.energyCost?.energyType;
-
-  if (!energyType && !isArmorEnergyLocked(dimItem, lockArmorEnergyType)) {
-    energyType = DestinyEnergyType.Any;
-  }
+  // Bucket specific mods have been validated
+  const energyType = getItemEnergyType(dimItem, lockArmorEnergyType, modsForSlot);
 
   return {
     id,
