7b3edd07dabdce098ef6967aa03d4bc8b25c9e0d
Ben Hollis <ben@benhollis.net>
2022-05-25 19:08:45 -0700

    Show deepsight pattern progress

--- src/app/dim-ui/WeaponDeepsightInfo.tsx
+++ src/app/dim-ui/WeaponDeepsightInfo.tsx
@@ -1,15 +1,44 @@
-import { DimDeepsight } from 'app/inventory/item-types';
+import { D2ManifestDefinitions } from 'app/destiny2/d2-definitions';
+import { DimItem } from 'app/inventory/item-types';
+import { profileResponseSelector } from 'app/inventory/selectors';
+import { d2ManifestSelector } from 'app/manifest/selectors';
 import Objective from 'app/progress/Objective';
+import { RootState } from 'app/store/types';
+import { DestinyRecordToastStyle } from 'bungie-api-ts/destiny2';
+import _ from 'lodash';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './WeaponDeepsightInfo.m.scss';
+
+const getItemPatternRecordHash = _.memoize((defs: D2ManifestDefinitions, itemName: string) => {
+  for (const record of Object.values(defs.Record.getAll())) {
+    if (
+      record.completionInfo?.toastStyle === DestinyRecordToastStyle.CraftingRecipeUnlocked &&
+      record.displayProperties.name === itemName
+    ) {
+      return record.hash;
+    }
+  }
+});
 
 /**
  * A progress bar that shows a weapon's Deepsight Resonance attunement progress.
  */
-export function WeaponDeepsightInfo({ deepsightInfo }: { deepsightInfo: DimDeepsight }) {
+export function WeaponDeepsightInfo({ item }: { item: DimItem }) {
+  const deepsightInfo = item.deepsightInfo!;
+  const record = useSelector((state: RootState) => {
+    if (!deepsightInfo.extractPattern) {
+      return undefined;
+    }
+    const defs = d2ManifestSelector(state)!;
+    const profileResponse = profileResponseSelector(state);
+    const recordHash = getItemPatternRecordHash(defs, item.name);
+    return recordHash ? profileResponse?.profileRecords?.data?.records[recordHash] : undefined;
+  });
   return (
     <div className={styles.deepsightProgress}>
       <Objective objective={deepsightInfo.attunementObjective} />
+      {record?.objectives[0] && <Objective objective={record.objectives[0]} />}
     </div>
   );
 }
