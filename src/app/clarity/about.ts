3d0842fc8a5e7449366ff84181ae92f523a5e874
Ice-mourne <Cboweris@gmail.com>
2022-06-11 10:10:05 +0300

    changed Pip1n compendium link

--- src/app/clarity/about.ts
+++ src/app/clarity/about.ts
@@ -1,7 +1,7 @@
 const clarityLinkDirect = 'https://d2clarity.page.link/websiteDIM';
 export const clarityLink = `<a href='${clarityLinkDirect}' target='_blank' rel='noopener noreferrer'>Clarity</a>`;
 const compendiumLinkDirect =
-  'https://docs.google.com/spreadsheets/d/1WaxvbLx7UoSZaBqdFr1u32F2uWVLo-CJunJB4nlGUE4/htmlview?pru=AAABe9E7ngw*TxEsfbPsk5ukmr0FbZfK8w#';
+  'https://docs.google.com/spreadsheets/d/1WaxvbLx7UoSZaBqdFr1u32F2uWVLo-CJunJB4nlGUE4';
 export const compendiumLink = `<a href='${compendiumLinkDirect}' target='_blank' rel='noopener noreferrer'>Pip1n's Destiny Data Compendium</a>`;
 const discordLinkDirect = 'https://d2clarity.page.link/discordDIM';
 export const clarityDiscordLink = `<a href='${discordLinkDirect}' target='_blank' rel='noopener noreferrer'>Clarity Discord Server</a>`;
