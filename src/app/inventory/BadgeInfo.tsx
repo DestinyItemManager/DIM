1ac64de924de717edc9086b0cc2ae1ca63cd9e1a
Ben Hollis <ben@benhollis.net>
2022-06-13 23:31:02 -0700

    Remove DOM element from badge

--- src/app/inventory/BadgeInfo.tsx
+++ src/app/inventory/BadgeInfo.tsx
@@ -80,7 +80,7 @@ export default function BadgeInfo({ item, isCapped, wishlistRoll }: Props) {
           {item.quality.min}%
         </div>
       )}
-      {summaryIcon && <div className={styles.summaryIcon}>{summaryIcon}</div>}
+      {summaryIcon}
       {item.energy ? (
         <span className={clsx(energyTypeStyles[item.energy.energyType], styles.energyCapacity)}>
           {item.energy.energyCapacity}
