6ea7ed4f411c783abecee4c9b442240d9b73a53a
nev <gh@f-m.fm>
2022-06-17 17:39:00 -0700

    compact some weapon pattern UI in item popup

--- src/app/dim-ui/WeaponDeepsightInfo.tsx
+++ src/app/dim-ui/WeaponDeepsightInfo.tsx
@@ -1,6 +1,12 @@
+import { t } from 'app/i18next-t';
 import { DimItem } from 'app/inventory/item-types';
-import Objective from 'app/progress/Objective';
-import React from 'react';
+import { useD2Definitions } from 'app/manifest/selectors';
+import Objective, { ObjectiveValue } from 'app/progress/Objective';
+import { faCheck } from 'app/shell/icons';
+import AppIcon from 'app/shell/icons/AppIcon';
+import { DestinyRecordComponent } from 'bungie-api-ts/destiny2';
+import clsx from 'clsx';
+import PressTip from './PressTip';
 import styles from './WeaponDeepsightInfo.m.scss';
 
 /**
@@ -9,17 +15,70 @@ import styles from './WeaponDeepsightInfo.m.scss';
 export function WeaponDeepsightInfo({ item }: { item: DimItem }) {
   const deepsightInfo = item.deepsightInfo;
   const record = item.patternUnlockRecord;
+  const relevantObjectives = record?.objectives.filter((o) => !o.complete);
 
-  if (!deepsightInfo && !record?.objectives[0]) {
+  if (!deepsightInfo && !relevantObjectives?.length) {
     return null;
   }
 
   return (
     <div className={styles.deepsightProgress}>
-      {deepsightInfo && <Objective objective={deepsightInfo.attunementObjective} />}
-      {record?.objectives.map((objective) => (
-        <Objective key={objective.objectiveHash} objective={objective} />
-      ))}
+      {deepsightInfo ? (
+        <>
+          <PatternUnlockedIndicator record={record} />
+          <div className={styles.deepsightProgressSection}>
+            <Objective objective={deepsightInfo.attunementObjective} />
+          </div>
+        </>
+      ) : (
+        Boolean(relevantObjectives?.length) && (
+          <div className={styles.deepsightProgressSection}>
+            {relevantObjectives!.map((objective) => (
+              <Objective key={objective.objectiveHash} objective={objective} />
+            ))}
+          </div>
+        )
+      )}
     </div>
+  );
+}
+
+function PatternUnlockedIndicator({ record }: { record: DestinyRecordComponent | undefined }) {
+  const defs = useD2Definitions()!;
+  const weaponPatternIcon = defs.InventoryItem.get(3131030715)?.displayProperties.icon;
+
+  if (!record) {
+    return null;
+  }
+  return (
+    <PressTip
+      className={clsx(styles.patternProgress, styles.deepsightProgressSection)}
+      tooltip={
+        <>
+          <h2>{t('MovePopup.CraftingPattern')}</h2>
+          {record?.objectives.map((objective) => (
+            <Objective key={objective.objectiveHash} objective={objective} />
+          ))}
+        </>
+      }
+    >
+      <img className={styles.patternIcon} src={weaponPatternIcon} />
+      <span>
+        {record.objectives.every((o) => o.complete) ? (
+          <AppIcon className={styles.patternOwned} icon={faCheck} />
+        ) : (
+          <>
+            {record.objectives.map((o) => (
+              <ObjectiveValue
+                key={o.objectiveHash}
+                objectiveDef={defs.Objective.get(o.objectiveHash)}
+                progress={o.progress!}
+                completionValue={o.completionValue}
+              />
+            ))}
+          </>
+        )}
+      </span>
+    </PressTip>
   );
 }
