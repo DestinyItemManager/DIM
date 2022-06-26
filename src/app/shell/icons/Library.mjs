2c41158917e78559d466a586024d324a2b2e3930
Ben Hollis <ben@benhollis.net>
2022-06-25 18:29:34 -0700

    Switch to simple exports

--- src/app/shell/icons/Library.mjs
+++ src/app/shell/icons/Library.mjs
@@ -2,163 +2,83 @@
 
 // IMPORTANT: run `yarn fa-subset` after changing this file or new icons won't show up
 
-const faArchive = 'fas fa-archive';
-const faArrowCircleUp = 'fas fa-arrow-circle-up';
-const faArrowRight = 'fas fa-arrow-right';
-const faBan = 'fas fa-ban';
-const faBars = 'fas fa-bars';
-const faBolt = 'fas fa-bolt';
-const faCalculator = 'fas fa-calculator';
-const faCheck = 'fas fa-check';
-const faCheckCircle = 'fas fa-check-circle';
-const faCheckCircleRegular = 'far fa-check-circle';
-const faCog = 'fas fa-cog';
-const faCopy = 'fas fa-copy';
-const faDownload = 'fas fa-file-export';
-const faEnvelope = 'fas fa-envelope';
-const faEraser = 'fas fa-eraser';
-const faLevelUpAlt = 'fas fa-level-up-alt';
-const faLock = 'fas fa-lock';
-const faUnlock = 'fas fa-unlock';
-const faCaretDown = 'fas fa-caret-down';
-const faPencilAlt = 'fas fa-pencil-alt';
-const faPlus = 'fas fa-plus';
-const faCaretRight = 'fas fa-caret-right';
-const faAngleRight = 'fas fa-angle-right';
-const faAngleLeft = 'fas fa-angle-left';
-const faAngleDoubleLeft = 'fas fa-angle-double-left';
-const faAngleDoubleRight = 'fas fa-angle-double-right';
-const faPlusCircle = 'fas fa-plus-circle';
-const faPlusSquare = 'fas fa-plus-square';
-const faQuestionCircle = 'far fa-question-circle';
-const faSearch = 'fas fa-search';
-const faSignOutAlt = 'fas fa-sign-out-alt';
-const faChevronDown = 'fas fa-chevron-down';
-const faChevronUp = 'fas fa-chevron-up';
-const faShare = 'fas fa-share';
-const faShoppingCart = 'fas fa-shopping-cart';
-const faStar = 'fas fa-star';
-const faStarOutline = 'far fa-star';
-const faSync = 'fas fa-sync';
-const faTable = 'fas fa-table';
-const faTag = 'fas fa-tag';
-const faList = 'fas fa-list';
-const faGrid = 'fas fa-th';
-const faThumbsUp = 'fas fa-thumbs-up';
-const faThumbsDown = 'fas fa-thumbs-down';
-const faTimes = 'fas fa-times';
-const faTimesCircle = 'fas fa-times-circle';
-const faTrashAlt = 'far fa-trash-alt';
-const faTwitter = 'fab fa-twitter';
-const faUndo = 'fas fa-undo';
-const faUpload = 'fas fa-file-import';
-const faHeart = 'fas fa-heart';
-const faGlobe = 'fas fa-globe';
-const faStickyNote = 'fas fa-sticky-note';
-const faMinusSquare = 'fas fa-minus-square';
-const faRandom = 'fas fa-random';
-const faEquals = 'fas fa-equals';
-const faEllipsisV = 'fas fa-ellipsis-v';
-const faArrowCircleDown = 'fas fa-arrow-circle-down';
-const faExclamationCircle = 'fas fa-exclamation-circle';
-const faCaretUp = 'fas fa-caret-up';
-const faClock = 'far fa-clock';
-const faExclamationTriangle = 'fas fa-exclamation-triangle';
-const faTshirt = 'fas fa-tshirt';
-const faExternalLinkAlt = 'fas fa-external-link-alt';
-const faBookmarkSolid = 'fas fa-bookmark';
-const faBookmark = 'far fa-bookmark';
-const faBalanceScaleLeft = 'fas fa-balance-scale-left';
-const faGripVertical = 'fas fa-grip-vertical';
-const faThumbtack = 'fas fa-thumbtack';
-
-const faXbox = 'fab fa-xbox';
-const faPlayStation = 'fab fa-playstation';
-const faSteam = 'fab fa-steam';
-const faYoutube = 'fab fa-youtube';
-const faDiscord = 'fab fa-discord';
-const faGithub = 'fab fa-github';
-const faReddit = 'fab fa-reddit';
-
-export {
-  faArchive as archiveIcon,
-  faArrowCircleUp as updateIcon,
-  faArrowRight as rightArrowIcon,
-  faBan as banIcon,
-  faBars as menuIcon,
-  faBolt as boltIcon,
-  faBalanceScaleLeft as compareIcon,
-  faCheck,
-  faCheckCircle as enabledIcon,
-  faCheckCircleRegular as unselectedCheckIcon,
-  faShoppingCart as shoppingCart,
-  faCog as settingsIcon,
-  faCopy as copyIcon,
-  faDownload as downloadIcon,
-  faEllipsisV as kebabIcon,
-  faEnvelope as sendIcon,
-  faEraser as clearIcon,
-  faLevelUpAlt as levellingIcon,
-  faLock as lockIcon,
-  faUnlock as unlockedIcon,
-  faCaretDown as collapseIcon,
-  faPencilAlt as editIcon,
-  faPlus as plusIcon,
-  faThumbtack as pinIcon,
-  faPlusSquare,
-  faCaretRight as expandIcon,
-  faAngleDoubleLeft as maximizeIcon,
-  faAngleDoubleRight as minimizeIcon,
-  faPlusCircle as addIcon,
-  faQuestionCircle as helpIcon,
-  faSearch as searchIcon,
-  faSignOutAlt as signOutIcon,
-  faChevronDown as moveDownIcon,
-  faChevronUp as moveUpIcon,
-  faShare as shareIcon,
-  faStar as starIcon,
-  faStarOutline as starOutlineIcon,
-  faSync as refreshIcon,
-  faTable as spreadsheetIcon,
-  faTag as tagIcon,
-  faThumbsUp as thumbsUpIcon,
-  faThumbsDown as thumbsDownIcon,
-  faTimes as closeIcon,
-  faTimesCircle as disabledIcon,
-  faTimesCircle,
-  faTrashAlt as deleteIcon,
-  faTwitter as twitterIcon,
-  faUndo as undoIcon,
-  faUpload as uploadIcon,
-  faHeart as heartIcon,
-  faGlobe as globeIcon,
-  faStickyNote as stickyNoteIcon,
-  faArrowRight as moveIcon,
-  faBookmark as unTrackedIcon,
-  faBookmarkSolid as trackedIcon,
-  faMinusSquare,
-  faRandom,
-  faEquals,
-  faArrowCircleDown,
-  faExclamationCircle,
-  faCalculator,
-  faCheckCircle,
-  faCaretDown,
-  faCaretUp,
-  faClock,
-  faExclamationTriangle,
-  faTshirt,
-  faGripVertical as dragHandleIcon,
-  faExternalLinkAlt,
-  faXbox,
-  faPlayStation,
-  faSteam,
-  faYoutube,
-  faDiscord,
-  faGithub,
-  faReddit,
-  faList,
-  faGrid,
-  faAngleLeft,
-  faAngleRight,
-};
+export const archiveIcon = 'fas fa-archive';
+export const updateIcon = 'fas fa-arrow-circle-up';
+export const rightArrowIcon = 'fas fa-arrow-right';
+export const moveIcon = rightArrowIcon;
+export const banIcon = 'fas fa-ban';
+export const menuIcon = 'fas fa-bars';
+export const boltIcon = 'fas fa-bolt';
+export const faCalculator = 'fas fa-calculator';
+export const faCheck = 'fas fa-check';
+export const faCheckCircle = 'fas fa-check-circle';
+export const enabledIcon = faCheckCircle;
+export const unselectedCheckIcon = 'far fa-check-circle';
+export const settingsIcon = 'fas fa-cog';
+export const copyIcon = 'fas fa-copy';
+export const downloadIcon = 'fas fa-file-export';
+export const sendIcon = 'fas fa-envelope';
+export const clearIcon = 'fas fa-eraser';
+export const levellingIcon = 'fas fa-level-up-alt';
+export const lockIcon = 'fas fa-lock';
+export const unlockedIcon = 'fas fa-unlock';
+export const faCaretDown = 'fas fa-caret-down';
+export const collapseIcon = faCaretDown;
+export const editIcon = 'fas fa-pencil-alt';
+export const plusIcon = 'fas fa-plus';
+export const expandIcon = 'fas fa-caret-right';
+export const faAngleRight = 'fas fa-angle-right';
+export const faAngleLeft = 'fas fa-angle-left';
+export const maximizeIcon = 'fas fa-angle-double-left';
+export const minimizeIcon = 'fas fa-angle-double-right';
+export const addIcon = 'fas fa-plus-circle';
+export const faPlusSquare = 'fas fa-plus-square';
+export const helpIcon = 'far fa-question-circle';
+export const searchIcon = 'fas fa-search';
+export const signOutIcon = 'fas fa-sign-out-alt';
+export const moveDownIcon = 'fas fa-chevron-down';
+export const moveUpIcon = 'fas fa-chevron-up';
+export const shareIcon = 'fas fa-share';
+export const shoppingCart = 'fas fa-shopping-cart';
+export const starIcon = 'fas fa-star';
+export const starOutlineIcon = 'far fa-star';
+export const refreshIcon = 'fas fa-sync';
+export const spreadsheetIcon = 'fas fa-table';
+export const tagIcon = 'fas fa-tag';
+export const faList = 'fas fa-list';
+export const faGrid = 'fas fa-th';
+export const thumbsUpIcon = 'fas fa-thumbs-up';
+export const thumbsDownIcon = 'fas fa-thumbs-down';
+export const closeIcon = 'fas fa-times';
+export const faTimesCircle = 'fas fa-times-circle';
+export const disabledIcon = faTimesCircle;
+export const deleteIcon = 'far fa-trash-alt';
+export const twitterIcon = 'fab fa-twitter';
+export const undoIcon = 'fas fa-undo';
+export const uploadIcon = 'fas fa-file-import';
+export const heartIcon = 'fas fa-heart';
+export const globeIcon = 'fas fa-globe';
+export const stickyNoteIcon = 'fas fa-sticky-note';
+export const faMinusSquare = 'fas fa-minus-square';
+export const faRandom = 'fas fa-random';
+export const faEquals = 'fas fa-equals';
+export const kebabIcon = 'fas fa-ellipsis-v';
+export const faArrowCircleDown = 'fas fa-arrow-circle-down';
+export const faExclamationCircle = 'fas fa-exclamation-circle';
+export const faCaretUp = 'fas fa-caret-up';
+export const faClock = 'far fa-clock';
+export const faExclamationTriangle = 'fas fa-exclamation-triangle';
+export const faTshirt = 'fas fa-tshirt';
+export const faExternalLinkAlt = 'fas fa-external-link-alt';
+export const trackedIcon = 'fas fa-bookmark';
+export const unTrackedIcon = 'far fa-bookmark';
+export const compareIcon = 'fas fa-balance-scale-left';
+export const dragHandleIcon = 'fas fa-grip-vertical';
+export const pinIcon = 'fas fa-thumbtack';
+export const faXbox = 'fab fa-xbox';
+export const faPlayStation = 'fab fa-playstation';
+export const faSteam = 'fab fa-steam';
+export const faYoutube = 'fab fa-youtube';
+export const faDiscord = 'fab fa-discord';
+export const faGithub = 'fab fa-github';
+export const faReddit = 'fab fa-reddit';
