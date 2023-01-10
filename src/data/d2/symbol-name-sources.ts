1ef13c359c7ebd136a6a6ab07050608c9cc3f00c
robojumper <robojumper@users.noreply.github.com>
2023-01-10 19:59:00 +0000

    D2AI Build Update

---
+++ src/data/d2/symbol-name-sources.ts
@@ -0,0 +1,721 @@
+export const symbolData: {
+  codepoint: number;
+  glyph: string;
+  source?: {
+    tableName: 'Trait' | 'InventoryItem' | 'SandboxPerk' | 'ActivityMode' | 'Objective';
+    hash: number;
+    fromRichText: boolean;
+  };
+}[] = [
+  {
+    codepoint: 57441,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 3112248479,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57640,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 4260353952,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57431,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2722573682,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57639,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 2999301420,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57444,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1656118680,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57632,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 3484134371,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57440,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2747500760,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57638,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2747500761,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57442,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 375052471,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57635,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 3205500087,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57430,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2274196887,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57625,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2274196886,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57432,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 119041298,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57634,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 119041299,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57443,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 2236497009,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57633,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 3769507633,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 61181,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 3769507632,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57445,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1081893461,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57637,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1081893460,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57475,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2021620139,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57476,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2625980631,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57477,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 3683904166,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57616,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 44811435,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57617,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 45245118,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57618,
+    glyph: '',
+  },
+  {
+    codepoint: 57665,
+    glyph: '',
+  },
+  {
+    codepoint: 57493,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 4220332374,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57447,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 852252789,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 61179,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2708585279,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57478,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2028772231,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57620,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2984351206,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57495,
+    glyph: '',
+  },
+  {
+    codepoint: 57636,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 3710811138,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57621,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 3066103996,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57479,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1341767667,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57491,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2321824287,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57494,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2299867342,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57622,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1470370538,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57624,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1293395731,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 61180,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1293395730,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57489,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2642597904,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57480,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 2543177538,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57492,
+    glyph: '',
+    source: {
+      tableName: 'Trait',
+      hash: 2650036230,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 61182,
+    glyph: '',
+    source: {
+      tableName: 'Trait',
+      hash: 2228690371,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57472,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1399219,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57473,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1399216,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57474,
+    glyph: '',
+    source: {
+      tableName: 'InventoryItem',
+      hash: 1399217,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57481,
+    glyph: '',
+    source: {
+      tableName: 'Trait',
+      hash: 4272830254,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57488,
+    glyph: '',
+    source: {
+      tableName: 'Trait',
+      hash: 4272830254,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57497,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 791594362,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57600,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 49808967,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57601,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 189060104,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57602,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 8272054,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57603,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 612360742,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57609,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 141911950,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57607,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 102976778,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57604,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 212380697,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57605,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 273389628,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57606,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 579143207,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57685,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 1217177904,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57686,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 714581758,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57656,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 364984909,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57608,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 13215836,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57619,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 43313268,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57682,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 1878887814,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57683,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 1260068656,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57684,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 606807407,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57666,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 116015535,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57654,
+    glyph: '',
+  },
+  {
+    codepoint: 57667,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 19991929,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57668,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 33657378,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57664,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 85535853,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57657,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 85761957,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57456,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 591985846,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57458,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 669536781,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57457,
+    glyph: '',
+    source: {
+      tableName: 'SandboxPerk',
+      hash: 72139184,
+      fromRichText: true,
+    },
+  },
+  {
+    codepoint: 57669,
+    glyph: '',
+    source: {
+      tableName: 'ActivityMode',
+      hash: 103143560,
+      fromRichText: false,
+    },
+  },
+  {
+    codepoint: 57671,
+    glyph: '',
+  },
+  {
+    codepoint: 57461,
+    glyph: '',
+    source: {
+      tableName: 'Objective',
+      hash: 119206183,
+      fromRichText: true,
+    },
+  },
+];
+const translateManually = [57618, 57665, 57495, 57654, 57671] as const;
+export type TranslateManually = typeof translateManually[number];
+
+/*
+ * Could not find a source for (did the definitions disappear?): 
+
+
+ * Unused rich text replacements (these should only be input actions replaced with the mapped buttons by the game): 
+ * [Stasis: Glyph 0]
+ * [Stasis: Glyph 3 Locked]
+ * [Stasis: Glyph 1 Locked]
+ * [Stasis: Glyph 2 Locked]
+ * [Insert Medal Here]
+ * [Aim Down Sights]
+ * [Alternate Weapon Action]
+ * [Light Attack]
+ * [Heavy Attack]
+ * [Super]
+ * [Sprint]
+ * [Block]
+ * [Shoot]
+ * [Boost]
+ * [Air Move]
+ * [Brake]
+ */
