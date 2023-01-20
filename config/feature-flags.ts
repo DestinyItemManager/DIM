a92bcc19eee21d07439571f3165e934e7035aafb
Ben Hollis <ben@benhollis.net>
2023-01-19 22:55:37 -0800

    Switch webpack to typescript, consolidate feature flags

---
+++ config/feature-flags.ts
@@ -0,0 +1,41 @@
+export function makeFeatureFlags(env: { release: boolean; beta: boolean; dev: boolean }) {
+  return {
+    // Print debug info to console about item moves
+    debugMoves: !env.release,
+    // Debug Service Worker
+    debugSW: !env.release,
+    // Send exception reports to Sentry.io on beta/prod only
+    sentry: !env.dev,
+    // Community-curated wish lists
+    wishLists: true,
+    // Show a banner for supporting a charitable cause
+    issueBanner: false,
+    // Show the triage tab in the item popup
+    triage: !env.release,
+    // Advanced Write Actions (inserting mods)
+    awa: process.env.USER === 'brh', // Only Ben has the keys...
+    // Item feed sidebar
+    itemFeed: true,
+    // Clarity perk descriptions
+    clarityDescriptions: true,
+    // Elgato Stream Deck integration
+    elgatoStreamDeck: true,
+    // Warn when DIM Sync is off and you save some DIM-specific data
+    warnNoSync: true,
+    // Expose the "Add required stat mods" Loadout Optimizer toggle
+    loAutoStatMods: !env.release,
+    // Whether to send cookies to the Bungie.net API
+    apiCookies: false,
+    // If saved DIM API data in IDB is recent enough, don't bother getting it from the server
+    skipDimApiFirstLoadIfRecent: !env.release,
+    // Pretend that Bungie.net is down for maintenance
+    simulateBungieMaintenance: false,
+    // Pretend that Bungie.net is not returning sockets info
+    simulateMissingSockets: false,
+    // Show a "pills" UI for filtering loadouts
+    loadoutFilterPills: true,
+    // Request the PresentationNodes component only needed during
+    // Solstice to associate each character with a set of triumphs.
+    solsticePresentationNodes: false,
+  };
+}
