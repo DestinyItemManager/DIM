b718cc5699cb3e6cd742dc043800a985416ecdae
PiqqiDesigns <eric.otuvaka@gmail.com>
2023-02-15 10:03:23 -0800

    fix: statName added to make filter fix readable

--- src/app/organizer/Columns.tsx
+++ src/app/organizer/Columns.tsx
@@ -160,10 +160,10 @@ export function getColumns(
             return <ItemStatValue stat={stat} item={item} />;
           },
           defaultSort: statInfo.lowerBetter ? SortDirection.ASC : SortDirection.DESC,
-          filter: (value) =>
-            `stat:${_.invert(statHashByName)[statHash]}:${
-              _.invert(statHashByName)[statHash] === `rof` ? `` : `>`
-            }=${value}`,
+          filter: (value) => {
+            const statName = _.invert(statHashByName)[statHash];
+            return `stat:${statName}:${statName === 'rof' ? '=' : '>='}${value}`;
+          },
         };
       })
     ),
