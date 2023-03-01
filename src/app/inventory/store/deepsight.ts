2a665ec59c2286921435ce5e2d1c41e87227802c
Ben Hollis <ben@benhollis.net>
2023-03-01 14:28:28 -0800

    Remove/restore deepsight info

--- src/app/inventory/store/deepsight.ts
+++ src/app/inventory/store/deepsight.ts
@@ -1,32 +1,20 @@
 import { THE_FORBIDDEN_BUCKET } from 'app/search/d2-known-values';
 import { socketContainsPlugWithCategory } from 'app/utils/socket-utils';
-import { DestinyObjectiveProgress } from 'bungie-api-ts/destiny2';
 import { resonantElementTagsByObjectiveHash } from 'data/d2/crafting-resonant-elements';
 import { PlugCategoryHashes } from 'data/d2/generated-enums';
-import { DimDeepsight, DimItem, DimSocket } from '../item-types';
+import { DimItem, DimSocket } from '../item-types';
 
 export const resonantElementObjectiveHashes = Object.keys(resonantElementTagsByObjectiveHash).map(
   (objectiveHashStr) => parseInt(objectiveHashStr, 10)
 );
 
-const fakeObjective: DestinyObjectiveProgress = {
-  complete: false,
-  objectiveHash: 0,
-  progress: 0,
-  visible: false,
-  completionValue: 1000,
-};
-
-export function buildDeepsightInfo(item: DimItem): DimDeepsight | undefined {
+export function buildDeepsightInfo(item: DimItem): boolean {
   const resonanceSocket = getResonanceSocket(item);
   if (!resonanceSocket?.plugged?.plugObjectives) {
-    return undefined;
+    return false;
   }
 
-  const attunementObjective = resonanceSocket.plugged.plugObjectives[0];
-  return {
-    attunementObjective: attunementObjective ?? fakeObjective,
-  };
+  return true;
 }
 
 function getResonanceSocket(item: DimItem): DimSocket | undefined {
@@ -37,7 +25,6 @@ function getResonanceSocket(item: DimItem): DimSocket | undefined {
 
 export function isDeepsightResonanceSocket(socket: DimSocket): boolean {
   return Boolean(
-    socketContainsPlugWithCategory(socket, PlugCategoryHashes.CraftingPlugsWeaponsModsMemories) &&
-      socket.plugged.plugDef.objectives
+    socketContainsPlugWithCategory(socket, PlugCategoryHashes.CraftingPlugsWeaponsModsMemories)
   );
 }
