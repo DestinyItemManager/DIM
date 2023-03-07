12248e1a8c6925866883c1b59eb91d34ecc19fa6
nev <gh@f-m.fm>
2023-03-07 06:14:47 -0800

    feature flags merged wrong

--- config/feature-flags.ts
+++ config/feature-flags.ts
@@ -19,7 +19,7 @@ export function makeFeatureFlags(env: { release: boolean; beta: boolean; dev: bo
     // Show a banner for supporting a charitable cause
     issueBanner: false,
     // Show the triage tab in the item popup
-    triage: !env.release,
+    triage: true,
     // Advanced Write Actions (inserting mods)
     awa: process.env.USER === 'brh', // Only Ben has the keys...
     // Item feed sidebar
@@ -30,7 +30,7 @@ export function makeFeatureFlags(env: { release: boolean; beta: boolean; dev: bo
     elgatoStreamDeck: true,
     // Warn when DIM Sync is off and you save some DIM-specific data
     warnNoSync: true,
-    // Expose the "Add required stat mods" Loadout Optimizer toggle
+    // Expose the "Automatically add stat mods" Loadout Optimizer toggle
     loAutoStatMods: !env.release,
     // Pretend that Bungie.net is down for maintenance
     simulateBungieMaintenance: false,
