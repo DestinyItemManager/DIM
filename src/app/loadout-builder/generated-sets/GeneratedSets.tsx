914bab592a5356da69aac8ae3fbadbe6dccebf3c
Ben Hollis <ben@benhollis.net>
2023-06-03 17:41:08 -0700

    Switch react-virtualized to @tanstack/react-virtual

--- src/app/loadout-builder/generated-sets/GeneratedSets.tsx
+++ src/app/loadout-builder/generated-sets/GeneratedSets.tsx
@@ -1,15 +1,9 @@
 import { LoadoutParameters } from '@destinyitemmanager/dim-api-types';
+import { useWindowVirtualizer } from '@tanstack/react-virtual';
 import { PluggableInventoryItemDefinition } from 'app/inventory/item-types';
 import { DimStore } from 'app/inventory/store-types';
 import { Loadout, ResolvedLoadoutItem } from 'app/loadout-drawer/loadout-types';
-import { Dispatch, memo, useEffect, useRef } from 'react';
-import {
-  CellMeasurer,
-  CellMeasurerCache,
-  List,
-  ListRowRenderer,
-  WindowScroller,
-} from 'react-virtualized';
+import { Dispatch, memo, useLayoutEffect, useRef } from 'react';
 import { LoadoutBuilderAction } from '../loadout-builder-reducer';
 import { ArmorEnergyRules, ArmorSet, ModStatChanges, PinnedItems } from '../types';
 import GeneratedSet, { containerClass } from './GeneratedSet';
@@ -48,70 +42,70 @@ export default memo(function GeneratedSets({
   armorEnergyRules: ArmorEnergyRules;
   notes?: string;
 }) {
-  const windowScroller = useRef<WindowScroller>(null);
+  // Dynamic-height window-based virtual list code based on https://tanstack.com/virtual/v3/docs/examples/react/dynamic
+  const parentRef = useRef<HTMLDivElement>(null);
+  const parentOffsetRef = useRef(0);
+  useLayoutEffect(() => {
+    parentOffsetRef.current = parentRef.current?.offsetTop ?? 0;
+  }, []);
 
-  const cache = useRef(
-    new CellMeasurerCache({
-      defaultHeight: 150,
-      fixedWidth: true,
-    })
-  );
-
-  useEffect(() => {
-    windowScroller.current?.updatePosition();
+  const virtualizer = useWindowVirtualizer({
+    count: sets.length,
+    estimateSize: () => 40,
+    scrollMargin: parentOffsetRef.current,
   });
-
-  const rowRenderer: ListRowRenderer = ({ index, key, parent, style }) => (
-    <CellMeasurer cache={cache.current} columnIndex={0} key={key} parent={parent} rowIndex={index}>
-      {({ registerChild }) => (
-        <div
-          className={containerClass}
-          style={style}
-          ref={registerChild as unknown as React.Ref<HTMLDivElement>}
-        >
-          <GeneratedSet
-            index={index}
-            set={sets[index]}
-            subclass={subclass}
-            selectedStore={selectedStore}
-            lockedMods={lockedMods}
-            pinnedItems={pinnedItems}
-            lbDispatch={lbDispatch}
-            statOrder={statOrder}
-            enabledStats={enabledStats}
-            modStatChanges={modStatChanges}
-            loadouts={loadouts}
-            params={params}
-            halfTierMods={halfTierMods}
-            armorEnergyRules={armorEnergyRules}
-            notes={notes}
-          />
-        </div>
-      )}
-    </CellMeasurer>
-  );
 
   if (sets.length === 0) {
     return null;
   }
 
+  const items = virtualizer.getVirtualItems();
+
   return (
-    <WindowScroller ref={windowScroller}>
-      {({ height, isScrolling, onChildScroll, scrollTop }) => (
-        <List
-          deferredMeasurementCache={cache.current}
-          autoHeight={true}
-          height={height}
-          width={1500}
-          isScrolling={isScrolling}
-          onScroll={onChildScroll}
-          overscanRowCount={2}
-          rowCount={sets.length}
-          rowHeight={cache.current.rowHeight}
-          rowRenderer={rowRenderer}
-          scrollTop={scrollTop}
-        />
-      )}
-    </WindowScroller>
+    <div
+      ref={parentRef}
+      style={{
+        height: `${virtualizer.getTotalSize()}px`,
+        width: '100%',
+        position: 'relative',
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          transform: `translateY(${items[0].start - virtualizer.options.scrollMargin}px)`,
+        }}
+      >
+        {items.map((virtualItem) => (
+          <div
+            key={virtualItem.key}
+            ref={virtualizer.measureElement}
+            className={containerClass}
+            data-index={virtualItem.index}
+          >
+            <GeneratedSet
+              index={virtualItem.index}
+              set={sets[virtualItem.index]}
+              subclass={subclass}
+              selectedStore={selectedStore}
+              lockedMods={lockedMods}
+              pinnedItems={pinnedItems}
+              lbDispatch={lbDispatch}
+              statOrder={statOrder}
+              enabledStats={enabledStats}
+              modStatChanges={modStatChanges}
+              loadouts={loadouts}
+              params={params}
+              halfTierMods={halfTierMods}
+              armorEnergyRules={armorEnergyRules}
+              notes={notes}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 });
