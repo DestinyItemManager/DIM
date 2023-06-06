34344266f31007d034e375c2e3820e8e0b8c6daa
Ben Hollis <ben@benhollis.net>
2023-06-06 15:25:27 -0700

    Try blocking scripts

--- config/webpack.ts
+++ config/webpack.ts
@@ -377,6 +377,8 @@ export default (env: Env) => {
       filename: 'index.html',
       template: 'src/index.html',
       chunks: ['main', 'browsercheck'],
+      // Don't defer scripts so GA can load correctly?
+      scriptLoading: 'blocking',
       templateParameters: {
         version,
         date: new Date(buildTime).toString(),
