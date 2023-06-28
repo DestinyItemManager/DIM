cfde36449a9effafea2cd1fc1608ea2a7534f36f
greggsalonga <greggry.1616085@gmail.com>
2023-06-27 17:13:39 -0700

    button sort of works, need to bugfix

--- src/app/records/Records.tsx
+++ src/app/records/Records.tsx
@@ -47,6 +47,7 @@ export default function Records({ account }: Props) {
   const [completedRecordsHidden, setCompletedRecordsHidden] = useSetting('completedRecordsHidden');
   const [redactedRecordsRevealed, setRedactedRecordsRevealed] =
     useSetting('redactedRecordsRevealed');
+  const [sortRecordProgression, setsortRecordProgression] = useSetting('sortRecordProgression');
 
   const defs = useD2Definitions();
 
@@ -105,6 +106,7 @@ export default function Records({ account }: Props) {
 
   const onToggleCompletedRecordsHidden = (checked: boolean) => setCompletedRecordsHidden(checked);
   const onToggleRedactedRecordsRevealed = (checked: boolean) => setRedactedRecordsRevealed(checked);
+  const onTogglesortRecordProgression = (checked: boolean) => setsortRecordProgression(checked);
 
   return (
     <PageWithMenu className="d2-vendors">
@@ -132,6 +134,13 @@ export default function Records({ account }: Props) {
             onChange={onToggleRedactedRecordsRevealed}
           >
             {t('Triumphs.RevealRedacted')}
+          </CheckButton>
+          <CheckButton
+            name="show-progression"
+            checked={sortRecordProgression}
+            onChange={onTogglesortRecordProgression}
+          >
+            {t('Triumphs.SortRecords')}
           </CheckButton>
         </div>
       </PageWithMenu.Menu>
