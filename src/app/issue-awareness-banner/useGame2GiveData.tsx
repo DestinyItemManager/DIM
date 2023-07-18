e494a86294d8e5c4cf4779c43e7b6afa854eb2c7
Ben Hollis <ben@benhollis.net>
2023-07-17 21:21:24 -0700

    Tie Game2Give refresh to Bungie.net profile refresh

--- src/app/issue-awareness-banner/useGame2GiveData.tsx
+++ src/app/issue-awareness-banner/useGame2GiveData.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useMemo, useState } from 'react';
+import { refresh$ } from 'app/shell/refresh-events';
+import { useEventBusListener } from 'app/utils/hooks';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 interface Game2GiveJSONResponse {
   fundraisingGoal: number;
@@ -27,50 +29,47 @@ export default function useGame2GiveData() {
     [fundraisingState, syncLoaded, syncError]
   );
 
-  useEffect(() => {
-    const syncInterval = 1 * 60 * 1000;
-    const getData = async (cached = true) => {
-      const params = cached ? '' : `?_=${Date.now().toString()}`;
+  const getData = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://bungiefoundation.donordrive.com/api/1.3/participants/19805`
+      );
 
-      try {
-        const response = await fetch(
-          `https://bungiefoundation.donordrive.com/api/1.3/participants/19805${params}`
-        );
-
-        // If there is an error communicating with the Game2Giver server, error gracefully.
-        if (!response.ok) {
-          setSyncError(true);
-          return;
-        }
-
-        const json: Game2GiveJSONResponse = await response.json();
-
-        // If there is unexpected data with the Game2Give response, error gracefully.
-        if (!json) {
-          setSyncError(true);
-          return;
-        }
-
-        // If the request is successful, capture data and reset the error and loading state.
-        setFundraisingState({
-          goal: json.fundraisingGoal,
-          donations: json.sumDonations,
-          streamIsLive: json.streamIsLive,
-          streamIsEnabled: json.streamIsEnabled,
-          streamingChannel: json.streamingChannel,
-        });
-        setSyncLoaded(true);
-        setSyncError(false);
-      } catch {
+      // If there is an error communicating with the Game2Giver server, error gracefully.
+      if (!response.ok) {
         setSyncError(true);
+        return;
       }
-    };
-    const intervalID = setInterval(getData, syncInterval);
 
-    getData(false);
+      const json: Game2GiveJSONResponse = await response.json();
 
-    return () => clearInterval(intervalID);
+      // If there is unexpected data with the Game2Give response, error gracefully.
+      if (!json) {
+        setSyncError(true);
+        return;
+      }
+
+      // If the request is successful, capture data and reset the error and loading state.
+      setFundraisingState({
+        goal: json.fundraisingGoal,
+        donations: json.sumDonations,
+        streamIsLive: json.streamIsLive,
+        streamIsEnabled: json.streamIsEnabled,
+        streamingChannel: json.streamingChannel,
+      });
+      setSyncLoaded(true);
+      setSyncError(false);
+    } catch {
+      setSyncError(true);
+    }
   }, []);
+
+  // Refresh data whenever DIM would refresh the Bungie.net profile
+  useEventBusListener(refresh$, getData);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
   return result;
 }
