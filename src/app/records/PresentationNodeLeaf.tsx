d43f93d0dc912f10970f9f6118cc066bba6c71c4
greggsalonga <greggry.1616085@gmail.com>
2023-07-29 14:27:24 -0700

    edit functions

--- src/app/records/PresentationNodeLeaf.tsx
+++ src/app/records/PresentationNodeLeaf.tsx
@@ -6,7 +6,12 @@ import CollectiblesGrid from './CollectiblesGrid';
 import Craftable from './Craftable';
 import Metrics from './Metrics';
 import { RecordGrid } from './Record';
-import { DimPresentationNodeLeaf } from './presentation-nodes';
+import {
+  DimCollectible,
+  DimMetric,
+  DimPresentationNodeLeaf,
+  DimRecord,
+} from './presentation-nodes';
 
 /**
  * Displays "leaf node" contents for presentation nodes (collectibles, triumphs, metrics)
@@ -24,8 +29,8 @@ export default function PresentationNodeLeaf({
   redactedRecordsRevealed: boolean;
   sortRecordProgression: boolean;
 }) {
-  if (sortRecordProgression && (node.records || node.collectibles || node.metrics)) {
-    node = sortRecords(node);
+  if (sortRecordProgression) {
+    sortRecords(node);
   }
 
   return (
@@ -74,61 +79,62 @@ export default function PresentationNodeLeaf({
 /**
  * Sorts the passed node if it has a DimRecord[] | DimCollectible[] | DimMetric[]
  */
-function sortRecords(node: DimPresentationNodeLeaf): DimPresentationNodeLeaf {
-  const sortedNode = { ...node };
-  // RECORDS
-  if (sortedNode.records) {
-    sortedNode.records = sortBy(sortedNode.records, (record) => {
-      //  Triumph is already completed so move it to back of list.
-      if (
-        record.recordComponent.state & DestinyRecordState.RecordRedeemed ||
-        record.recordComponent.state & DestinyRecordState.CanEquipTitle ||
-        !record.recordComponent.state
-      ) {
-        return -1;
-      }
-
-      // check which key is used to track progress
-      let objectives;
-      if (record.recordComponent.intervalObjectives) {
-        objectives = record.recordComponent.intervalObjectives;
-      } else if (record.recordComponent.objectives) {
-        objectives = record.recordComponent.objectives;
-      } else {
-        // its a legacy triumph so it has no objectives and is not completed
-        return 0;
-      }
-
-      //  Sum up the progress
-      let totalProgress = 0;
-      for (const x of objectives) {
-        // Some progress bars exceed its completionValue
-        if (x.complete) {
-          totalProgress += 1;
-          continue;
-        }
-        totalProgress += x.progress! / x.completionValue;
-      }
-      return totalProgress / objectives.length;
-    }).reverse();
-    // COLLECTIBLES
-  } else if (sortedNode.collectibles) {
-    //  Only need to push owned items to the back of the list
-    sortedNode.collectibles = sortBy(sortedNode.collectibles, (collectible) => {
-      if (collectible.state & DestinyCollectibleState.NotAcquired) {
-        return -1;
-      }
-      return 0;
-    });
-    // METRICS
-  } else if (sortedNode.metrics) {
-    sortedNode.metrics = sortBy(sortedNode.metrics, (metric) => {
-      const objectives = metric.metricComponent.objectiveProgress;
-      if (objectives.complete) {
-        return -1;
-      }
-      return objectives.progress! / objectives.completionValue;
-    }).reverse();
+function sortNode(node: DimPresentationNodeLeaf) {
+  if (node.records) {
+    return sortRecords(node.records);
+  } else if (node.collectibles) {
+    return sortCollectibles(node.collectibles);
+  } else if (node.metrics) {
+    return sortMetrics(node.metrics);
   }
-  return sortedNode;
+}
+
+function sortRecords(records: DimRecord[]) {
+  return sortBy(records, (record) => {
+    //  Triumph is already completed so move it to back of list.
+    if (
+      record.recordComponent.state & DestinyRecordState.RecordRedeemed ||
+      record.recordComponent.state & DestinyRecordState.CanEquipTitle ||
+      !record.recordComponent.state
+    ) {
+      return -1;
+    }
+
+    // check which key is used to track progress
+    let objectives;
+    if (record.recordComponent.intervalObjectives) {
+      objectives = record.recordComponent.intervalObjectives;
+    } else if (record.recordComponent.objectives) {
+      objectives = record.recordComponent.objectives;
+    } else {
+      // its a legacy triumph so it has no objectives and is not completed
+      return 0;
+    }
+
+    //  Sum up the progress
+    let totalProgress = 0;
+    for (const x of objectives) {
+      totalProgress += Math.min(1, x.progress! / x.completionValue);
+    }
+    return totalProgress / objectives.length;
+  }).reverse();
+}
+
+function sortCollectibles(collectibles: DimCollectible[]) {
+  return sortBy(collectibles, (collectible) => {
+    if (collectible.state & DestinyCollectibleState.NotAcquired) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+function sortMetrics(metrics: DimMetric[]) {
+  return sortBy(metrics, (metric) => {
+    const objectives = metric.metricComponent.objectiveProgress;
+    if (objectives.complete) {
+      return -1;
+    }
+    return objectives.progress! / objectives.completionValue;
+  }).reverse();
 }
