150a52951c820f31d84a7d6eae8e4988717de6c4
Ben Hollis <ben@benhollis.net>
2023-07-30 17:16:19 -0700

    Make the mastodon widget font match ours

--- src/app/whats-new/WhatsNew.tsx
+++ src/app/whats-new/WhatsNew.tsx
@@ -18,7 +18,7 @@ export default function WhatsNew() {
         <iframe
           allowFullScreen
           sandbox="allow-top-navigation allow-scripts allow-popups allow-popups-to-escape-sandbox"
-          src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmstdn.games%2Fusers%2FThisIsDIM&theme=dark&size=100&header=false&replies=false&boosts=true"
+          src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmstdn.games%2Fusers%2FThisIsDIM&theme=dark&size=80&header=false&replies=false&boosts=true"
         />
       </div>
 
