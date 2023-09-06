0c9389ee0c52eae021d2a257320f306382ec466d
Chazz Basuta <guise@me.com>
2023-09-06 15:06:31 +0100

    Added harmonizable status/icon beside pattern progress

--- src/app/item-popup/WeaponDeepsightInfo.tsx
+++ src/app/item-popup/WeaponDeepsightInfo.tsx
@@ -1,5 +1,6 @@
 import { DimItem } from 'app/inventory/item-types';
 import Objective from 'app/progress/Objective';
+import { PlugCategoryHashes } from 'data/d2/generated-enums';
 import styles from './WeaponDeepsightInfo.m.scss';
 
 /**
@@ -9,6 +10,12 @@ export function WeaponDeepsightInfo({ item }: { item: DimItem }) {
   const record = item.patternUnlockRecord;
   const relevantObjectives = record?.objectives.filter((o) => !o.complete);
 
+  const isHarmonizable = item.sockets?.allSockets.filter(
+    (s) =>
+      s.plugged?.plugDef.plug.plugCategoryHash ===
+        PlugCategoryHashes.CraftingPlugsWeaponsModsExtractors && s.visibleInGame
+  );
+
   if (!relevantObjectives?.length) {
     return null;
   }
@@ -16,11 +23,21 @@ export function WeaponDeepsightInfo({ item }: { item: DimItem }) {
   return (
     <div className={styles.deepsightProgress}>
       {relevantObjectives && relevantObjectives.length > 0 && (
-        <div className={styles.deepsightProgressSection}>
-          {relevantObjectives.map((objective) => (
-            <Objective key={objective.objectiveHash} objective={objective} showHidden />
-          ))}
-        </div>
+        <>
+          <div className={styles.deepsightProgressSection}>
+            {relevantObjectives.map((objective) => (
+              <Objective key={objective.objectiveHash} objective={objective} showHidden />
+            ))}
+          </div>
+          {isHarmonizable && isHarmonizable.length > 0 && (
+            <div className={styles.deepsightHarmonizableSection}>
+              <div className={styles.harmonizableIcon}>
+                <img src="https://www.bungie.net/common/destiny2_content/icons/e816fd6ecf653de012dfc52087d8d1d9.jpg" />
+              </div>
+              <div>Deepsight harmonizer is available</div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
