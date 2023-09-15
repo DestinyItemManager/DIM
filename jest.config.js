3e709e3e70c826898b4ec883d580e9cecb476d6c
Ben Hollis <ben@benhollis.net>
2023-09-15 00:04:32 -0700

    Rearrange

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,10 @@ module.exports = {
   roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: {
-    // Automatically include paths from tsconfig
-    ...pathsToModuleNameMapper(compilerOptions.paths),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.js',
+    // Automatically include paths from tsconfig
+    ...pathsToModuleNameMapper(compilerOptions.paths),
     '^.+\\.s?css$': 'identity-obj-proxy',
     'Library\\.mjs$': 'identity-obj-proxy',
   },
