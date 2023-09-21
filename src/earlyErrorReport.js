9e4acea19a36e486826f96ec747868d8941e1d44
nev <gh@f-m.fm>
2023-09-21 14:22:09 -0700

    a little indentation never hurt anyone

--- src/earlyErrorReport.js
+++ src/earlyErrorReport.js
@@ -2,11 +2,11 @@ window.onerror = (message, source, line, col, error) => {
   const params = { message, source, line, col };
   // eslint-disable-next-line no-console
   console.log(params, error);
-  const errorBox = document.querySelector("#errorreport");
+  const errorBox = document.querySelector('#errorreport');
   if (errorBox) {
     errorBox.textContent = Object.entries(params)
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      .map(([k, v]) => `${k}:\n${v}`)
-      .join("\n");
+      .map(([k, v]) => `${k}:\n  ${v}`)
+      .join('\n');
   }
 };
