38be96f582855a805fce5274ae12eb0ef585e88c
robojumper <robojumper@gmail.com>
2023-10-08 18:38:59 +0200

    Don't extend archetype socket presstip beyond icon (take 2)

--- src/app/item-popup/ItemSocketsGeneral.tsx
+++ src/app/item-popup/ItemSocketsGeneral.tsx
@@ -158,7 +158,7 @@ function IntrinsicArmorPerk({
   const plugDescriptions = usePlugDescriptions(socket.plugged?.plugDef);
   return (
     <ArchetypeRow minimal={minimal}>
-      <ArchetypeSocket archetypeSocket={socket} item={item} onClick={onPlugClicked}>
+      <ArchetypeSocket archetypeSocket={socket} noTooltip item={item} onClick={onPlugClicked}>
         {!minimal && (
           <div className={styles.armorIntrinsicDescription}>
             {plugDescriptions.perks.map(
