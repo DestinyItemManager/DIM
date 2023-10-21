89cbcc20641443c86ee8585fff95217f8fd75ab6
bhollis <bhollis@users.noreply.github.com>
2023-10-21 06:13:49 +0000

    D2AI Build Update

--- src/data/d2/crafting-resonant-elements.ts
+++
@@ -1,7 +0,0 @@
-export const resonantElementTagsByObjectiveHash: Record<number, string> = {} as const;
-
-export const resonantMaterialStringVarHashes: {
-  currentCountHash: number;
-  maxCapacityHash: number;
-  materialHash: number;
-}[] = [];
