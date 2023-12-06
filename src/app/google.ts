482d0681dad5c1aadd47110e098a8fe1dadf2fb3
Ben Hollis <ben@benhollis.net>
2023-12-05 22:22:19 -0800

    Fix Google Analytics for real this time

--- src/app/google.ts
+++ src/app/google.ts
@@ -6,8 +6,12 @@ declare global {
   }
 }
 
-export function ga(...args: unknown[]) {
-  (window.dataLayer ??= []).push(args);
+window.dataLayer ||= [];
+
+export function ga(..._args: any[]) {
+  // Google Analytics actually requires that we push arguments here, not _args!
+  // eslint-disable-next-line prefer-rest-params
+  window.dataLayer.push(arguments);
 }
 
 export function gaPageView(path: string, title?: string) {
@@ -15,8 +19,6 @@ export function gaPageView(path: string, title?: string) {
     page_title: title,
     page_location: window.location.origin + path,
     page_path: path,
-    dim_version: $DIM_VERSION,
-    dim_flavor: $DIM_FLAVOR,
   });
 }
 
@@ -25,36 +27,20 @@ export function gaEvent(type: string, params: Record<string, string>) {
 }
 
 export function initGoogleAnalytics() {
+  ga('js', new Date());
+  const token = getToken();
+  ga('config', $ANALYTICS_PROPERTY, {
+    allow_ad_personalization_signals: false,
+    allow_google_signals: false,
+    send_page_view: false,
+    user_id: token?.bungieMembershipId,
+    dim_version: $DIM_VERSION,
+    dim_flavor: $DIM_FLAVOR,
+  });
+
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.async = true;
-  // ensure PageViews is always tracked (on script load)
-  script.onload = () => {
-    window.dataLayer ??= [];
-    // https:// constantsolutions.dk/2020/06/delay-loading-of-google-analytics-google-tag-manager-script-for-better-pagespeed-score-and-initial-load/
-    window.dataLayer.push({
-      event: 'gtm.js',
-      'gtm.start': new Date().getTime(),
-      'gtm.uniqueEventId': 0,
-      dim_version: $DIM_VERSION,
-      dim_flavor: $DIM_FLAVOR,
-    });
-    ga('js', new Date());
-    const token = getToken();
-    ga('config', $ANALYTICS_PROPERTY, {
-      store_gac: false,
-      allow_ad_personalization_signals: false,
-      allow_google_signals: false,
-      send_page_view: false,
-      dim_version: $DIM_VERSION,
-      dim_flavor: $DIM_FLAVOR,
-      user_id: token?.bungieMembershipId,
-    });
-    ga('set', {
-      dim_version: $DIM_VERSION,
-      dim_flavor: $DIM_FLAVOR,
-    });
-  };
   script.src = `https://www.googletagmanager.com/gtag/js?id=${$ANALYTICS_PROPERTY}`;
   document.head.appendChild(script);
 }
