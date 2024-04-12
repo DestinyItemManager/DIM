4c5fea37154db5d0d535c3fa3d60a7b744ac9331
robojumper <robojumper@gmail.com>
2024-04-12 14:40:39 +0200

    Clarity Stats schema 1.9

--- src/app/clarity/descriptions/character-stats.ts
+++ src/app/clarity/descriptions/character-stats.ts
@@ -29,12 +29,6 @@ export interface SuperAbility {
   Hash: number;
   /** Array index represents the Character Stat tier. Cooldowns are in seconds. Rounded to 2 decimal points. Note: Rounding to 2 decimal places is solely for improving math precision when combined with Override objects. When displaying these cooldown times, it is STRONGLY recommended to round them to an integer. */
   Cooldowns: number[];
-  /** Numbers are provided in Damage Resist percentages and represent the Damage Resistance the super provied inside PvP. If the array is empty, the DR value is still unknown. If the array only contains one value, it represents the passive DR of the super. If the value is -9999, the value is still unknown (workaround that allows only inputting one of the PvE or PvP values. If other values are present, check the condition array for the conditions of each of them. */
-  PvPDamageResistance: number[];
-  /** Same length as the PvPDamageResistance array and works the exact same way except it stores the DR values for PvE. */
-  PvEDamageResistance: number[];
-  /** Array length matches the length of the DamageResistance arrays. This property can be ignored when the length of the arrays is 1 as that represents the passive DR of the Super that doesn't have a condition. The array contains the condition for each Damage Resist value at the same index in the DamageResistance arrays. Unfortunately, there is no way to realistically account for all the different conditions without using simple text for it so these will be succinct descriptions instead of item hashes or the like. */
-  DRCondition: Description[];
 }
 
 export interface Override {
@@ -43,7 +37,7 @@ export interface Override {
   /** The inventoryItem hash of each ability that is required to trigger the effects of this 'Override'. Only overrides 'Abilities' under the same Character Stat as the 'Override'. Any one of these will trigger its effect defined in the other 'Override' properties. Wildcards: if the requirements array only contains 1 item and it's a 0, any ability tied to this Character Stat will have its cooldown overwritten. Negative numbers in the array indicate filters, these will be the inventoryItem hashes of subclasses multiplied by -1. Any abilities tied to the given subclass will have their cooldowns overwritten. */
   Requirements: number[];
 
-  // One of CooldownOverride, Scalar, or FlatIncrease will be set.
+  // One of CooldownOverride, Scalar, or ChunkEnergyOverride will be set.
 
   /** Array index represents the Character Stat tier. Cooldowns are in seconds. Rounded to 2 decimal points. Overrides the cooldowns of the items listed in the 'Requirements' array before the scalar is applied. Identical to the 'Cooldowns' array of the 'Ability' object. */
   CooldownOverride?: number[];
@@ -52,9 +46,9 @@ export interface Override {
    */
   Scalar?: number[];
   /**
-   * Length of the array is equal to the length of the 'Requirements' array. Each item represents a flat increase to the cooldown time of the abilities (of a subclass) listed in the 'Requirements' array at the same array index. If 'CooldownOverride' or 'Scalar' property is specified: Time is added to the cooldown times at every tier after 'CooldownOverride's and 'Scalar's have been applied.
+   * Length of the array is equal to the length of the <Requirements> array. Each item represents an override of the <ChunkEnergyScalar> property of the abilities (of a subclass) listed in the <Requirements> array at the same array index. If <CooldownOverride> or <Scalar> property is specified: Time is added to the cooldown times at every tier after <CooldownOverride>s and <Scalar>s have been applied.
    */
-  FlatIncrease?: number[];
+  ChunkEnergyOverride?: number[];
 }
 
 export interface StatAbilities {
@@ -91,8 +85,8 @@ export interface Recovery extends StatAbilities {
 }
 
 export interface Resilience extends StatAbilities {
-  /** Array index represents the Resilience tier. The numbers represent your total HP at each tier. 'Health' is a static 70 HP, the rest are what Bungie calls 'Shields' in-game. If you wish to display them separately, just subtract 70 from the numbers to get your shield HP. */
-  TotalHP: DescriptionArray;
+  /** Array index represents the Resilience tier. The numbers represent how much HP your <Shields> have at each tier. <Shields> are the 115 to 130 HP 'right-side portion' of your Total HP alongside <Health>. The amount of <Health> you have depends on the activity: 100 HP in most Crucible playlists (excluding Momentum Control and Mayhem) and 70 HP everywhere else. */
+  ShieldHP: DescriptionArray;
   /** Array index represents the Resilience tier. The numbers represent the percentage damage resistance granted IN PVE at each tier. */
   PvEDamageResistance: DescriptionArray;
   /** Array index represents the Resilience tier. The numbers represent the percentage flinch resistance granted at each tier. */
