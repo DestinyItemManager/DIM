c0518b52e2e280e5ca17fbf064260efde0739830
Ryan <ryan.rushton79@gmail.com>
2024-05-21 08:24:03 +1000

    Immer changes

--- src/app/loadout-drawer/reducer.ts
+++ src/app/loadout-drawer/reducer.ts
@@ -1,4 +1,3 @@
-import { produce } from 'immer';
 import { Reducer } from 'redux';
 import { ActionType, getType } from 'typesafe-actions';
 import * as actions from './actions';
@@ -24,25 +23,26 @@ export const loadouts: Reducer<LoadoutsState, LoadoutsAction> = (
 ): LoadoutsState => {
   switch (action.type) {
     case getType(actions.savePreviousLoadout): {
-      return produce(state, (draft) => {
-        const { storeId, loadoutId, previousLoadout } = action.payload;
-        let previousLoadouts = state.previousLoadouts[storeId] || [];
-        const lastPreviousLoadout = previousLoadouts.at(-1);
-        previousLoadouts =
-          lastPreviousLoadout && loadoutId === lastPreviousLoadout.id
-            ? // Pop the previous loadout since we're reapplying it
-              previousLoadouts.filter((l) => l.id !== loadoutId)
-            : // Push the previous loadout
-              [...previousLoadouts, previousLoadout];
-
-        draft.previousLoadouts[storeId] = previousLoadouts;
-      });
+      const { storeId, loadoutId, previousLoadout } = action.payload;
+      let previousLoadouts = state.previousLoadouts[storeId] || [];
+      const lastPreviousLoadout = previousLoadouts.at(-1);
+      previousLoadouts =
+        lastPreviousLoadout && loadoutId === lastPreviousLoadout.id
+          ? // Pop the previous loadout since we're reapplying it
+            previousLoadouts.filter((l) => l.id !== loadoutId)
+          : // Push the previous loadout
+            [...previousLoadouts, previousLoadout];
+      return {
+        ...state,
+        previousLoadouts: {
+          ...state.previousLoadouts,
+          [storeId]: previousLoadouts,
+        },
+      };
     }
 
     case getType(actions.updateLoadoutStore): {
-      return produce(state, (draft) => {
-        draft.selectedLoadoutStoreId = action.payload.storeId;
-      });
+      return { ...state, selectedLoadoutStoreId: action.payload.storeId };
     }
 
     default:
