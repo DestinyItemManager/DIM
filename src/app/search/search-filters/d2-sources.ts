c0db64a5ef498b35c1f03771399f2097e738e590
Rob Jones <delphi.actual@gmail.com>
2024-05-25 15:56:21 -0600

    use dcv info to generate source:dcv
    requires https://github.com/DestinyItemManager/d2-additional-info/pull/602

--- src/app/search/search-filters/d2-sources.ts
+++ src/app/search/search-filters/d2-sources.ts
@@ -1,10 +1,28 @@
+import { D2CalculatedSeason, D2SeasonInfo } from 'data/d2/d2-season-info';
 import D2Sources from 'data/d2/source-info-v2';
+const currentSeason = D2SeasonInfo[D2CalculatedSeason].season;
 
 // Fill in extra entries for the aliased source names
 for (const sourceAttrs of Object.values(D2Sources)) {
   if (sourceAttrs.aliases) {
     for (const alias of sourceAttrs.aliases) {
       D2Sources[alias] = sourceAttrs;
+    }
+  }
+}
+
+// Generate DCV source
+for (const sourceAttrs of Object.values(D2Sources)) {
+  if (sourceAttrs.enteredDCV && sourceAttrs.enteredDCV <= currentSeason) {
+    if (D2Sources.dcv) {
+      if (D2Sources.dcv.itemHashes && sourceAttrs.itemHashes) {
+        D2Sources.dcv.itemHashes.push(...sourceAttrs.itemHashes);
+      }
+      if (D2Sources.dcv.sourceHashes && sourceAttrs.sourceHashes) {
+        D2Sources.dcv.sourceHashes.push(...sourceAttrs.sourceHashes);
+      }
+    } else {
+      D2Sources.dcv = sourceAttrs;
     }
   }
 }
