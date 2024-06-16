fb885d223bd440e14f64fbbdc3ea7093220cc2f7
Ben Hollis <ben@benhollis.net>
2024-06-16 00:12:36 -0700

    More

--- src/app/inventory/RatingIcon.tsx
+++ src/app/inventory/RatingIcon.tsx
@@ -6,7 +6,7 @@ import styles from './RatingIcon.m.scss';
 export default function RatingIcon({ uiWishListRoll }: { uiWishListRoll: UiWishListRoll }) {
   if (uiWishListRoll === UiWishListRoll.Bad) {
     return (
-      <AppIcon className={styles.trashlist} icon={thumbsDownIcon} title={t('Item.ThumbsUp')} />
+      <AppIcon className={styles.trashlist} icon={thumbsDownIcon} title={t('Item.ThumbsDown')} />
     );
   }
 
