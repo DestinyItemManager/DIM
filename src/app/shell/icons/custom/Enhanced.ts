f8cf6308b74313ada25eb0a086cc7447bcc1dda6
FlaminSarge <flaminsarge@gmail.com>
2024-08-02 16:40:05 -0700

    Add 'enhanced' icon for enhanced weapons in place of 'shaped' icon
    This affects the item's badge and the icon next to the crafted level on the item popup

---
+++ src/app/shell/icons/custom/Enhanced.ts
@@ -0,0 +1,8 @@
+import { makeCustomIcon } from './utils';
+
+export const dimEnhancedIcon = makeCustomIcon(
+  'Enhanced',
+  32,
+  32,
+  'm 0 17.25 h 7 l 0 2 h 4 c 1 0 3 1 3.75 2 v 10.75 z m 32 0 h -7 l 0 2 h -4 c -1 0 -3 1 -3.75 2 v 10.75 z m -32 -2.5 h 7 l 0 -2 h 4 c 1 0 3 -1 3.75 -2 v -10.75 z m 32 0 h -7 l 0 -2 h -4 c -1 0 -3 -1 -3.75 -2 v -10.75 z',
+);
