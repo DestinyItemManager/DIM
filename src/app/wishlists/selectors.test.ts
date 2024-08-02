fc6dcfb7b20539423e3a9448e3832a5378f3778f
Jacobi <jtayala89@gmail.com>
2024-08-02 03:09:57 +0000

    chore: add tests on wishlist selector file.

--- src/app/wishlists/selectors.test.ts
+++ src/app/wishlists/selectors.test.ts
@@ -1,7 +1,11 @@
 import { RootState } from 'app/store/types';
 import Chance from 'chance';
-import { getTestRootState } from 'testing/test-utils';
-import { wishListsLastFetchedSelector, wishListsSelector } from './selectors';
+import { getTestRootState, getTestWishListRoll } from 'testing/test-utils';
+import {
+  wishListsByHashSelector,
+  wishListsLastFetchedSelector,
+  wishListsSelector,
+} from './selectors';
 
 const chance = Chance();
 
@@ -22,6 +26,42 @@ describe('wishlists selectors', () => {
     it('should return the lastFetched date for the whishlists in state', () => {
       const actualWishlistsLastFetched = wishListsLastFetchedSelector(expectedRootState);
       expect(actualWishlistsLastFetched).toEqual(expectedRootState.wishLists.lastFetched);
+    });
+  });
+
+  describe('wishListsByHashSelector', () => {
+    it('should return a map of item hashes and wishlist rolls', () => {
+      const expectedWishListRoll = getTestWishListRoll(chance);
+      expectedRootState.wishLists.wishListAndInfo.wishListRolls = [
+        ...expectedRootState.wishLists.wishListAndInfo.wishListRolls,
+        expectedWishListRoll,
+      ];
+      const actualWishlistsByHash = wishListsByHashSelector(expectedRootState);
+      expect(actualWishlistsByHash.get(expectedWishListRoll.itemHash)).toContain(
+        expectedWishListRoll,
+      );
+    });
+
+    it('should put wishlist rolls with the same hash under the same key', () => {
+      const expectedItemHash = chance.natural();
+      const expectedWishListRollOne = {
+        ...getTestWishListRoll(chance),
+        itemHash: expectedItemHash,
+      };
+      const expectedWishListRollTwo = {
+        ...getTestWishListRoll(chance),
+        itemHash: expectedItemHash,
+      };
+      expectedRootState.wishLists.wishListAndInfo.wishListRolls = [
+        ...expectedRootState.wishLists.wishListAndInfo.wishListRolls,
+        expectedWishListRollOne,
+        expectedWishListRollTwo,
+      ];
+      const actualWishlistsByHash = wishListsByHashSelector(expectedRootState);
+      expect(actualWishlistsByHash.get(expectedWishListRollOne.itemHash)).toEqual([
+        expectedWishListRollOne,
+        expectedWishListRollTwo,
+      ]);
     });
   });
 });
