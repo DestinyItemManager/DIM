18c4d8693e84323d1193d34bdf0829ef432f8cf0
FlaminSarge <flaminsarge@gmail.com>
2024-08-04 07:22:18 -0700

    Fix Europa theme godroll indicator color and add more examples for god/bad rolls when previewing themes

--- src/app/settings/SettingsPage.tsx
+++ src/app/settings/SettingsPage.tsx
@@ -69,11 +69,37 @@ export default function SettingsPage() {
   const exampleWeapon = allItems.find(
     (i) => i.bucket.sort === 'Weapons' && !i.isExotic && !i.masterwork && !i.deepsightInfo,
   );
+  const exampleWeaponBad = allItems.find(
+    (i) =>
+      i !== exampleWeapon &&
+      i.bucket.sort === 'Weapons' &&
+      !i.isExotic &&
+      !i.masterwork &&
+      !i.deepsightInfo,
+  );
   // Include a masterworked item because they look different in some themes
   const exampleWeaponMasterworked = allItems.find(
     (i) => i.bucket.sort === 'Weapons' && !i.isExotic && i.masterwork && !i.deepsightInfo,
   );
+  const exampleWeaponMasterworkedBad = allItems.find(
+    (i) =>
+      i !== exampleWeaponMasterworked &&
+      i.bucket.sort === 'Weapons' &&
+      !i.isExotic &&
+      i.masterwork &&
+      !i.deepsightInfo,
+  );
   const exampleArmor = allItems.find((i) => i.bucket.sort === 'Armor' && !i.isExotic);
+  const godRoll = {
+    wishListPerks: new Set(),
+    notes: undefined,
+    isUndesirable: false,
+  };
+  const badRoll = {
+    wishListPerks: new Set(),
+    notes: undefined,
+    isUndesirable: true,
+  };
 
   const onCheckChange = (checked: boolean, name: keyof Settings) => {
     if (name.length === 0) {
@@ -270,7 +296,17 @@ export default function SettingsPage() {
                 <InventoryItem
                   item={exampleWeapon}
                   isNew={settings.showNewItems}
-                  tag="favorite"
+                  tag="keep"
+                  wishlistRoll={godRoll}
+                  autoLockTagged={settings.autoLockTagged}
+                />
+              )}
+              {exampleWeaponBad && (
+                <InventoryItem
+                  item={exampleWeaponBad}
+                  isNew={settings.showNewItems}
+                  tag="infuse"
+                  wishlistRoll={badRoll}
                   autoLockTagged={settings.autoLockTagged}
                 />
               )}
@@ -278,7 +314,17 @@ export default function SettingsPage() {
                 <InventoryItem
                   item={exampleWeaponMasterworked}
                   isNew={settings.showNewItems}
-                  tag="keep"
+                  tag="favorite"
+                  wishlistRoll={godRoll}
+                  autoLockTagged={settings.autoLockTagged}
+                />
+              )}
+              {exampleWeaponMasterworkedBad && (
+                <InventoryItem
+                  item={exampleWeaponMasterworkedBad}
+                  isNew={settings.showNewItems}
+                  tag="junk"
+                  wishlistRoll={badRoll}
                   autoLockTagged={settings.autoLockTagged}
                 />
               )}
@@ -286,7 +332,7 @@ export default function SettingsPage() {
                 <InventoryItem
                   item={exampleArmor}
                   isNew={settings.showNewItems}
-                  tag="keep"
+                  tag="archive"
                   autoLockTagged={settings.autoLockTagged}
                 />
               )}
