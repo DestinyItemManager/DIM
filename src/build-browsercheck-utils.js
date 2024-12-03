72dfb39eb077fbf2e8eb75b09c563a3aca9bd6d9
robojumper <robojumper@gmail.com>
2024-12-03 19:20:37 +0100

    Maybe fix browsercheck escaping

--- src/build-browsercheck-utils.js
+++ src/build-browsercheck-utils.js
@@ -21,19 +21,23 @@ import zhCHT from './locale/zhCHT.json' with { type: 'json' };
 function getI18nKey(key) {
   let key1 = key.split('.')[0];
   let key2 = key.split('.')[1];
-  return `  en: "${en[key1][key2]}",
-  de: "${de[key1]?.[key2] ?? en[key1][key2]}",
-  es: "${es[key1]?.[key2] ?? en[key1][key2]}",
-  'es-mx': "${esMX[key1]?.[key2] ?? en[key1][key2]}",
-  fr: "${fr[key1]?.[key2] ?? en[key1][key2]}",
-  it: "${it[key1]?.[key2] ?? en[key1][key2]}",
-  ja: "${ja[key1]?.[key2] ?? en[key1][key2]}",
-  ko: "${ko[key1]?.[key2] ?? en[key1][key2]}",
-  pl: "${pl[key1]?.[key2] ?? en[key1][key2]}",
-  'pt-br': "${ptBR[key1]?.[key2] ?? en[key1][key2]}",
-  ru: "${ru[key1]?.[key2] ?? en[key1][key2]}",
-  'zh-chs': "${zhCHS[key1]?.[key2] ?? en[key1][key2]}",
-  'zh-cht': "${zhCHT[key1]?.[key2] ?? en[key1][key2]}",\n};`;
+  let getStringFor = (lang) => {
+    const str = lang[key1]?.[key2] ?? en[key1][key2];
+    return JSON.stringify(str);
+  };
+  return `  en: ${getStringFor(en)},
+  de: ${getStringFor(de)},
+  es: ${getStringFor(es)},
+  'es-mx': ${getStringFor(esMX)},
+  fr: ${getStringFor(fr)},
+  it: ${getStringFor(it)},
+  ja: ${getStringFor(ja)},
+  ko: ${getStringFor(ko)},
+  pl: ${getStringFor(pl)},
+  'pt-br': ${getStringFor(ptBR)},
+  ru: ${getStringFor(ru)},
+  'zh-chs': ${getStringFor(zhCHS)},
+  'zh-cht': ${getStringFor(zhCHT)},\n};`;
 }
 
 var browserCheckUtils = `export const supportedLanguages = [
