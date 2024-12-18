bd220948285782eeb2ddecc83c9a5280410c88eb
Ben Hollis <ben@benhollis.net>
2024-12-17 22:26:37 -0800

    Downgrade buggy react-hooks lint

--- src/earlyErrorReport.js
+++ src/earlyErrorReport.js
@@ -5,7 +5,7 @@ window.onerror = (message, source, line, col, error) => {
   const errorBox = document.querySelector('#errorreport');
   if (errorBox) {
     errorBox.textContent = Object.entries(params)
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
       .map(([k, v]) => `${k}:\n  ${v}`)
       .join('\n');
   }
