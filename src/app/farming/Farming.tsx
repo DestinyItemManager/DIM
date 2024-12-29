bafc858357f4d5ec1f33217de3c09bc45fc14400
Ben Hollis <ben@benhollis.net>
2024-12-28 16:27:48 -0800

    We don't need this ref at all

--- src/app/farming/Farming.tsx
+++ src/app/farming/Farming.tsx
@@ -4,7 +4,7 @@ import { useSetting } from 'app/settings/hooks';
 import { useThunkDispatch } from 'app/store/thunk-dispatch';
 import clsx from 'clsx';
 import { AnimatePresence, Spring, Variants, motion } from 'motion/react';
-import React, { useRef } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { stopFarming } from './actions';
 import './farming.scss';
@@ -25,15 +25,12 @@ export default function Farming() {
   const makeRoomForItemsChanged = (e: React.ChangeEvent<HTMLInputElement>) =>
     setMakeRoomForItems(e.currentTarget.checked);
 
-  const nodeRef = useRef<HTMLDivElement>(null);
-
   const onStopClicked = () => dispatch(stopFarming());
 
   return (
     <AnimatePresence>
       {store && (
         <motion.div
-          ref={nodeRef}
           id="item-farming"
           className={clsx({ 'd2-farming': store.destinyVersion === 2 })}
           initial="hidden"
