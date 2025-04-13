2bfc755cc190b3fc8670a3ec53049607227c94d9
Ben Hollis <ben@benhollis.net>
2025-04-13 08:58:25 -0700

    Correctly log about initial load

--- src/app/inventory/d2-stores.ts
+++ src/app/inventory/d2-stores.ts
@@ -158,6 +158,12 @@ export function loadStores({
               infoLog(TAG, 'First time loading stores, only loading from IDB (if available)');
               await dispatch(loadStoresData(account, { firstTime, fromOtherTab }));
               firstTime = false;
+              if (getState().inventory.live) {
+                infoLog(TAG, 'Initial load got live data, skipping fast-follow load');
+                return;
+              } else {
+                infoLog(TAG, 'Fast-follow load live stores from Bungie.net');
+              }
             }
             stores = await dispatch(loadStoresData(account, { firstTime: false, fromOtherTab }));
           } finally {
