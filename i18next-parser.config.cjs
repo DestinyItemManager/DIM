30aef4202133c7fbea5e7619943efc3abf6db085
Rob Jones <delphi.actual@gmail.com>
2025-07-23 08:10:30 -0600

    regex map

--- i18next-parser.config.cjs
+++ i18next-parser.config.cjs
@@ -5,37 +5,37 @@ const typescript = require('typescript');
 // Dynamic Keys Regexen
 // Split up for legibility
 
-const Accounts = /Accounts.NoCharactersTitle/;
-const Activities = /Activities.*/;
-const AWA = /AWA.FailedToken/;
-const Bucket = /Bucket.*/;
-const BungieService = /BungieService.*/;
-const Countdown = /Countdown.*/;
-const Csv = /Csv.(EmptyFile|WrongFields)/;
-const FarmingMode = /FarmingMode.*/;
-const Filter = /Filter.*/;
-const Glyphs = /Glyphs.*/;
-const Help = /Help.CannotMove/;
-const Hotkey = /Hotkey.(Enter|Tab)/;
-const KillType = /KillType.*/;
-const ItemService = /ItemService.(Classified|NotEnoughRoomGeneral)/;
-const LB = /LB.SelectModsCount*/;
-const LoadoutAnalysis = /LoadoutAnalysis.*/;
-const LoadoutBuilder = /LoadoutBuilder.*/;
-const Sockets = /Sockets.*/;
-const Loadouts = /Loadouts.*/;
-const Organizer = /Organizer.*/;
-const Progress = /Progress.Percent*/;
-const Stats = /Stats.WeaponPart/;
-const Tags = /Tags.*/;
-const Triage = /Triage.*/;
-const WishListRoll = /WishListRoll.*/;
-const nospace = /no-space/;
-const wronglevel = /wrong-level/;
+const regexConstants = [
+  { name: 'Accounts', pattern: /Accounts.NoCharactersTitle/ },
+  { name: 'Activities', pattern: /Activities.*/ },
+  { name: 'AWA', pattern: /AWA.FailedToken/ },
+  { name: 'Bucket', pattern: /Bucket.*/ },
+  { name: 'BungieService', pattern: /BungieService.*/ },
+  { name: 'Countdown', pattern: /Countdown.*/ },
+  { name: 'Csv', pattern: /Csv.(EmptyFile|WrongFields)/ },
+  { name: 'FarmingMode', pattern: /FarmingMode.*/ },
+  { name: 'Filter', pattern: /Filter.*/ },
+  { name: 'Glyphs', pattern: /Glyphs.*/ },
+  { name: 'Help', pattern: /Help.CannotMove/ },
+  { name: 'Hotkey', pattern: /Hotkey.(Enter|Tab)/ },
+  { name: 'KillType', pattern: /KillType.*/ },
+  { name: 'ItemService', pattern: /ItemService.(Classified|NotEnoughRoomGeneral)/ },
+  { name: 'LB', pattern: /LB.SelectModsCount*/ },
+  { name: 'LoadoutAnalysis', pattern: /LoadoutAnalysis.*/ },
+  { name: 'LoadoutBuilder', pattern: /LoadoutBuilder.*/ },
+  { name: 'Sockets', pattern: /Sockets.*/ },
+  { name: 'Loadouts', pattern: /Loadouts.*/ },
+  { name: 'Organizer', pattern: /Organizer.*/ },
+  { name: 'Progress', pattern: /Progress.Percent*/ },
+  { name: 'Stats', pattern: /Stats.WeaponPart/ },
+  { name: 'Tags', pattern: /Tags.*/ },
+  { name: 'Triage', pattern: /Triage.*/ },
+  { name: 'WishListRoll', pattern: /WishListRoll.*/ },
+  { name: 'nospace', pattern: /no-space/ },
+  { name: 'wronglevel', pattern: /wrong-level/ },
+];
 
-const DynamicKeys = new RegExp(
-  `${Accounts.source}|${Activities.source}|${AWA.source}|${Bucket.source}|${BungieService.source}|${Countdown.source}|${Csv.source}|${FarmingMode.source}|${Filter.source}|${Glyphs.source}|${Help.source}|${Hotkey.source}|${KillType.source}|${ItemService.source}|${LB.source}|${LoadoutAnalysis.source}|${LoadoutBuilder.source}|${Sockets.source}|${Loadouts.source}|${Organizer.source}|${Progress.source}|${Stats.source}|${Tags.source}|${Triage.source}|${WishListRoll.source}|${nospace.source}|${wronglevel.source}`,
-);
+const DynamicKeys = new RegExp(regexConstants.map((r) => r.pattern.source).join('|'));
 
 module.exports = {
   input: ['src/app/**/*.{js,jsx,ts,tsx,cjs,mjs,cts,mts}', 'src/browsercheck.js'],
