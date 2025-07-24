14f300c44d222ff0236d3f9d2c3aa6857153000a
Ben Hollis <ben@benhollis.net>
2025-07-24 02:08:32 -0700

    Show faded silver when it's not available (#11250)

--- src/app/store-stats/AccountCurrencies.tsx
+++ src/app/store-stats/AccountCurrencies.tsx
@@ -1,29 +1,54 @@
 import BungieImage from 'app/dim-ui/BungieImage';
+import { t } from 'app/i18next-t';
 import { currenciesSelector } from 'app/inventory/selectors';
+import { useD2Definitions } from 'app/manifest/selectors';
+import clsx from 'clsx';
 import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import styles from './AccountCurrencies.m.scss';
 
 /** The account currencies (glimmer, shards, etc.) */
 export default memo(function AccountCurrency() {
-  const currencies = useSelector(currenciesSelector);
+  let currencies = useSelector(currenciesSelector);
+  const defs = useD2Definitions();
+  let missingSilver = false;
+
+  if (defs && !currencies.some((c) => c.itemHash === 3147280338 /* Silver */)) {
+    const silverDef = defs.InventoryItem.get(3147280338);
+    missingSilver = true;
+    currencies = [
+      ...currencies,
+      { itemHash: silverDef.hash, quantity: 0, displayProperties: silverDef.displayProperties },
+    ];
+  }
+
   return currencies.length > 0 ? (
     <React.Fragment key={currencies.map((c) => c.itemHash).join()}>
-      {currencies.map((currency) => (
-        <React.Fragment key={currency.itemHash}>
-          <BungieImage
-            className={styles.icon}
-            src={currency.displayProperties.icon}
-            title={currency.displayProperties.name}
-          />
-          <div
-            className={styles.text}
-            title={`${currency.quantity.toLocaleString()} ${currency.displayProperties.name}`}
-          >
-            {currency.quantity.toLocaleString()}
-          </div>
-        </React.Fragment>
-      ))}
+      {currencies.map((currency) => {
+        const isMissingSilver = missingSilver && currency.itemHash === 3147280338;
+        const title = isMissingSilver
+          ? t('Inventory.MissingSilver')
+          : `${currency.quantity.toLocaleString()} ${currency.displayProperties.name}`;
+        return (
+          <React.Fragment key={currency.itemHash}>
+            <BungieImage
+              className={clsx(styles.icon, {
+                [styles.faded]: isMissingSilver,
+              })}
+              src={currency.displayProperties.icon}
+              title={title}
+            />
+            <div
+              className={clsx(styles.text, {
+                [styles.faded]: isMissingSilver,
+              })}
+              title={title}
+            >
+              {isMissingSilver ? '???' : currency.quantity.toLocaleString()}
+            </div>
+          </React.Fragment>
+        );
+      })}
       {/* add 0-2 blank slots to keep each currencyGroup rounded to a multiple of 3 (for css grid) */}
       {Array.from({ length: (3 - (currencies.length % 3)) % 3 }, (_, i) => (
         <React.Fragment key={i}>
