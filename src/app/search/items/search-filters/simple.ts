86cc8a81727eb8acebcf6ec6ab93c8b0cb7ad0b1
Ben Hollis <ben@benhollis.net>
2025-07-24 01:28:14 -0700

    Add Armor3.0 search, restrict Armor2.0 search (#11242)
    * Add Armor3.0 search, restrict Armor2.0 search
    
    * Update a few strings that reference Armor 2.0

--- src/app/search/items/search-filters/simple.ts
+++ src/app/search/items/search-filters/simple.ts
@@ -1,4 +1,5 @@
 import { tl } from 'app/i18next-t';
+import { isEdgeOfFateArmorMasterwork } from 'app/utils/item-utils';
 import { BucketHashes } from 'data/d2/generated-enums';
 import { ItemFilterDefinition } from '../item-filter-types';
 
@@ -8,7 +9,15 @@ const simpleFilters: ItemFilterDefinition[] = [
     keywords: 'armor2.0',
     description: tl('Filter.Energy'),
     destinyVersion: 2,
-    filter: () => (item) => Boolean(item.energy) && item.bucket.inArmor,
+    filter: () => (item) =>
+      Boolean(item.energy) && item.bucket.inArmor && !isEdgeOfFateArmorMasterwork(item),
+  },
+  {
+    keywords: 'armor3.0',
+    description: tl('Filter.Armor3'),
+    destinyVersion: 2,
+    filter: () => (item) =>
+      Boolean(item.energy) && item.bucket.inArmor && isEdgeOfFateArmorMasterwork(item),
   },
   {
     keywords: 'weapon',
