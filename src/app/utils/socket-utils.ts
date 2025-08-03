6397b2b078423a4d98aabed56c1c447e8c52a0b9
chainrez <186519033+chainrez@users.noreply.github.com>
2025-08-02 21:19:09 -0700

    Fix: Widen detection of enhanced plugs

--- src/app/utils/socket-utils.ts
+++ src/app/utils/socket-utils.ts
@@ -14,6 +14,7 @@ import {
   GhostActivitySocketTypeHashes,
   armor2PlugCategoryHashes,
   weaponMasterworkY2SocketTypeHash,
+  weaponParts,
 } from 'app/search/d2-known-values';
 import {
   DestinyInventoryItemDefinition,
@@ -254,8 +255,9 @@ export function isEventArmorRerollSocket(socket: DimSocket) {
 
 export function isEnhancedPerk(plugDef: PluggableInventoryItemDefinition) {
   return (
-    plugDef.plug.plugCategoryHash === PlugCategoryHashes.Frames &&
-    plugDef.inventory!.tierType === TierType.Common
+    plugDef.inventory!.tierType === TierType.Common &&
+    (plugDef.plug.plugCategoryHash === PlugCategoryHashes.Frames ||
+      weaponParts.has(plugDef.plug.plugCategoryHash))
   );
 }
 
