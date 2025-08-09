d826dd3d54ac34c2ed9d15299bd4331467489083
Ben Hollis <ben@benhollis.net>
2025-08-09 15:34:26 -0700

    Explain how pinned items work (or don't, as the case may be)

--- src/app/loadout-builder/filter/LoadoutOptimizerMenuItems.tsx
+++ src/app/loadout-builder/filter/LoadoutOptimizerMenuItems.tsx
@@ -80,6 +80,7 @@ export const LoadoutOptimizerPinnedItems = memo(function LoadoutOptimizerPinnedI
           ))}
           <AddItemButton onClick={chooseLockItem} title={t('LoadoutBuilder.LockItem')} />
         </div>
+        <div className={styles.fineprint}>{t('LoadoutBuilder.PinnedItemsFinePrint')}</div>
       </LoadoutBucketDropTarget>
     </LoadoutEditSection>
   );
