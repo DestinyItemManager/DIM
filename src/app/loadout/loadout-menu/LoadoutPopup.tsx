a0c5b4b8caae67b0494a73165ae63911498e0458
Ben Hollis <ben@benhollis.net>
2025-09-14 15:03:30 -0700

    Use current season pass hash from D2 core settings

--- src/app/loadout/loadout-menu/LoadoutPopup.tsx
+++ src/app/loadout/loadout-menu/LoadoutPopup.tsx
@@ -6,7 +6,7 @@ import ClassIcon from 'app/dim-ui/ClassIcon';
 import ColorDestinySymbols from 'app/dim-ui/destiny-symbols/ColorDestinySymbols';
 import { startFarming } from 'app/farming/actions';
 import { t } from 'app/i18next-t';
-import { getRewardMultiplier } from 'app/inventory/rewards';
+import { useRewardMultiplier } from 'app/inventory/rewards';
 import {
   allItemsSelector,
   bucketsSelector,
@@ -462,6 +462,6 @@ function doApplyRandomLoadout(store: DimStore, options: RandomLoadoutOptions): T
 
 function RewardMultiplier({ defs, store }: { defs: D2ManifestDefinitions; store: DimStore }) {
   const profileInfo = useSelector(profileResponseSelector);
-  const multi = profileInfo && getRewardMultiplier(defs, profileInfo, store);
+  const multi = useRewardMultiplier(defs, profileInfo, store);
   return multi?.toFixed(2);
 }
