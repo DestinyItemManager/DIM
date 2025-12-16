658790357c47692a14313130b6192b38139ccf1f
Carlos Guillot <carlos@guillot.dev>
2025-12-16 09:57:06 -0600

    Add Heat Generated and Cooling Efficiency stats to weapons CSV export (#11651)

--- src/app/inventory/spreadsheets.ts
+++ src/app/inventory/spreadsheets.ts
@@ -84,6 +84,8 @@ export const csvStatNamesForDestinyVersion = (destinyVersion: DestinyVersion) =>
     [StatHashes.ShieldDuration, 'Shield Duration'],
     [StatHashes.AirborneEffectiveness, 'Airborne Effectiveness'],
     [StatHashes.AmmoGeneration, 'Ammo Generation'],
+    [StatHashes.HeatGenerated, 'Heat Generated'],
+    [StatHashes.CoolingEfficiency, 'Cooling Efficiency'],
     [StatHashes.Weapons, destinyVersion === 2 ? 'Weapons' : 'Mobility'],
     [StatHashes.Health, destinyVersion === 2 ? 'Health' : 'Resilience'],
     [StatHashes.Class, destinyVersion === 2 ? 'Class' : 'Recovery'],
