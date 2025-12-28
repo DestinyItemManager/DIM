077a523a369c3e4ae191b20ef025b1993905e80d
FlaminSarge <flaminsarge@gmail.com>
2025-12-28 02:28:44 -0700

    Add: setting to toggle display of ornaments, behavior for viewing ornaments when hovering over items
    Includes:
    icon fades into alternate icon with 0.2s transition
    icon fade is instant for reduced-motion preference
    masterworkGlow now shows up on top of foreground/altIcon
    content-visibility is used to hide the inactive icon
    example ornamented item is previewed on settings page

--- src/app/css-variables.ts
+++ src/app/css-variables.ts
@@ -1,3 +1,4 @@
+import { OrnamentDisplay } from '@destinyitemmanager/dim-api-types';
 import { settingsSelector } from 'app/dim-api/selectors';
 import { deepEqual } from 'fast-equals';
 import { isPhonePortraitSelector } from './shell/selectors';
@@ -22,6 +23,24 @@ export function createItemSizeObserver(): StoreObserver<number> {
     getObserved: (rs) => settingsSelector(rs).itemSize,
     sideEffect: ({ current }) => {
       setCSSVariable('--item-size', `${Math.max(48, current)}px`);
+    },
+  };
+}
+
+export function createOrnamentDisplayObserver(): StoreObserver<OrnamentDisplay> {
+  return {
+    id: 'ornament-display-observer',
+    getObserved: (rs) => settingsSelector(rs).ornamentDisplay,
+    sideEffect: ({ current }) => {
+      setCSSVariable('--ornament-display-opacity', current === OrnamentDisplay.All ? 1 : 0);
+      setCSSVariable(
+        '--ornament-display-visibility',
+        current === OrnamentDisplay.All ? 'auto' : 'hidden',
+      );
+      setCSSVariable(
+        '--ornament-display-visibility-inverse',
+        current === OrnamentDisplay.All ? 'hidden' : 'auto',
+      );
     },
   };
 }
