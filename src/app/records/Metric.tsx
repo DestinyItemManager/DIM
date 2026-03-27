4d529fac479a546bbe8eae29438f7c87897a4789
FlaminSarge <flaminsarge@gmail.com>
2026-03-27 00:54:15 -0700

    Make stat trackers use card layout instead of one-per-row, show 'goal' value

--- src/app/records/Metric.tsx
+++ src/app/records/Metric.tsx
@@ -1,6 +1,7 @@
 import RichDestinyText from 'app/dim-ui/destiny-symbols/RichDestinyText';
 import { useD2Definitions } from 'app/manifest/selectors';
 import { ObjectiveValue } from 'app/progress/Objective';
+import { percent } from 'app/shell/formatters';
 import clsx from 'clsx';
 import * as styles from './Metric.m.scss';
 import MetricBanner from './MetricBanner';
@@ -21,31 +22,56 @@ export default function Metric({ metric }: Props) {
   const masterwork = metricComponent.objectiveProgress.complete;
 
   const objectiveDef = defs.Objective.get(metricComponent.objectiveProgress.objectiveHash);
+  const completionValue = objectiveDef?.completionValue ?? 0;
+  const hasGoal =
+    completionValue > 0 &&
+    !(
+      objectiveDef &&
+      'allowOvercompletion' in objectiveDef &&
+      objectiveDef.allowOvercompletion &&
+      completionValue === 1
+    );
+
+  const progress = metricComponent.objectiveProgress.progress || 0;
 
   return (
-    <div className={styles.metric}>
+    <div
+      className={clsx(styles.metric, {
+        [styles.completed]: masterwork,
+        [styles.hasProgressBar]: hasGoal,
+      })}
+    >
       <MetricBanner
         className={styles.icon}
         metricHash={metricHash}
         objectiveProgress={metricComponent.objectiveProgress}
       />
-      <div className={clsx(styles.header, { [styles.masterworked]: masterwork })}>
-        <div className={styles.name}>{name}</div>
-        <div className={styles.value}>
-          <ObjectiveValue
-            objectiveDef={objectiveDef}
-            progress={metricComponent.objectiveProgress.progress || 0}
-          />
+      <div className={styles.info}>
+        <div className={clsx(styles.value, { [styles.masterworked]: masterwork })}>
+          <ObjectiveValue objectiveDef={objectiveDef} progress={progress} />
+          {hasGoal && (
+            <div className={styles.goal}>
+              / <ObjectiveValue objectiveDef={objectiveDef} progress={completionValue} />
+            </div>
+          )}
         </div>
-      </div>
-
-      <div>
+        <div className={clsx(styles.name, { [styles.masterworked]: masterwork })}>{name}</div>
         {description && (
           <p className={styles.description}>
             <RichDestinyText text={description} />
           </p>
         )}
       </div>
+      {hasGoal && (
+        <div className={clsx(styles.progressBar, { [styles.complete]: masterwork })}>
+          {!masterwork && (
+            <div
+              className={styles.progressFill}
+              style={{ width: percent(progress / completionValue) }}
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 }
