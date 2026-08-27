b98fd8e7957d27de38fba48b24c28c25a88af6ed
FlaminSarge <FlaminSarge@users.noreply.github.com>
2026-08-27 05:38:49 -0700

    Update shared loadout text to indicate that overrides could be for subclass or artifact

--- src/app/loadout/loadout-share/LoadoutShareSheet.tsx
+++ src/app/loadout/loadout-share/LoadoutShareSheet.tsx
@@ -77,7 +77,7 @@ export default function LoadoutShareSheet({
   const numMods = loadout.parameters?.mods?.length ?? 0;
   const numItems = count(loadout.items, (i) => !i.socketOverrides);
   const hasFashion = Boolean(loadout.parameters?.modsByBucket);
-  const hasSubclass = loadout.items.some((i) => i.equip && i.socketOverrides);
+  const hasOverrides = loadout.items.some((i) => i.equip && i.socketOverrides);
   const hasLoParams = Boolean(
     loadout.parameters &&
     (loadout.parameters.query ||
@@ -108,7 +108,7 @@ export default function LoadoutShareSheet({
                   {numItems > 0 && <li>{t('Loadouts.Share.NumItems', { count: numItems })}</li>}
                   {numMods > 0 && <li>{t('Loadouts.Share.NumMods', { count: numItems })}</li>}
                   {hasFashion && <li>{t('Loadouts.Share.Fashion')}</li>}
-                  {hasSubclass && <li>{t('Loadouts.Share.Subclass')}</li>}
+                  {hasOverrides && <li>{t('Loadouts.Share.SocketOverrides')}</li>}
                   {hasLoParams && <li>{t('Loadouts.Share.LoadoutOptimizer')}</li>}
                   {loadout.notes && <li>{t('Loadouts.Share.Notes')}</li>}
                 </ul>
